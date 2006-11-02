primiano/blink-gitcs

commit 1c7f64865fce2c7c599052dd7c2c10e60185301c

Reviewed by Mitz.
        Fix for http://bugs.webkit.org/show_bug.cgi?id=11474
        Rename the "p" member variable of the PaintInfo struct to "context"

        - Renames the 'p' and 'r' member variables of the PaintInfo struct to
          'context' and 'rect' respectively.

        - Assorted surrounding cleanups.

        * rendering/EllipsisBox.cpp:
        (WebCore::EllipsisBox::paint):
        * rendering/EllipsisBox.h:
        * rendering/InlineBox.cpp:
        (WebCore::InlineBox::paint):
        * rendering/InlineFlowBox.cpp:
        (WebCore::InlineFlowBox::paint):
        (WebCore::InlineFlowBox::paintBackground):
        (WebCore::InlineFlowBox::paintBackgroundAndBorder):
        (WebCore::InlineFlowBox::paintDecorations):
        * rendering/InlineFlowBox.h:
        * rendering/InlineRunBox.h:
        * rendering/InlineTextBox.cpp:
        (WebCore::InlineTextBox::paint):
        * rendering/InlineTextBox.h:
        * rendering/RenderBlock.cpp:
        (WebCore::RenderBlock::paint):
        (WebCore::RenderBlock::paintChildren):
        (WebCore::RenderBlock::paintCaret):
        (WebCore::RenderBlock::paintObject):
        (WebCore::RenderBlock::paintFloats):
        (WebCore::RenderBlock::paintEllipsisBoxes):
        (WebCore::RenderBlock::paintSelection):
        (WebCore::RenderBlock::fillSelectionGaps):
        (WebCore::RenderBlock::fillInlineSelectionGaps):
        (WebCore::RenderBlock::fillBlockSelectionGaps):
        (WebCore::RenderBlock::fillHorizontalSelectionGap):
        (WebCore::RenderBlock::fillVerticalSelectionGap):
        (WebCore::RenderBlock::fillLeftSelectionGap):
        (WebCore::RenderBlock::fillRightSelectionGap):
        * rendering/RenderBox.cpp:
        (WebCore::RenderBox::paint):
        (WebCore::RenderBox::paintRootBoxDecorations):
        (WebCore::RenderBox::paintBoxDecorations):
        * rendering/RenderBox.h:
        * rendering/RenderButton.cpp:
        (WebCore::RenderButton::paintObject):

WebCore/rendering/RenderTable.cpp
@@ -380,22 +380,22 @@ void RenderTable::setCellWidths()
             static_cast<RenderTableSection *>(child)->setCellWidths();
 }
 
-void RenderTable::paint(PaintInfo& i, int _tx, int _ty)
+void RenderTable::paint(PaintInfo& paintInfo, int tx, int ty)
 {
-    _tx += xPos();
-    _ty += yPos();
+    tx += xPos();
+    ty += yPos();
 
-    PaintPhase paintPhase = i.phase;
-    
-    int os = 2*maximalOutlineSize(paintPhase);
-    if (_ty >= i.r.bottom() + os || _ty + height() <= i.r.y() - os)
+    PaintPhase paintPhase = paintInfo.phase;
+
+    int os = 2 * maximalOutlineSize(paintPhase);
+    if (ty >= paintInfo.rect.bottom() + os || ty + height() <= paintInfo.rect.y() - os)
         return;
-    if (_tx >= i.r.right() + os || _tx + width() <= i.r.x() - os)
+    if (tx >= paintInfo.rect.right() + os || tx + width() <= paintInfo.rect.x() - os)
         return;
 
     if ((paintPhase == PaintPhaseBlockBackground || paintPhase == PaintPhaseChildBlockBackground)
         && shouldPaintBackgroundOrBorder() && style()->visibility() == VISIBLE)
-        paintBoxDecorations(i, _tx, _ty);
+        paintBoxDecorations(paintInfo, tx, ty);
 
     // We're done.  We don't bother painting any children.
     if (paintPhase == PaintPhaseBlockBackground)
@@ -404,12 +404,13 @@ void RenderTable::paint(PaintInfo& i, int _tx, int _ty)
     // We don't paint our own background, but we do let the kids paint their backgrounds.
     if (paintPhase == PaintPhaseChildBlockBackgrounds)
         paintPhase = PaintPhaseChildBlockBackground;
-    PaintInfo paintInfo(i);
-    paintInfo.phase = paintPhase;
-    
-    for (RenderObject *child = firstChild(); child; child = child->nextSibling())
+    PaintInfo info(paintInfo);
+    info.phase = paintPhase;
+
+    for (RenderObject* child = firstChild(); child; child = child->nextSibling()) {
         if (!child->layer() && (child->isTableSection() || child == tCaption))
-            child->paint(paintInfo, _tx, _ty);
+            child->paint(info, tx, ty);
+    }
 
     if (collapseBorders() && paintPhase == PaintPhaseChildBlockBackground && style()->visibility() == VISIBLE) {
         // Collect all the unique border styles that we want to paint in a sorted list.  Once we
@@ -422,41 +423,42 @@ void RenderTable::paint(PaintInfo& i, int _tx, int _ty)
         DeprecatedValueListIterator<CollapsedBorderValue> end = borderStyles.end();
         for (; it != end; ++it) {
             m_currentBorder = &(*it);
-            for (RenderObject* child = firstChild(); child; child = child->nextSibling())
+            for (RenderObject* child = firstChild(); child; child = child->nextSibling()) {
                 if (child->isTableSection())
-                    child->paint(paintInfo, _tx, _ty);
+                    child->paint(info, tx, ty);
+            }
         }
     }
-        
+
 #ifdef BOX_DEBUG
-    outlineBox(i.p, _tx, _ty, "blue");
+    outlineBox(paintInfo.context, tx, ty, "blue");
 #endif
 }
 
-void RenderTable::paintBoxDecorations(PaintInfo& i, int _tx, int _ty)
+void RenderTable::paintBoxDecorations(PaintInfo& paintInfo, int tx, int ty)
 {
     int w = width();
     int h = height();
-    
+
     // Account for the caption.
     if (tCaption) {
         int captionHeight = (tCaption->height() + tCaption->marginBottom() +  tCaption->marginTop());
         h -= captionHeight;
         if (tCaption->style()->captionSide() != CAPBOTTOM)
-            _ty += captionHeight;
+            ty += captionHeight;
     }
 
-    int my = max(_ty, i.r.y());
+    int my = max(ty, paintInfo.rect.y());
     int mh;
-    if (_ty < i.r.y())
-        mh= max(0, h - (i.r.y() - _ty));
+    if (ty < paintInfo.rect.y())
+        mh = max(0, h - (paintInfo.rect.y() - ty));
     else
-        mh = min(i.r.height(), h);
-    
-    paintBackground(i.p, style()->backgroundColor(), style()->backgroundLayers(), my, mh, _tx, _ty, w, h);
-    
+        mh = min(paintInfo.rect.height(), h);
+
+    paintBackground(paintInfo.context, style()->backgroundColor(), style()->backgroundLayers(), my, mh, tx, ty, w, h);
+
     if (style()->hasBorder() && !collapseBorders())
-        paintBorder(i.p, _tx, _ty, w, h, style());
+        paintBorder(paintInfo.context, tx, ty, w, h, style());
 }
 
 void RenderTable::calcMinMaxWidth()

WebCore/rendering/EllipsisBox.cpp
@@ -29,35 +29,35 @@
 
 namespace WebCore {
 
-void EllipsisBox::paint(RenderObject::PaintInfo& i, int _tx, int _ty)
+void EllipsisBox::paint(RenderObject::PaintInfo& paintInfo, int tx, int ty)
 {
-    GraphicsContext* p = i.p;
-    RenderStyle* _style = m_firstLine ? m_object->firstLineStyle() : m_object->style();
-    if (_style->font() != p->font())
-        p->setFont(_style->font());
+    GraphicsContext* context = paintInfo.context;
+    RenderStyle* style = m_object->style(m_firstLine);
+    if (style->font() != context->font())
+        context->setFont(style->font());
 
-    Color textColor = _style->color();
-    if (textColor != p->pen().color())
-        p->setPen(textColor);
+    Color textColor = style->color();
+    if (textColor != context->pen().color())
+        context->setPen(textColor);
     bool setShadow = false;
-    if (_style->textShadow()) {
-        p->setShadow(IntSize(_style->textShadow()->x, _style->textShadow()->y),
-                     _style->textShadow()->blur, _style->textShadow()->color);
+    if (style->textShadow()) {
+        context->setShadow(IntSize(style->textShadow()->x, style->textShadow()->y),
+                           style->textShadow()->blur, style->textShadow()->color);
         setShadow = true;
     }
 
     const String& str = m_str;
-    TextStyle textStyle(0, 0, 0, false, _style->visuallyOrdered());
-    p->drawText(TextRun(str.impl()), IntPoint(m_x + _tx, m_y + _ty + m_baseline), textStyle);
+    TextStyle textStyle(0, 0, 0, false, style->visuallyOrdered());
+    context->drawText(TextRun(str.impl()), IntPoint(m_x + tx, m_y + ty + m_baseline), textStyle);
 
     if (setShadow)
-        p->clearShadow();
+        context->clearShadow();
 
     if (m_markupBox) {
         // Paint the markup box
-        _tx += m_x + m_width - m_markupBox->xPos();
-        _ty += m_y + m_baseline - (m_markupBox->yPos() + m_markupBox->baseline());
-        m_markupBox->paint(i, _tx, _ty);
+        tx += m_x + m_width - m_markupBox->xPos();
+        ty += m_y + m_baseline - (m_markupBox->yPos() + m_markupBox->baseline());
+        m_markupBox->paint(paintInfo, tx, ty);
     }
 }
 

WebCore/rendering/EllipsisBox.h
@@ -19,8 +19,8 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#ifndef EllipsisBox_H
-#define EllipsisBox_H
+#ifndef EllipsisBox_h
+#define EllipsisBox_h
 
 #include "InlineBox.h"
 
@@ -39,9 +39,9 @@ public:
         , m_markupBox(markupBox)
     {
     }
-    
-    virtual void paint(RenderObject::PaintInfo&, int _tx, int _ty);
-    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int _x, int _y, int _tx, int _ty);
+
+    virtual void paint(RenderObject::PaintInfo&, int tx, int ty);
+    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty);
 
 private:
     AtomicString m_str;
@@ -50,4 +50,4 @@ private:
 
 } // namespace WebCore
 
-#endif // EllipsisBox_H
+#endif // EllipsisBox_h

WebCore/rendering/InlineBox.cpp
@@ -125,17 +125,17 @@ void InlineBox::adjustPosition(int dx, int dy)
         m_object->setPos(m_object->xPos() + dx, m_object->yPos() + dy);
 }
 
-void InlineBox::paint(RenderObject::PaintInfo& i, int tx, int ty)
+void InlineBox::paint(RenderObject::PaintInfo& paintInfo, int tx, int ty)
 {
-    if (!object()->shouldPaintWithinRoot(i) || (i.phase != PaintPhaseForeground && i.phase != PaintPhaseSelection))
+    if (!object()->shouldPaintWithinRoot(paintInfo) || (paintInfo.phase != PaintPhaseForeground && paintInfo.phase != PaintPhaseSelection))
         return;
 
     // Paint all phases of replaced elements atomically, as though the replaced element established its
     // own stacking context.  (See Appendix E.2, section 6.4 on inline block/table elements in the CSS2.1
     // specification.)
-    bool paintSelectionOnly = i.phase == PaintPhaseSelection;
-    RenderObject::PaintInfo info(i);
-    info.phase = paintSelectionOnly ? i.phase : PaintPhaseBlockBackground;
+    bool paintSelectionOnly = paintInfo.phase == PaintPhaseSelection;
+    RenderObject::PaintInfo info(paintInfo);
+    info.phase = paintSelectionOnly ? paintInfo.phase : PaintPhaseBlockBackground;
     object()->paint(info, tx, ty);
     if (!paintSelectionOnly) {
         info.phase = PaintPhaseChildBlockBackgrounds;

WebCore/rendering/InlineFlowBox.cpp
@@ -543,32 +543,30 @@ bool InlineFlowBox::nodeAtPoint(const HitTestRequest& request, HitTestResult& re
     return false;
 }
 
-void InlineFlowBox::paint(RenderObject::PaintInfo& i, int tx, int ty)
+void InlineFlowBox::paint(RenderObject::PaintInfo& paintInfo, int tx, int ty)
 {
-    int xPos = tx + m_x - object()->maximalOutlineSize(i.phase);
-    int w = width() + 2 * object()->maximalOutlineSize(i.phase);
-    bool intersectsDamageRect = xPos < i.r.right() && xPos + w > i.r.x();
-    
-    if (intersectsDamageRect && i.phase != PaintPhaseChildOutlines) {
-        if (i.phase == PaintPhaseOutline || i.phase == PaintPhaseSelfOutline) {
+    int xPos = tx + m_x - object()->maximalOutlineSize(paintInfo.phase);
+    int w = width() + 2 * object()->maximalOutlineSize(paintInfo.phase);
+    bool intersectsDamageRect = xPos < paintInfo.rect.right() && xPos + w > paintInfo.rect.x();
+
+    if (intersectsDamageRect && paintInfo.phase != PaintPhaseChildOutlines) {
+        if (paintInfo.phase == PaintPhaseOutline || paintInfo.phase == PaintPhaseSelfOutline) {
             // Add ourselves to the paint info struct's list of inlines that need to paint their
             // outlines.
             if (object()->style()->visibility() == VISIBLE && object()->hasOutline() &&
-                !object()->isInlineContinuation() && !isRootInlineBox()) {
-                i.outlineObjects->add(flowObject());
-            }
-        }
-        else {
+                    !object()->isInlineContinuation() && !isRootInlineBox())
+                paintInfo.outlineObjects->add(flowObject());
+        } else {
             // 1. Paint our background and border.
-            paintBackgroundAndBorder(i, tx, ty);
-            
+            paintBackgroundAndBorder(paintInfo, tx, ty);
+
             // 2. Paint our underline and overline.
-            paintDecorations(i, tx, ty, false);
+            paintDecorations(paintInfo, tx, ty, false);
         }
     }
 
-    PaintPhase paintPhase = i.phase == PaintPhaseChildOutlines ? PaintPhaseOutline : i.phase;
-    RenderObject::PaintInfo childInfo(i);
+    PaintPhase paintPhase = paintInfo.phase == PaintPhaseChildOutlines ? PaintPhaseOutline : paintInfo.phase;
+    RenderObject::PaintInfo childInfo(paintInfo);
     childInfo.phase = paintPhase;
 
     // 3. Paint our children.
@@ -580,8 +578,8 @@ void InlineFlowBox::paint(RenderObject::PaintInfo& i, int tx, int ty)
     }
 
     // 4. Paint our strike-through
-    if (intersectsDamageRect && (i.phase == PaintPhaseForeground || i.phase == PaintPhaseSelection))
-        paintDecorations(i, tx, ty, true);
+    if (intersectsDamageRect && (paintInfo.phase == PaintPhaseForeground || paintInfo.phase == PaintPhaseSelection))
+        paintDecorations(paintInfo, tx, ty, true);
 }
 
 void InlineFlowBox::paintBackgrounds(GraphicsContext* p, const Color& c, const BackgroundLayer* bgLayer,
@@ -593,13 +591,13 @@ void InlineFlowBox::paintBackgrounds(GraphicsContext* p, const Color& c, const B
     paintBackground(p, c, bgLayer, my, mh, _tx, _ty, w, h);
 }
 
-void InlineFlowBox::paintBackground(GraphicsContext* p, const Color& c, const BackgroundLayer* bgLayer,
-                                    int my, int mh, int _tx, int _ty, int w, int h)
+void InlineFlowBox::paintBackground(GraphicsContext* context, const Color& c, const BackgroundLayer* bgLayer,
+                                    int my, int mh, int tx, int ty, int w, int h)
 {
     CachedImage* bg = bgLayer->backgroundImage();
     bool hasBackgroundImage = bg && bg->canRender();
     if (!hasBackgroundImage || (!prevLineBox() && !nextLineBox()) || !parent())
-        object()->paintBackgroundExtended(p, c, bgLayer, my, mh, _tx, _ty, w, h, 
+        object()->paintBackgroundExtended(context, c, bgLayer, my, mh, tx, ty, w, h, 
                                           borderLeft(), borderRight(), paddingLeft(), paddingRight());
     else {
         // We have a background image that spans multiple lines.
@@ -613,47 +611,47 @@ void InlineFlowBox::paintBackground(GraphicsContext* p, const Color& c, const Ba
         int xOffsetOnLine = 0;
         for (InlineRunBox* curr = prevLineBox(); curr; curr = curr->prevLineBox())
             xOffsetOnLine += curr->width();
-        int startX = _tx - xOffsetOnLine;
+        int startX = tx - xOffsetOnLine;
         int totalWidth = xOffsetOnLine;
         for (InlineRunBox* curr = this; curr; curr = curr->nextLineBox())
             totalWidth += curr->width();
-        p->save();
-        p->clip(IntRect(_tx, _ty, width(), height()));
-        object()->paintBackgroundExtended(p, c, bgLayer, my, mh, startX, _ty,
+        context->save();
+        context->clip(IntRect(tx, ty, width(), height()));
+        object()->paintBackgroundExtended(context, c, bgLayer, my, mh, startX, ty,
                                           totalWidth, h, borderLeft(), borderRight(), paddingLeft(), paddingRight());
-        p->restore();
+        context->restore();
     }
 }
 
-void InlineFlowBox::paintBackgroundAndBorder(RenderObject::PaintInfo& i, int _tx, int _ty)
+void InlineFlowBox::paintBackgroundAndBorder(RenderObject::PaintInfo& paintInfo, int tx, int ty)
 {
-    if (!object()->shouldPaintWithinRoot(i) || object()->style()->visibility() != VISIBLE ||
-        i.phase != PaintPhaseForeground)
+    if (!object()->shouldPaintWithinRoot(paintInfo) || object()->style()->visibility() != VISIBLE ||
+            paintInfo.phase != PaintPhaseForeground)
         return;
 
     // Move x/y to our coordinates.
-    _tx += m_x;
-    _ty += m_y;
+    tx += m_x;
+    ty += m_y;
     
     int w = width();
     int h = height();
 
-    int my = max(_ty, i.r.y());
+    int my = max(ty, paintInfo.rect.y());
     int mh;
-    if (_ty < i.r.y())
-        mh = max(0, h - (i.r.y() - _ty));
+    if (ty < paintInfo.rect.y())
+        mh = max(0, h - (paintInfo.rect.y() - ty));
     else
-        mh = min(i.r.height(), h);
+        mh = min(paintInfo.rect.height(), h);
 
-    GraphicsContext* p = i.p;
+    GraphicsContext* context = paintInfo.context;
     
     // You can use p::first-line to specify a background. If so, the root line boxes for
     // a line may actually have to paint a background.
     RenderStyle* styleToUse = object()->style(m_firstLine);
     if ((!parent() && m_firstLine && styleToUse != object()->style()) || 
-        (parent() && object()->shouldPaintBackgroundOrBorder())) {
+            (parent() && object()->shouldPaintBackgroundOrBorder())) {
         Color c = styleToUse->backgroundColor();
-        paintBackgrounds(p, c, styleToUse->backgroundLayers(), my, mh, _tx, _ty, w, h);
+        paintBackgrounds(context, c, styleToUse->backgroundLayers(), my, mh, tx, ty, w, h);
 
         // :first-line cannot be used to put borders on a line. Always paint borders with our
         // non-first-line style.
@@ -662,11 +660,11 @@ void InlineFlowBox::paintBackgroundAndBorder(RenderObject::PaintInfo& i, int _tx
             bool hasBorderImage = borderImage && borderImage->canRender();
             if (hasBorderImage && !borderImage->isLoaded())
                 return; // Don't paint anything while we wait for the image to load.
-            
+
             // The simple case is where we either have no border image or we are the only box for this object.  In those
             // cases only a single call to draw is required.
             if (!hasBorderImage || (!prevLineBox() && !nextLineBox()))
-                object()->paintBorder(p, _tx, _ty, w, h, object()->style(), includeLeftEdge(), includeRightEdge());
+                object()->paintBorder(context, tx, ty, w, h, object()->style(), includeLeftEdge(), includeRightEdge());
             else {
                 // We have a border image that spans multiple lines.
                 // We need to adjust _tx and _ty by the width of all previous lines.
@@ -679,14 +677,14 @@ void InlineFlowBox::paintBackgroundAndBorder(RenderObject::PaintInfo& i, int _tx
                 int xOffsetOnLine = 0;
                 for (InlineRunBox* curr = prevLineBox(); curr; curr = curr->prevLineBox())
                     xOffsetOnLine += curr->width();
-                int startX = _tx - xOffsetOnLine;
+                int startX = tx - xOffsetOnLine;
                 int totalWidth = xOffsetOnLine;
                 for (InlineRunBox* curr = this; curr; curr = curr->nextLineBox())
                     totalWidth += curr->width();
-                p->save();
-                p->clip(IntRect(_tx, _ty, width(), height()));
-                object()->paintBorder(p, startX, _ty, totalWidth, h, object()->style());
-                p->restore();
+                context->save();
+                context->clip(IntRect(tx, ty, width(), height()));
+                object()->paintBorder(context, startX, ty, totalWidth, h, object()->style());
+                context->restore();
             }
         }
     }
@@ -712,21 +710,21 @@ static bool shouldDrawDecoration(RenderObject* obj)
     return false;
 }
 
-void InlineFlowBox::paintDecorations(RenderObject::PaintInfo& i, int _tx, int _ty, bool paintedChildren)
+void InlineFlowBox::paintDecorations(RenderObject::PaintInfo& paintInfo, int tx, int ty, bool paintedChildren)
 {
     // Paint text decorations like underlines/overlines. We only do this if we aren't in quirks mode (i.e., in
     // almost-strict mode or strict mode).
-    if (object()->style()->htmlHacks() || !object()->shouldPaintWithinRoot(i) ||
-        object()->style()->visibility() != VISIBLE)
+    if (object()->style()->htmlHacks() || !object()->shouldPaintWithinRoot(paintInfo) ||
+            object()->style()->visibility() != VISIBLE)
         return;
     
     // We don't want underlines or other decorations when we're trying to draw nothing but the selection as white text.
-    if (i.phase == PaintPhaseSelection && i.forceWhiteText)
+    if (paintInfo.phase == PaintPhaseSelection && paintInfo.forceWhiteText)
         return;
 
-    GraphicsContext* p = i.p;
-    _tx += m_x;
-    _ty += m_y;
+    GraphicsContext* context = paintInfo.context;
+    tx += m_x;
+    ty += m_y;
     RenderStyle* styleToUse = object()->style(m_firstLine);
     int deco = parent() ? styleToUse->textDecoration() : styleToUse->textDecorationsInEffect();
     if (deco != TDNONE && 
@@ -743,51 +741,50 @@ void InlineFlowBox::paintDecorations(RenderObject::PaintInfo& i, int _tx, int _t
             if (rootLine == this) {
                 if (x + w >= ellipsisX + ellipsisWidth)
                     w -= (x + w - ellipsisX - ellipsisWidth);
-            }
-            else {
+            } else {
                 if (x >= ellipsisX)
                     return;
                 if (x + w >= ellipsisX)
                     w -= (x + w - ellipsisX);
             }
         }
-            
+
         // Set up the appropriate text-shadow effect for the decoration.
         // FIXME: Support multiple shadow effects.  Need more from the CG API before we can do this.
         bool setShadow = false;
         if (styleToUse->textShadow()) {
-            p->setShadow(IntSize(styleToUse->textShadow()->x, styleToUse->textShadow()->y),
-                         styleToUse->textShadow()->blur, styleToUse->textShadow()->color);
+            context->setShadow(IntSize(styleToUse->textShadow()->x, styleToUse->textShadow()->y),
+                               styleToUse->textShadow()->blur, styleToUse->textShadow()->color);
             setShadow = true;
         }
-        
+
         // We must have child boxes and have decorations defined.
-        _tx += borderLeft() + paddingLeft();
-        
+        tx += borderLeft() + paddingLeft();
+
         Color underline, overline, linethrough;
         underline = overline = linethrough = styleToUse->color();
         if (!parent())
             object()->getTextDecorationColors(deco, underline, overline, linethrough);
 
-        if (styleToUse->font() != p->font())
-            p->setFont(styleToUse->font());
+        if (styleToUse->font() != context->font())
+            context->setFont(styleToUse->font());
 
         bool isPrinting = object()->document()->printing();
         if (deco & UNDERLINE && !paintedChildren) {
-            p->setPen(underline);
-            p->drawLineForText(IntPoint(_tx, _ty), m_baseline, w, isPrinting);
+            context->setPen(underline);
+            context->drawLineForText(IntPoint(tx, ty), m_baseline, w, isPrinting);
         }
         if (deco & OVERLINE && !paintedChildren) {
-            p->setPen(overline);
-            p->drawLineForText(IntPoint(_tx, _ty), 0, w, isPrinting);
+            context->setPen(overline);
+            context->drawLineForText(IntPoint(tx, ty), 0, w, isPrinting);
         }
         if (deco & LINE_THROUGH && paintedChildren) {
-            p->setPen(linethrough);
-            p->drawLineForText(IntPoint(_tx, _ty), 2*m_baseline/3, w, isPrinting);
+            context->setPen(linethrough);
+            context->drawLineForText(IntPoint(tx, ty), 2 * m_baseline / 3, w, isPrinting);
         }
 
         if (setShadow)
-            p->clearShadow();
+            context->clearShadow();
     }
 }
 

WebCore/rendering/InlineFlowBox.h
@@ -74,14 +74,14 @@ public:
 
     virtual void clearTruncation();
 
-    virtual void paintBackgroundAndBorder(RenderObject::PaintInfo& i, int _tx, int _ty);
-    void paintBackgrounds(GraphicsContext* p, const Color& c, const BackgroundLayer* bgLayer,
-                          int my, int mh, int _tx, int _ty, int w, int h);
-    void paintBackground(GraphicsContext* p, const Color& c, const BackgroundLayer* bgLayer,
-                         int my, int mh, int _tx, int _ty, int w, int h);
-    virtual void paintDecorations(RenderObject::PaintInfo& i, int _tx, int _ty, bool paintedChildren = false);
-    virtual void paint(RenderObject::PaintInfo& i, int _tx, int _ty);
-    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int, int, int, int);
+    virtual void paintBackgroundAndBorder(RenderObject::PaintInfo&, int tx, int ty);
+    void paintBackgrounds(GraphicsContext*, const Color&, const BackgroundLayer*,
+                          int my, int mh, int tx, int ty, int w, int h);
+    void paintBackground(GraphicsContext*, const Color&, const BackgroundLayer*,
+                         int my, int mh, int tx, int ty, int w, int h);
+    virtual void paintDecorations(RenderObject::PaintInfo&, int tx, int ty, bool paintedChildren = false);
+    virtual void paint(RenderObject::PaintInfo&, int tx, int ty);
+    virtual bool nodeAtPoint(const HitTestRequest&, HitTestResult&, int x, int y, int tx, int ty);
 
     int marginBorderPaddingLeft();
     int marginBorderPaddingRight();

WebCore/rendering/InlineRunBox.h
@@ -20,8 +20,8 @@
  *
  */
 
-#ifndef InlineRunBox_H
-#define InlineRunBox_H
+#ifndef InlineRunBox_h
+#define InlineRunBox_h
 
 #include "InlineBox.h"
 
@@ -51,4 +51,4 @@ protected:
 
 } // namespace WebCore
 
-#endif // InlineRunBox_H
+#endif // InlineRunBox_h

commit 0c7184016261e91684282ee75c46c37902d98060

Reviewed by Adele.
        - Fix a regression from r17521: painting of and crash caused by tables
          with collapsed borders

        * rendering/RenderTable.cpp:
        (WebCore::RenderTable::paint): Changed 'paintInfo' to 'info'. Prior to
        r17521, 'paintInfo' was the local variable. Now 'info' is the local variable
        and 'paintInfo' is the parameter, which we were accidently modifying.



git-svn-id: svn://svn.chromium.org/blink/trunk@17526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/rendering/RenderTable.cpp
@@ -416,7 +416,7 @@ void RenderTable::paint(PaintInfo& paintInfo, int tx, int ty)
         // Collect all the unique border styles that we want to paint in a sorted list.  Once we
         // have all the styles sorted, we then do individual passes, painting each style of border
         // from lowest precedence to highest precedence.
-        paintInfo.phase = PaintPhaseCollapsedTableBorders;
+        info.phase = PaintPhaseCollapsedTableBorders;
         DeprecatedValueList<CollapsedBorderValue> borderStyles;
         collectBorders(borderStyles);
         DeprecatedValueListIterator<CollapsedBorderValue> it = borderStyles.begin();