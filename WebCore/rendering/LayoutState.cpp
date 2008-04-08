primiano/blink-gitcs

commit b1d835c52fbb21d3cc0a4727a5e6f0b46bd1cdf1

WebCore:
        Reviewed by Darin Adler.

        - fix http://bugs.webkit.org/show_bug.cgi?id=17994
          REGRESSION (523-525+) View not updated when changing CSS size via JavaScript
        - fix http://bugs.webkit.org/show_bug.cgi?id=18063
          REGRESSION (r31250): Small bug that just cropped up in WebKit r31275

        Tests: fast/repaint/subtree-root-clip.html
               fast/repaint/subtree-root-clip-2.html
               fast/repaint/subtree-root-clip-3.html

        * rendering/LayoutState.cpp:
        (WebCore::LayoutState::LayoutState): Changed to not set an initial clip.
        Since the root is clipping, there is no need to set a clip, and setting
        the clip based on the container was wrong because often the root was
        not included in the container's overflow rect because it had its own
        layer.
        * rendering/RenderBox.cpp:
        (WebCore::RenderBox::absolutePosition): Removed code that added table
        cell's extra top offset only in the slow (recursive) code path.
        * rendering/RenderTableCell.cpp:
        (WebCore::RenderTableCell::absolutePosition): Added code to add the
        extra top offset in both code paths.

LayoutTests:

        Reviewed by Darin Adler.

        - tests for http://bugs.webkit.org/show_bug.cgi?id=17994
          REGRESSION (523-525+) View not updated when changing CSS size via JavaScript
        - and http://bugs.webkit.org/show_bug.cgi?id=18063
          REGRESSION (r31250): Small bug that just cropped up in WebKit r31275

        * fast/repaint/subtree-root-clip-2.html: Added.
        * fast/repaint/subtree-root-clip-3.html: Added.
        * fast/repaint/subtree-root-clip.html: Added.
        * platform/mac/fast/repaint/subtree-root-clip-2-expected.checksum: Added.
        * platform/mac/fast/repaint/subtree-root-clip-2-expected.png: Added.
        * platform/mac/fast/repaint/subtree-root-clip-2-expected.txt: Added.

WebCore/rendering/LayoutState.cpp
@@ -81,10 +81,9 @@ LayoutState::LayoutState(LayoutState* prev, RenderBox* renderer, const IntSize& 
 }
 
 LayoutState::LayoutState(RenderObject* root)
+    : m_clipped(false)
 {
     RenderObject* container = root->container();
-    m_clipped = true;
-    m_clipRect = container->absoluteClippedOverflowRect();
     int x = 0;
     int y = 0;
     container->absolutePosition(x, y);

WebCore/rendering/RenderBox.cpp
@@ -884,8 +884,6 @@ bool RenderBox::absolutePosition(int& xPos, int& yPos, bool fixed) const
 
     RenderObject* o = container();
     if (o && o->absolutePosition(xPos, yPos, fixed)) {
-        yPos += o->borderTopExtra();
-
         if (style()->position() == AbsolutePosition) {
             IntSize offset = offsetForPositionedInContainer(o);
             xPos += offset.width();

WebCore/rendering/RenderTableCell.cpp
@@ -199,6 +199,7 @@ void RenderTableCell::computeAbsoluteRepaintRect(IntRect& r, bool fixed)
 bool RenderTableCell::absolutePosition(int& xPos, int& yPos, bool fixed) const
 {
     bool result = RenderBlock::absolutePosition(xPos, yPos, fixed);
+    yPos += m_topExtra;
     RenderView* v = view();
     if (!v || !v->layoutState()) {
         xPos -= parent()->xPos(); // Rows are in the same coordinate space, so don't add their offset in.

LayoutTests/fast/repaint/subtree-root-clip-2.html
@@ -1,0 +1,21 @@
+<html>
+<head>
+    <script src="repaint.js"></script>
+    <script>
+        function repaintTest()
+        {
+            document.getElementById("target").style.width = "100px";
+        }
+    </script>
+</head>
+<body onload="runRepaintTest()">
+<p>
+    <span style="-position: relative; top: 25px; left: 25px;">
+        <div style="float: left; height: 100px; width: 100px; overflow: hidden; background-color: red;">
+            <div id="target" style="height: 100px; width: 0; background-color: green;">
+            </div>
+        </div>
+    </span>
+</p>
+</body>
+</html>

LayoutTests/fast/repaint/subtree-root-clip-3.html
@@ -1,0 +1,23 @@
+<html>
+<head>
+    <script src="repaint.js"></script>
+    <script>
+        function repaintTest()
+        {
+            document.getElementById("target").style.width = "100px";
+        }
+    </script>
+</head>
+<body onload="runRepaintTest()">
+<table>
+    <tr>
+        <td style="height: 200px;"></td>
+        <td>
+            <div style="height: 100px; width: 100px; overflow: hidden; background-color: red;">
+                <div id="target" style="height: 100px; width: 0; background-color: green;"></div>
+            </div>
+        </td>
+    </tr>
+</table>
+</body>
+</html>

LayoutTests/fast/repaint/subtree-root-clip.html
@@ -1,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <script src="repaint.js"></script>
+    <script>
+        function repaintTest()
+        {
+            document.getElementById("inner").style.width = "0";
+        }
+    </script>
+</head>
+<body onload="runRepaintTest()">
+<div style="float: left; overflow: hidden; width: 100px; height: 100px; background-color: green;">
+    <div id="inner" style="width: 100px; height: 100px; background-color: red;"></div>
+</div>
+</body>
+</html>

LayoutTests/platform/mac/fast/repaint/subtree-root-clip-2-expected.checksum
@@ -1,0 +1,1 @@
+e1a695f3d1889abdff11e18becb6e3f0

LayoutTests/platform/mac/fast/repaint/subtree-root-clip-2-expected.png.gitcs
@@ -1,0 +1,1 @@
+src gs://blink-gitcs/3ab9f1acad1d112495e25245605414db504f1b03.blob

commit 16e7bbcd4b0a7ad0b1fa49e47297d3310cf1b0fa

Reviewed by Antti Koivisto.
        - fix a regression from r31313 which made the inspector highlight and
          yellow find highlight for table cells with extra top appear in the
          wrong position

        * rendering/LayoutState.cpp:
        (WebCore::LayoutState::LayoutState): Changed absolutePosition() to
        absolutePositionForContent(), to get the right position for children of
        table cells. This is the correct fix for this part of r31313.
        * rendering/RenderBox.cpp:
        (WebCore::RenderBox::absolutePosition): Reverted the change from r31313
        by changing absolutePosition() to absolutePositionForContent() here too.
        * rendering/RenderObject.h:
        (WebCore::RenderObject::absolutePositionForContent): Changed to return
        the result from absolutePosition().
        * rendering/RenderTableCell.cpp:
        (WebCore::RenderTableCell::absoluteClippedOverflowRect): Turned the
        "else" case into an early return.
        (WebCore::RenderTableCell::absolutePosition): Reverted the change from
        r31313.



git-svn-id: svn://svn.chromium.org/blink/trunk@31735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/rendering/LayoutState.cpp
@@ -86,7 +86,7 @@ LayoutState::LayoutState(RenderObject* root)
     RenderObject* container = root->container();
     int x = 0;
     int y = 0;
-    container->absolutePosition(x, y);
+    container->absolutePositionForContent(x, y);
     m_offset = IntSize(x, y);
     m_next = 0;
 }

WebCore/rendering/RenderBox.cpp
@@ -883,7 +883,7 @@ bool RenderBox::absolutePosition(int& xPos, int& yPos, bool fixed) const
         fixed = true;
 
     RenderObject* o = container();
-    if (o && o->absolutePosition(xPos, yPos, fixed)) {
+    if (o && o->absolutePositionForContent(xPos, yPos, fixed)) {
         if (style()->position() == AbsolutePosition) {
             IntSize offset = offsetForPositionedInContainer(o);
             xPos += offset.width();

WebCore/rendering/RenderObject.h
@@ -578,10 +578,11 @@ public:
     // This function is used to deal with the extra top space that can occur in table cells (called borderTopExtra).
     // The children of the cell do not factor this space in, so we have to add it in.  Any code that wants to
     // accurately deal with the contents of a cell must call this function instad of absolutePosition.
-    void absolutePositionForContent(int& xPos, int& yPos, bool fixed = false) const
+    bool absolutePositionForContent(int& xPos, int& yPos, bool fixed = false) const
     {
-        absolutePosition(xPos, yPos, fixed);
+        bool result = absolutePosition(xPos, yPos, fixed);
         yPos += borderTopExtra();
+        return result;
     }
 
     // width and height are without margins but include paddings and borders

WebCore/rendering/RenderTableCell.cpp
@@ -143,48 +143,48 @@ IntRect RenderTableCell::absoluteClippedOverflowRect()
     // so we ignore outside borders. This should not be a problem because it means that
     // the table is going to recalculate the grid, relayout and repaint its current rect, which
     // includes any outside borders of this cell.
-    if (table()->collapseBorders() && !table()->needsSectionRecalc()) {
-        bool rtl = table()->style()->direction() == RTL;
-        int outlineSize = style()->outlineSize();
-        int left = max(borderHalfLeft(true), outlineSize);
-        int right = max(borderHalfRight(true), outlineSize);
-        int top = max(borderHalfTop(true), outlineSize);
-        int bottom = max(borderHalfBottom(true), outlineSize);
-        if (left && !rtl || right && rtl) {
-            if (RenderTableCell* before = table()->cellBefore(this)) {
-                top = max(top, before->borderHalfTop(true));
-                bottom = max(bottom, before->borderHalfBottom(true));
-            }
-        }
-        if (left && rtl || right && !rtl) {
-            if (RenderTableCell* after = table()->cellAfter(this)) {
-                top = max(top, after->borderHalfTop(true));
-                bottom = max(bottom, after->borderHalfBottom(true));
-            }
-        }
-        if (top) {
-            if (RenderTableCell* above = table()->cellAbove(this)) {
-                left = max(left, above->borderHalfLeft(true));
-                right = max(right, above->borderHalfRight(true));
-            }
-        }
-        if (bottom) {
-            if (RenderTableCell* below = table()->cellBelow(this)) {
-                left = max(left, below->borderHalfLeft(true));
-                right = max(right, below->borderHalfRight(true));
-            }
-        }
-        left = max(left, -overflowLeft(false));
-        top = max(top, -overflowTop(false) - borderTopExtra());
-        IntRect r(-left, -borderTopExtra() - top, left + max(width() + right, overflowWidth(false)), borderTopExtra() + top + max(height() + bottom + borderBottomExtra(), overflowHeight(false)));
+    if (!table()->collapseBorders() || table()->needsSectionRecalc())
+        return RenderBlock::absoluteClippedOverflowRect();
 
-        if (RenderView* v = view())
-            r.move(v->layoutDelta());
-
-        computeAbsoluteRepaintRect(r);
-        return r;
+    bool rtl = table()->style()->direction() == RTL;
+    int outlineSize = style()->outlineSize();
+    int left = max(borderHalfLeft(true), outlineSize);
+    int right = max(borderHalfRight(true), outlineSize);
+    int top = max(borderHalfTop(true), outlineSize);
+    int bottom = max(borderHalfBottom(true), outlineSize);
+    if (left && !rtl || right && rtl) {
+        if (RenderTableCell* before = table()->cellBefore(this)) {
+            top = max(top, before->borderHalfTop(true));
+            bottom = max(bottom, before->borderHalfBottom(true));
+        }
     }
-    return RenderBlock::absoluteClippedOverflowRect();
+    if (left && rtl || right && !rtl) {
+        if (RenderTableCell* after = table()->cellAfter(this)) {
+            top = max(top, after->borderHalfTop(true));
+            bottom = max(bottom, after->borderHalfBottom(true));
+        }
+    }
+    if (top) {
+        if (RenderTableCell* above = table()->cellAbove(this)) {
+            left = max(left, above->borderHalfLeft(true));
+            right = max(right, above->borderHalfRight(true));
+        }
+    }
+    if (bottom) {
+        if (RenderTableCell* below = table()->cellBelow(this)) {
+            left = max(left, below->borderHalfLeft(true));
+            right = max(right, below->borderHalfRight(true));
+        }
+    }
+    left = max(left, -overflowLeft(false));
+    top = max(top, -overflowTop(false) - borderTopExtra());
+    IntRect r(-left, -borderTopExtra() - top, left + max(width() + right, overflowWidth(false)), borderTopExtra() + top + max(height() + bottom + borderBottomExtra(), overflowHeight(false)));
+
+    if (RenderView* v = view())
+        r.move(v->layoutDelta());
+
+    computeAbsoluteRepaintRect(r);
+    return r;
 }
 
 void RenderTableCell::computeAbsoluteRepaintRect(IntRect& r, bool fixed)
@@ -199,7 +199,6 @@ void RenderTableCell::computeAbsoluteRepaintRect(IntRect& r, bool fixed)
 bool RenderTableCell::absolutePosition(int& xPos, int& yPos, bool fixed) const
 {
     bool result = RenderBlock::absolutePosition(xPos, yPos, fixed);
-    yPos += m_topExtra;
     RenderView* v = view();
     if (!v || !v->layoutState()) {
         xPos -= parent()->xPos(); // Rows are in the same coordinate space, so don't add their offset in.