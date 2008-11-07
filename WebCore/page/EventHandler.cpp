primiano/blink-gitcs

commit 4d65a985dc7381d795a09e862efda00843d51372

2008-04-22  Jon Honeycutt  <jhoneycutt@apple.com>
        Reviewed by Sam.

        Add a method to query which modifier keys the platform uses for access
        key actions.

        * page/EventHandler.cpp:
        (WebCore::EventHandler::handleAccessKey): Bitwise and the event's
        modifier keys with the platform's access key modifier mask to determine
        whether this event can perform an access key action.
        * page/EventHandler.h: Added s_accessKeyModifiers, a platform-defined
        mask of modifier keys used for access key actions, and a getter.
        * page/mac/EventHandlerMac.mm: Use PlatformKeyboardEvent::CtrlKey for
        access key actions.
        * page/qt/EventHandlerQt.cpp: Same.
        * page/gtk/EventHandlerGtk.cpp: Use AltKey.
        * page/win/EventHandlerWin.cpp: Same.
        * page/wx/EventHandlerWx.cpp: Same.
        * platform/PlatformKeyboardEvent.h: Added an enum, ModifierKey.
        (WebCore::PlatformKeyboardEvent::):
        (WebCore::PlatformKeyboardEvent::modifiers): Return which modifier keys
        were active, a combination of ModifierKey values.


git-svn-id: svn://svn.chromium.org/blink/trunk@32424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/page/EventHandler.cpp
@@ -1475,21 +1475,14 @@ static EventTargetNode* eventTargetNodeForDocument(Document* doc)
 
 bool EventHandler::handleAccessKey(const PlatformKeyboardEvent& evt)
 {
-#if PLATFORM(MAC) || PLATFORM(QT)
-    if (evt.ctrlKey())
-#else
-    if (evt.altKey())
-#endif
-    {
-        String key = evt.unmodifiedText();
-        Element* elem = m_frame->document()->getElementByAccessKey(key.lower());
-        if (elem) {
-            elem->accessKeyAction(false);
-            return true;
-        }
-    }
-
-    return false;
+    if ((evt.modifiers() & s_accessKeyModifiers) != s_accessKeyModifiers)
+        return false;
+    String key = evt.unmodifiedText();
+    Element* elem = m_frame->document()->getElementByAccessKey(key.lower());
+    if (!elem)
+        return false;
+    elem->accessKeyAction(false);
+    return true;
 }
 
 #if !PLATFORM(MAC)

WebCore/page/EventHandler.h
@@ -136,6 +136,7 @@ public:
 
     bool needsKeyboardEventDisambiguationQuirks() const;
 
+    static unsigned accessKeyModifiers() { return s_accessKeyModifiers; }
     bool handleAccessKey(const PlatformKeyboardEvent&);
     bool keyEvent(const PlatformKeyboardEvent&);
     void defaultKeyboardEventHandler(KeyboardEvent*);
@@ -311,6 +312,8 @@ private:
     IntPoint m_mouseDownPos; // in our view's coords
     double m_mouseDownTimestamp;
     PlatformMouseEvent m_mouseDown;
+
+    static unsigned s_accessKeyModifiers;
 
 #if PLATFORM(MAC)
     NSView *m_mouseDownView;

WebCore/page/gtk/EventHandlerGtk.cpp
@@ -36,6 +36,7 @@
 #include "MouseEventWithHitTestResults.h"
 #include "NotImplemented.h"
 #include "Page.h"
+#include "PlatformKeyboardEvent.h"
 #include "PlatformScrollBar.h"
 #include "PlatformWheelEvent.h"
 #include "RenderWidget.h"
@@ -43,6 +44,8 @@
 namespace WebCore {
 
 using namespace EventNames;
+
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
 
 bool EventHandler::tabsToAllControls(KeyboardEvent* event) const
 {

WebCore/page/mac/EventHandlerMac.mm
@@ -47,6 +47,8 @@ namespace WebCore {
 
 using namespace EventNames;
 
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey;
+
 static RetainPtr<NSEvent>& currentEvent()
 {
     static RetainPtr<NSEvent> event;

WebCore/page/qt/EventHandlerQt.cpp
@@ -54,6 +54,8 @@ namespace WebCore {
 
 using namespace EventNames;
 
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey;
+
 static bool isKeyboardOptionTab(KeyboardEvent* event)
 {
     return event

WebCore/page/win/EventHandlerWin.cpp
@@ -36,6 +36,7 @@
 #include "HitTestResult.h"
 #include "MouseEventWithHitTestResults.h"
 #include "Page.h"
+#include "PlatformKeyboardEvent.h"
 #include "PlatformScrollbar.h"
 #include "PlatformWheelEvent.h"
 #include "SelectionController.h"
@@ -43,6 +44,8 @@
 #include "NotImplemented.h"
 
 namespace WebCore {
+
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {

WebCore/page/wx/EventHandlerWx.cpp
@@ -33,10 +33,13 @@
 #include "KeyboardEvent.h"
 #include "MouseEventWithHitTestResults.h"
 #include "Page.h"
+#include "PlatformKeyboardEvent.h"
 #include "PlatformScrollBar.h"
 #include "RenderWidget.h"
 
 namespace WebCore {
+
+unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {

WebCore/platform/PlatformKeyboardEvent.h
@@ -77,6 +77,13 @@ namespace WebCore {
             Char
         };
 
+        enum ModifierKey {
+            AltKey = 1 << 0,
+            CtrlKey = 1 << 1,
+            MetaKey = 1 << 2,
+            ShiftKey = 1 << 3,
+        };
+
         Type type() const { return m_type; }
         void disambiguateKeyDownEvent(Type, bool backwardCompatibilityMode = false); // Only used on platforms that need it, i.e. those that generate KeyDown events.
 
@@ -106,6 +113,12 @@ namespace WebCore {
         bool ctrlKey() const { return m_ctrlKey; }
         bool altKey() const { return m_altKey; }
         bool metaKey() const { return m_metaKey; }
+        unsigned modifiers() const {
+            return (altKey() ? AltKey : 0)
+                | (ctrlKey() ? CtrlKey : 0)
+                | (metaKey() ? MetaKey : 0)
+                | (shiftKey() ? ShiftKey : 0);
+        }
 
         static bool currentCapsLockState();
 

commit 27da43136a46818906b37b71338f8db700157c7a

Reviewed by Darin Adler.
        https://bugs.webkit.org/show_bug.cgi?id=21107
        <rdar://problem/6264219> New access key combination conflicts with VoiceOver

        * page/EventHandler.h:
        * page/gtk/EventHandlerGtk.cpp:
        (WebCore::EventHandler::accessKeyModifiers):
        * page/qt/EventHandlerQt.cpp:
        (WebCore::EventHandler::accessKeyModifiers):
        * page/win/EventHandlerWin.cpp:
        (WebCore::EventHandler::accessKeyModifiers):
        * page/wx/EventHandlerWx.cpp:
        (WebCore::EventHandler::accessKeyModifiers):
        Access access key modifiers via a function, not a static variable.

        * page/mac/EventHandlerMac.mm: (WebCore::EventHandler::accessKeyModifiers):
        Use Ctrl when VoiceOver is enabled, because a conflict with Emacs-style key bindings is
        less troublesome than one with VO keys.

        * page/EventHandler.cpp: (WebCore::EventHandler::handleAccessKey):
        Also fix an access key matching bug introduced in r32424 - Any superset of specified
        modifier set invoked access keys. We can use simple equality comparison instead because
        CapsLock is not part of modifiers(), so it doesn't need to be ignored explicitly.



git-svn-id: svn://svn.chromium.org/blink/trunk@38211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/page/EventHandler.cpp
@@ -1722,7 +1722,7 @@ static EventTargetNode* eventTargetNodeForDocument(Document* doc)
 
 bool EventHandler::handleAccessKey(const PlatformKeyboardEvent& evt)
 {
-    if ((evt.modifiers() & s_accessKeyModifiers) != s_accessKeyModifiers)
+    if (evt.modifiers() != accessKeyModifiers())
         return false;
     String key = evt.unmodifiedText();
     Element* elem = m_frame->document()->getElementByAccessKey(key.lower());

WebCore/page/EventHandler.h
@@ -140,7 +140,7 @@ public:
 
     bool needsKeyboardEventDisambiguationQuirks() const;
 
-    static unsigned accessKeyModifiers() { return s_accessKeyModifiers; }
+    static unsigned accessKeyModifiers();
     bool handleAccessKey(const PlatformKeyboardEvent&);
     bool keyEvent(const PlatformKeyboardEvent&);
     void defaultKeyboardEventHandler(KeyboardEvent*);
@@ -334,8 +334,6 @@ private:
     IntPoint m_mouseDownPos; // in our view's coords
     double m_mouseDownTimestamp;
     PlatformMouseEvent m_mouseDown;
-
-    static unsigned s_accessKeyModifiers;
     
     unsigned m_pendingFrameUnloadEventCount;
     unsigned m_pendingFrameBeforeUnloadEventCount;

WebCore/page/gtk/EventHandlerGtk.cpp
@@ -42,8 +42,6 @@
 
 namespace WebCore {
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
-
 const double EventHandler::TextDragDelay = 0.0;
 
 bool EventHandler::tabsToAllControls(KeyboardEvent* event) const
@@ -118,6 +116,11 @@ bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults&
 {
     subframe->eventHandler()->handleMouseReleaseEvent(mev.event());
     return true;
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::AltKey;
 }
 
 }

WebCore/page/mac/EventHandlerMac.mm
@@ -26,6 +26,7 @@
 #include "config.h"
 #include "EventHandler.h"
 
+#include "AXObjectCache.h"
 #include "BlockExceptions.h"
 #include "ChromeClient.h"
 #include "ClipboardMac.h"
@@ -44,8 +45,6 @@
 #include "Settings.h"
 
 namespace WebCore {
-
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey | PlatformKeyboardEvent::AltKey;
 
 const double EventHandler::TextDragDelay = 0.15;
 
@@ -641,6 +640,17 @@ bool EventHandler::passMouseMoveEventToSubframe(MouseEventWithHitTestResults& me
 bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {
     return passSubframeEventToSubframe(mev, subframe);
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    // Control+Option key combinations are usually unused on Mac OS X, but not when VoiceOver is enabled.
+    // So, we use Control in this case, even though it conflicts with Emacs-style key bindings.
+    // See <https://bugs.webkit.org/show_bug.cgi?id=21107> for more detail.
+    if (AXObjectCache::accessibilityEnhancedUserInterfaceEnabled())
+        return PlatformKeyboardEvent::CtrlKey;
+
+    return PlatformKeyboardEvent::CtrlKey | PlatformKeyboardEvent::AltKey;
 }
 
 }

WebCore/page/qt/EventHandlerQt.cpp
@@ -56,8 +56,6 @@ QT_END_NAMESPACE
 
 namespace WebCore {
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::CtrlKey;
-
 const double EventHandler::TextDragDelay = 0.0;
 
 static bool isKeyboardOptionTab(KeyboardEvent* event)
@@ -130,6 +128,11 @@ bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults&
 {
     subframe->eventHandler()->handleMouseReleaseEvent(mev.event());
     return true;
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::CtrlKey;
 }
 
 }

WebCore/page/win/EventHandlerWin.cpp
@@ -45,8 +45,6 @@
 
 namespace WebCore {
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
-
 const double EventHandler::TextDragDelay = 0.0;
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
@@ -106,6 +104,11 @@ bool EventHandler::passWidgetMouseDownEventToWidget(const MouseEventWithHitTestR
 {
     notImplemented();
     return false;
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::AltKey;
 }
 
 }

WebCore/page/wx/EventHandlerWx.cpp
@@ -39,8 +39,6 @@
 
 namespace WebCore {
 
-unsigned EventHandler::s_accessKeyModifiers = PlatformKeyboardEvent::AltKey;
-
 const double EventHandler::TextDragDelay = 0.0;
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
@@ -89,6 +87,11 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
 PassRefPtr<Clipboard> EventHandler::createDraggingClipboard() const 
 {
     return ClipboardWx::create(ClipboardWritable, true);
+}
+
+unsigned EventHandler::accessKeyModifiers()
+{
+    return PlatformKeyboardEvent::AltKey;
 }
 
 }