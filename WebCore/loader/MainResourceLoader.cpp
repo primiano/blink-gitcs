primiano/blink-gitcs

commit aa8574ec5c97d16bf358ac3c3d436d4568533ca8

Reviewed by Brady Eidson.
        https://bugs.webkit.org/show_bug.cgi?id=47294
        <rdar://problem/8425647>Application cache isn't consulted on redirect

        Test: http/tests/appcache/main-resource-redirect.html

        * loader/MainResourceLoader.cpp:
        (WebCore::MainResourceLoader::continueAfterNavigationPolicy): If we have substitute data now
        (presumably after a redirect is served by appcache), switch to it.
        (WebCore::MainResourceLoader::willSendRequest): Check appcache for the new URL.

        * loader/MainResourceLoader.h: Made handleDataLoadSoon() take a const ResourceRequest
        reference - it doesn't ever modify it, and we can call it from continueAfterNavigationPolicy() now.



git-svn-id: svn://svn.chromium.org/blink/trunk@69226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/loader/MainResourceLoader.cpp
@@ -123,10 +123,17 @@ void MainResourceLoader::callContinueAfterNavigationPolicy(void* argument, const
     static_cast<MainResourceLoader*>(argument)->continueAfterNavigationPolicy(request, shouldContinue);
 }
 
-void MainResourceLoader::continueAfterNavigationPolicy(const ResourceRequest&, bool shouldContinue)
+void MainResourceLoader::continueAfterNavigationPolicy(const ResourceRequest& request, bool shouldContinue)
 {
     if (!shouldContinue)
         stopLoadingForPolicyChange();
+    else if (m_substituteData.isValid()) {
+        // A redirect resulted in loading substitute data.
+        ASSERT(documentLoader()->timing()->redirectCount);
+        handle()->cancel();
+        handleDataLoadSoon(request);
+    }
+
     deref(); // balances ref in willSendRequest
 }
 
@@ -191,6 +198,15 @@ void MainResourceLoader::willSendRequest(ResourceRequest& newRequest, const Reso
     Frame* top = m_frame->tree()->top();
     if (top != m_frame)
         frameLoader()->checkIfDisplayInsecureContent(top->document()->securityOrigin(), newRequest.url());
+
+#if ENABLE(OFFLINE_WEB_APPLICATIONS)
+    if (!redirectResponse.isNull()) {
+        // We checked application cache for initial URL, now we need to check it for redirected one.
+        ASSERT(documentLoader()->applicationCacheHost()->status() == ApplicationCacheHost::UNCACHED);
+        ASSERT(!m_substituteData.isValid());
+        documentLoader()->applicationCacheHost()->maybeLoadMainResource(newRequest, m_substituteData);
+    }
+#endif
 
     // FIXME: Ideally we'd stop the I/O until we hear back from the navigation policy delegate
     // listener. But there's no way to do that in practice. So instead we cancel later if the
@@ -500,7 +516,7 @@ void MainResourceLoader::startDataLoadTimer()
 #endif
 }
 
-void MainResourceLoader::handleDataLoadSoon(ResourceRequest& r)
+void MainResourceLoader::handleDataLoadSoon(const ResourceRequest& r)
 {
     m_initialRequest = r;
     

LayoutTests/http/tests/appcache/main-resource-redirect-expected.txt
@@ -1,0 +1,4 @@
+Test that application cache is consulted again after a redirect response.
+
+PASS
+

LayoutTests/http/tests/appcache/main-resource-redirect.html
@@ -1,0 +1,39 @@
+<html manifest="resources/main-resource-redirect.manifest">
+<body>
+<p>Test that application cache is consulted again after a redirect response.</p>
+<div id="result">Testing...</div>
+<script>
+if (window.layoutTestController) {
+    layoutTestController.waitUntilDone();
+    layoutTestController.dumpAsText();
+}
+
+function setNetworkEnabled(state)
+{
+    var req = new XMLHttpRequest;
+    req.open("GET", "/resources/network-simulator.php?command=" + (state ? "connect" : "disconnect"), false);
+    req.send("");
+}
+
+function test()
+{
+    applicationCache.oncached = null;
+    applicationCache.onnoupdate = null;
+    setNetworkEnabled(false);
+    var ifr = document.createElement("iframe");
+    ifr.setAttribute("src", "resources/main-resource-redirect-frame.php");
+    document.body.appendChild(ifr);
+}
+
+function done()
+{
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+    setNetworkEnabled(true);
+}
+
+applicationCache.oncached=test;
+applicationCache.onnoupdate=test;
+applicationCache.onerror = function() { setNetworkEnabled(true); window.location.reload(); }
+</script>
+</body>

LayoutTests/http/tests/appcache/resources/main-resource-redirect-frame-2.html
@@ -1,0 +1,6 @@
+<html manifest="http://127.0.0.1:8000/appcache/resources/main-resource-redirect.manifest">
+<script>
+parent.document.getElementById("result").innerHTML = (applicationCache.status) > 0 ? "PASS" : "FAIL: child frame status is UNCACHED";
+parent.done();
+
+</script>

LayoutTests/http/tests/appcache/resources/main-resource-redirect-frame.php
@@ -1,0 +1,4 @@
+<?php
+  header('HTTP/1.0 302 Found');
+  header('Location: http://127.0.0.1:8000/resources/network-simulator.php?path=/appcache/resources/main-resource-redirect-frame-2.html');
+?>

LayoutTests/http/tests/appcache/resources/main-resource-redirect.manifest
@@ -1,0 +1,4 @@
+CACHE MANIFEST
+http://127.0.0.1:8000/resources/network-simulator.php?path=/appcache/resources/main-resource-redirect-frame-2.html
+NETWORK:
+http://127.0.0.1:8000/resources/network-simulator.php?command

WebCore/loader/MainResourceLoader.h
@@ -79,7 +79,7 @@ namespace WebCore {
         bool loadNow(ResourceRequest&);
 
         void handleEmptyLoad(const KURL&, bool forURLScheme);
-        void handleDataLoadSoon(ResourceRequest& r);
+        void handleDataLoadSoon(const ResourceRequest& r);
 
         void startDataLoadTimer();
         void handleDataLoad(ResourceRequest&);

commit 0e4ad07a4c3acb788ee9727a639f1a9e02b798d4

2010-10-28  Michael Nordman  <michaeln@google.com>
        Reviewed by ap.

        [Chrome] Fix an appcache regression introduced in r69226
        https://bugs.webkit.org/show_bug.cgi?id=48592
        Provide a noop method body ApplicationCacheHost::maybeLoadMainResourceForRedirect.

        * src/ApplicationCacheHost.cpp:
        (WebCore::ApplicationCacheHost::maybeLoadMainResourceForRedirect):
2010-10-28  Michael Nordman  <michaeln@google.com>

        Reviewed by ap.

        [Chrome] Fix an appcache regression introduced in r69226
        https://bugs.webkit.org/show_bug.cgi?id=48592
        Define a new ApplicationCacheHost method for the specific case of main resource redirect handling.

        Tests: the existing layout test applies

        * loader/MainResourceLoader.cpp: use the newly introduced method
        (WebCore::MainResourceLoader::willSendRequest):
        * loader/appcache/ApplicationCacheHost.cpp:
        (WebCore::ApplicationCacheHost::maybeLoadMainResourceForRedirect):
        * loader/appcache/ApplicationCacheHost.h:


git-svn-id: svn://svn.chromium.org/blink/trunk@70923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/loader/MainResourceLoader.cpp
@@ -202,9 +202,8 @@ void MainResourceLoader::willSendRequest(ResourceRequest& newRequest, const Reso
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
     if (!redirectResponse.isNull()) {
         // We checked application cache for initial URL, now we need to check it for redirected one.
-        ASSERT(documentLoader()->applicationCacheHost()->status() == ApplicationCacheHost::UNCACHED);
         ASSERT(!m_substituteData.isValid());
-        documentLoader()->applicationCacheHost()->maybeLoadMainResource(newRequest, m_substituteData);
+        documentLoader()->applicationCacheHost()->maybeLoadMainResourceForRedirect(newRequest, m_substituteData);
     }
 #endif
 

WebCore/loader/appcache/ApplicationCacheHost.cpp
@@ -91,6 +91,12 @@ void ApplicationCacheHost::maybeLoadMainResource(ResourceRequest& request, Subst
     }
 }
 
+void ApplicationCacheHost::maybeLoadMainResourceForRedirect(ResourceRequest& request, SubstituteData& substituteData)
+{
+    ASSERT(status() == UNCACHED);
+    maybeLoadMainResource(request, substituteData);
+}
+
 bool ApplicationCacheHost::maybeLoadFallbackForMainResponse(const ResourceRequest& request, const ResourceResponse& r)
 {
     if (r.httpStatusCode() / 100 == 4 || r.httpStatusCode() / 100 == 5) {

WebCore/loader/appcache/ApplicationCacheHost.h
@@ -122,6 +122,7 @@ namespace WebCore {
         void selectCacheWithManifest(const KURL& manifestURL);
 
         void maybeLoadMainResource(ResourceRequest&, SubstituteData&);
+        void maybeLoadMainResourceForRedirect(ResourceRequest&, SubstituteData&);
         bool maybeLoadFallbackForMainResponse(const ResourceRequest&, const ResourceResponse&);
         bool maybeLoadFallbackForMainError(const ResourceRequest&, const ResourceError&);
         void mainResourceDataReceived(const char* data, int length, long long lengthReceived, bool allAtOnce);

WebKit/chromium/src/ApplicationCacheHost.cpp
@@ -113,6 +113,11 @@ void ApplicationCacheHost::selectCacheWithManifest(const KURL& manifestURL)
     }
 }
 
+void ApplicationCacheHost::maybeLoadMainResourceForRedirect(ResourceRequest&, SubstituteData&)
+{
+    // N/A to the chromium port
+}
+
 bool ApplicationCacheHost::maybeLoadFallbackForMainResponse(const ResourceRequest&, const ResourceResponse& response)
 {
     if (m_internal) {