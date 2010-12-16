primiano/blink-gitcs

commit 6ce7c99ae9bfcba1ef6ffc319065bb2ac09d9667

2010-12-16  Leandro Pereira  <leandro@profusion.mobi>
        [EFL] Unreviewed build fix.

        Disable ContextMenu support for now while support for
        CROSS_PLATFORM_MENUS is written.

        * CMakeListsEfl.txt:
        * WebCoreSupport/ContextMenuClientEfl.cpp: Removed.
        * WebCoreSupport/ContextMenuClientEfl.h: Removed.
        * ewk/ewk_contextmenu.cpp:
        (ewk_context_menu_item_append):
        * ewk/ewk_view.cpp:
        (_ewk_view_priv_new):
2010-12-16  Leandro Pereira  <leandro@profusion.mobi>

        [EFL] Unreviewed build fix.

        Disable ContextMenu support for now while support for
        CROSS_PLATFORM_MENUS is written.

        * CMakeListsEfl.txt:
        * platform/ContextMenu.h:
        * platform/ContextMenuItem.h:
        * platform/efl/ContextMenuEfl.cpp:
        (WebCore::ContextMenu::ContextMenu):
        (WebCore::ContextMenu::getContextMenuItems):
        (WebCore::ContextMenu::createNativeMenuFromItems):
        (WebCore::ContextMenu::nativeMenu):
        * platform/efl/ContextMenuItemEfl.cpp:
        (WebCore::ContextMenuItem::ContextMenuItem):
        (WebCore::ContextMenuItem::~ContextMenuItem):
        (WebCore::ContextMenuItem::nativeMenuItem):
        (WebCore::ContextMenuItem::type):
        (WebCore::ContextMenuItem::setAction):
        (WebCore::ContextMenuItem::action):
        (WebCore::ContextMenuItem::setChecked):
        (WebCore::ContextMenuItem::checked):
        (WebCore::ContextMenuItem::setEnabled):
        (WebCore::ContextMenuItem::enabled):
        (WebCore::ContextMenuItem::setSubMenu):
2010-12-16  Leandro Pereira  <leandro@profusion.mobi>

        [EFL] Unreviewed build fix.

        Disable ContextMenu support for now while support for
        CROSS_PLATFORM_MENUS is written.

        * cmake/OptionsEfl.cmake:


git-svn-id: svn://svn.chromium.org/blink/trunk@74207 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/platform/ContextMenuItem.h
@@ -216,23 +216,6 @@ namespace WebCore {
         bool checked;
         bool enabled;
     };
-#elif PLATFORM(EFL)
-    struct PlatformMenuItemDescription {
-        PlatformMenuItemDescription()
-            : type(ActionType)
-            , action(ContextMenuItemTagNoAction)
-            , title("")
-            , subMenu(0)
-            , checked(false)
-            , enabled(true) { }
-        ContextMenuItemType type;
-        ContextMenuAction action;
-        String title;
-        ContextMenu* subMenu;
-        bool checked;
-        bool enabled;
-    };
-#else
     typedef void* PlatformMenuItemDescription;
 #endif
 
@@ -260,6 +243,8 @@ namespace WebCore {
 #if USE(CROSS_PLATFORM_CONTEXT_MENUS)
 #if PLATFORM(WIN)
         typedef MENUITEMINFO NativeItem;
+#elif PLATFORM(EFL)
+        typedef void* NativeItem;
 #endif
         ContextMenuItem(ContextMenuAction, const String&, bool enabled, bool checked, const Vector<ContextMenuItem>& subMenuItems);
         explicit ContextMenuItem(const NativeItem&);

WebCore/platform/ContextMenu.h
@@ -40,9 +40,6 @@
 namespace WebCore {
 
     class ContextMenuController;
-#if PLATFORM(EFL)
-    class ContextMenuClientEfl;
-#endif
 
     class ContextMenu : public Noncopyable
     {
@@ -54,6 +51,8 @@ namespace WebCore {
 #if USE(CROSS_PLATFORM_CONTEXT_MENUS)
 #if PLATFORM(WIN)
         typedef HMENU NativeMenu;
+#elif PLATFORM(EFL)
+        typedef void* NativeMenu;
 #endif
         explicit ContextMenu(NativeMenu);
 
@@ -102,9 +101,6 @@ namespace WebCore {
         QList<ContextMenuItem> m_items;
 #elif PLATFORM(CHROMIUM)
         Vector<ContextMenuItem> m_items;
-#elif PLATFORM(EFL)
-        ContextMenuClientEfl* m_contextMenuClient;
-        PlatformMenuDescription m_platformDescription;
 #else
         PlatformMenuDescription m_platformDescription;
 #if OS(WINCE)

WebCore/platform/efl/ContextMenuEfl.cpp
@@ -22,55 +22,34 @@
 #include "config.h"
 #include "ContextMenu.h"
 
-#include "ContextMenuClient.h"
-#include "ContextMenuClientEfl.h"
-#include "ContextMenuController.h"
 #include "NotImplemented.h"
 
 namespace WebCore {
 
+ContextMenu::ContextMenu(void* menu)
+{
+    getContextMenuItems(menu, m_items);
+}
+
 ContextMenu::ContextMenu()
 {
-    m_platformDescription = (PlatformMenuDescription) ewk_context_menu_new(m_view, menu->controller());
+    notImplemented();
 }
 
-ContextMenu::ContextMenu(const PlatformMenuDescription menu)
-    : m_platformDescription(menu)
+void ContextMenu::getContextMenuItems(void* menu, Vector<ContextMenuItem>& items)
 {
+    notImplemented();
 }
 
-ContextMenu::~ContextMenu()
+void* ContextMenu::createNativeMenuFromItems(const Vector<ContextMenuItem>& items)
 {
-    if (m_platformDescription)
-        ewk_context_menu_free(static_cast<Ewk_Context_Menu*>(m_platformDescription));
+    notImplemented();
+    return 0;
 }
 
-void ContextMenu::appendItem(ContextMenuItem& item)
+void* ContextMenu::nativeMenu() const
 {
-    ewk_context_menu_item_append(static_cast<Ewk_Context_Menu*>(m_platformDescription), item);
-}
-
-void ContextMenu::setPlatformDescription(PlatformMenuDescription menu)
-{
-    ASSERT(!m_platformDescription);
-
-    m_platformDescription = menu;
-    ewk_context_menu_show(static_cast<Ewk_Context_Menu*>(m_platformDescription));
-}
-
-PlatformMenuDescription ContextMenu::platformDescription() const
-{
-    return m_platformDescription;
-}
-
-PlatformMenuDescription ContextMenu::releasePlatformDescription()
-{
-    // Ref count remains the same, just pass it and remove our ref, so it
-    // will not be decremented when this object goes away.
-    PlatformMenuDescription description = m_platformDescription;
-    m_platformDescription = 0;
-
-    return description;
+    return createNativeMenuFromItems(m_items);
 }
 
 }

WebCore/platform/efl/ContextMenuItemEfl.cpp
@@ -30,113 +30,73 @@
 #include "config.h"
 #include "ContextMenuItem.h"
 
-#include "ContextMenu.h"
 #include "NotImplemented.h"
 
 namespace WebCore {
 
-ContextMenuItem::ContextMenuItem(PlatformMenuItemDescription)
+ContextMenuItem::ContextMenuItem(void* const&)
 {
-    // It's inside WebKit that this initialization is done, as WebCore doesn't
-    // know how PlatformMenuItemDescription is implemented.
     notImplemented();
 }
 
-ContextMenuItem::ContextMenuItem(ContextMenu* submenu)
+ContextMenuItem::ContextMenuItem(ContextMenuItemType, ContextMenuAction, const String&, ContextMenu*)
 {
-    m_platformDescription.type = SubmenuType;
-    setSubMenu(submenu);
-}
-
-ContextMenuItem::ContextMenuItem(ContextMenuItemType type, ContextMenuAction action, const String& title, ContextMenu* subMenu)
-{
-    m_platformDescription.type = type;
-    m_platformDescription.action = action;
-    m_platformDescription.title = String(title);
-
-    setSubMenu(subMenu);
+    notImplemented();
 }
 
 ContextMenuItem::~ContextMenuItem()
 {
-    if (m_platformDescription.subMenu)
-        delete m_platformDescription.subMenu;
+    notImplemented();
 }
 
-PlatformMenuItemDescription ContextMenuItem::releasePlatformDescription()
+void* ContextMenuItem::nativeMenuItem() const
 {
-    return m_platformDescription;
+    notImplemented();
+    return 0;
 }
 
 ContextMenuItemType ContextMenuItem::type() const
 {
-    return m_platformDescription.type;
+    notImplemented();
+    return ActionType;
 }
 
-void ContextMenuItem::setType(ContextMenuItemType type)
+void ContextMenuItem::setAction(ContextMenuAction)
 {
-    m_platformDescription.type = type;
+    notImplemented();
 }
 
 ContextMenuAction ContextMenuItem::action() const
 {
-    return m_platformDescription.action;
+    notImplemented();
+    return ContextMenuItemTagNoAction;
 }
 
-void ContextMenuItem::setAction(ContextMenuAction action)
+void ContextMenuItem::setChecked(bool)
 {
-    m_platformDescription.action = action;
-}
-
-String ContextMenuItem::title() const
-{
-    return m_platformDescription.title;
-}
-
-void ContextMenuItem::setTitle(const String& title)
-{
-    m_platformDescription.title = String(title);
-}
-
-PlatformMenuDescription ContextMenuItem::platformSubMenu() const
-{
-    if (!m_platformDescription.subMenu)
-        return 0;
-
-    return m_platformDescription.subMenu->platformDescription();
-
-}
-
-void ContextMenuItem::setSubMenu(ContextMenu* subMenu)
-{
-    delete m_platformDescription.subMenu;
-    m_platformDescription.subMenu = 0;
-
-    if (!subMenu)
-        return;
-
-    m_platformDescription.type = SubmenuType;
-    m_platformDescription.subMenu = new ContextMenu(subMenu->releasePlatformDescription());
+    notImplemented();
 }
 
 bool ContextMenuItem::checked() const
 {
-    return m_platformDescription.checked;
+    notImplemented();
+    return 0;
 }
 
-void ContextMenuItem::setChecked(bool shouldCheck)
+void ContextMenuItem::setEnabled(bool)
 {
-    m_platformDescription.checked = shouldCheck;
+    notImplemented();
 }
 
 bool ContextMenuItem::enabled() const
 {
-    return m_platformDescription.enabled;
+    notImplemented();
+    return false;
 }
 
-void ContextMenuItem::setEnabled(bool shouldEnable)
+void ContextMenuItem::setSubMenu(ContextMenu*)
 {
-    m_platformDescription.enabled = shouldEnable;
+    notImplemented();
 }
 
 }

WebKit/efl/WebCoreSupport/ContextMenuClientEfl.cpp
@@ -1,123 +1,0 @@
-/*
- * Copyright (C) 2010 ProFUSION embedded systems
- * Copyright (C) 2010 Samsung Electronics
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-
-#include "ContextMenuClientEfl.h"
-
-#include "ContextMenu.h"
-#include "EWebKit.h"
-#include "HitTestResult.h"
-#include "KURL.h"
-#include "NotImplemented.h"
-#include "PlatformMenuDescription.h"
-#include "ewk_private.h"
-
-using namespace WebCore;
-
-namespace WebCore {
-
-ContextMenuClientEfl::ContextMenuClientEfl(Evas_Object* view)
-    : m_view(view)
-{
-}
-
-void ContextMenuClientEfl::contextMenuDestroyed()
-{
-    delete this;
-}
-
-PlatformMenuDescription ContextMenuClientEfl::getCustomMenuFromDefaultItems(ContextMenu* menu)
-{
-    PlatformMenuDescription newmenu = ewk_context_menu_custom_get(static_cast<Ewk_Context_Menu*>(menu->releasePlatformDescription()));
-
-    return newmenu;
-}
-
-void ContextMenuClientEfl::contextMenuItemSelected(ContextMenuItem*, const ContextMenu*)
-{
-    notImplemented();
-}
-
-void ContextMenuClientEfl::downloadURL(const KURL& url)
-{
-    if (!m_view)
-        return;
-
-    Ewk_Download download;
-
-    CString downloadUrl = url.prettyURL().utf8();
-    download.url = downloadUrl.data();
-    ewk_view_download_request(m_view, &download);
-}
-
-void ContextMenuClientEfl::searchWithGoogle(const Frame*)
-{
-    notImplemented();
-}
-
-void ContextMenuClientEfl::lookUpInDictionary(Frame*)
-{
-    notImplemented();
-}
-
-bool ContextMenuClientEfl::isSpeaking()
-{
-    notImplemented();
-    return false;
-}
-
-void ContextMenuClientEfl::speak(const String&)
-{
-    notImplemented();
-}
-
-void ContextMenuClientEfl::stopSpeaking()
-{
-    notImplemented();
-}
-
-PlatformMenuDescription ContextMenuClientEfl::createPlatformDescription(ContextMenu* menu)
-{
-    return (PlatformMenuDescription) ewk_context_menu_new(m_view, menu->controller());
-}
-
-void ContextMenuClientEfl::freePlatformDescription(PlatformMenuDescription menu)
-{
-    ewk_context_menu_free(static_cast<Ewk_Context_Menu*>(menu));
-}
-
-void ContextMenuClientEfl::appendItem(PlatformMenuDescription menu, ContextMenuItem& item)
-{
-    ewk_context_menu_item_append(static_cast<Ewk_Context_Menu*>(menu), item);
-}
-
-void ContextMenuClientEfl::show(PlatformMenuDescription menu)
-{
-    ewk_context_menu_show(static_cast<Ewk_Context_Menu*>(menu));
-}
-
-}

WebKit/efl/WebCoreSupport/ContextMenuClientEfl.h
@@ -1,62 +1,0 @@
-/*
- * Copyright (C) 2010 ProFUSION embedded systems
- * Copyright (C) 2010 Samsung Electronics
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#ifndef ContextMenuClientEfl_h
-#define ContextMenuClientEfl_h
-
-#include "ContextMenuClient.h"
-#include "EWebKit.h"
-
-namespace WebCore {
-
-class ContextMenu;
-
-class ContextMenuClientEfl : public ContextMenuClient {
- public:
-    explicit ContextMenuClientEfl(Evas_Object*);
-
-    virtual void contextMenuDestroyed();
-
-    virtual PlatformMenuDescription getCustomMenuFromDefaultItems(ContextMenu*);
-    virtual void contextMenuItemSelected(ContextMenuItem*, const ContextMenu*);
-
-    virtual void downloadURL(const KURL&);
-    virtual void searchWithGoogle(const Frame*);
-    virtual void lookUpInDictionary(Frame*);
-    virtual void speak(const String&);
-    virtual bool isSpeaking();
-    virtual void stopSpeaking();
-
-    PlatformMenuDescription createPlatformDescription(ContextMenu*);
-    void freePlatformDescription(PlatformMenuDescription);
-    void appendItem(PlatformMenuDescription, ContextMenuItem&);
-    void show(PlatformMenuDescription menu);
- private:
-    Evas_Object* m_view;
-};
-}
-
-#endif // ContextMenuClientEfl_h

WebKit/efl/ewk/ewk_contextmenu.cpp
@@ -236,10 +236,9 @@ void ewk_context_menu_item_append(Ewk_Context_Menu* o, WebCore::ContextMenuItem&
 {
     Ewk_Context_Menu_Item_Type type = static_cast<Ewk_Context_Menu_Item_Type>(core.type());
     Ewk_Context_Menu_Action action = static_cast<Ewk_Context_Menu_Action>(core.action());
-    Ewk_Context_Menu* submenu = static_cast<Ewk_Context_Menu*>(core.platformSubMenu());
 
     Ewk_Context_Menu_Item* menu_item = ewk_context_menu_item_new
-        (type, action, submenu, core.title().utf8().data(), core.checked(),
+        (type, action, 0, core.title().utf8().data(), core.checked(),
          core.enabled());
     EINA_SAFETY_ON_NULL_RETURN(menu_item);
 

WebKit/efl/ewk/ewk_view.cpp
@@ -25,7 +25,6 @@
 #include "BackForwardListImpl.h"
 #include "Chrome.h"
 #include "ChromeClientEfl.h"
-#include "ContextMenuClientEfl.h"
 #include "ContextMenuController.h"
 #include "DocumentLoader.h"
 #include "DragClientEfl.h"
@@ -548,7 +547,6 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* sd)
 
     WebCore::Page::PageClients pageClients;
     pageClients.chromeClient = static_cast<WebCore::ChromeClient*>(new WebCore::ChromeClientEfl(sd->self));
-    pageClients.contextMenuClient = static_cast<WebCore::ContextMenuClient*>(new WebCore::ContextMenuClientEfl(sd->self));
     pageClients.editorClient = static_cast<WebCore::EditorClient*>(new WebCore::EditorClientEfl(sd->self));
     pageClients.dragClient = static_cast<WebCore::DragClient*>(new WebCore::DragClientEfl);
     pageClients.inspectorClient = static_cast<WebCore::InspectorClient*>(new WebCore::InspectorClientEfl);

commit 2747efc1280551b42abab4d219520565c6987897

Unreviewed. Fixed chromium build break caused by r74207.
* platform/ContextMenuItem.h:

git-svn-id: svn://svn.chromium.org/blink/trunk@74208 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/platform/ContextMenuItem.h
@@ -216,6 +216,7 @@ namespace WebCore {
         bool checked;
         bool enabled;
     };
+#else
     typedef void* PlatformMenuItemDescription;
 #endif
 