primiano/blink-gitcs

commit 758f4ac2d7e9a40b62035964f73bc4eba733e797

2010-04-01  Nate Chapin  <japhet@chromium.org>
        Reviewed by Dimitri Glazkov.

        [V8] Generalize (and generate!) the creation of hidden references
        between JS wrappers.
        
        https://bugs.webkit.org/show_bug.cgi?id=36777

        * Android.v8bindings.mk:
        * WebCore.gypi:
        * bindings/scripts/CodeGeneratorV8.pm: Generate calls to setHiddenReference() in
        attribute getters.
        * bindings/v8/V8DOMWrapper.cpp:
        (WebCore::V8DOMWrapper::setHiddenReference): Split common logic out of hidden setHiddenWindowReference
        (WebCore::V8DOMWrapper::setHiddenWindowReference): Now contains logic specific to putting a 
        hidden reference on a global object.
        (WebCore::globalObjectPrototypeIsDOMWindow): Be more thorough in the COMPILE_ASSERTs.
        (WebCore::V8DOMWrapper::convertEventTargetToV8Object): Cleanup: Remove a duplicate if statement.
        * bindings/v8/V8DOMWrapper.h:
        * bindings/v8/WrapperTypeInfo.h:
        * bindings/v8/custom/V8BarInfoCustom.cpp: Removed.
        * bindings/v8/custom/V8CSSStyleSheetCustom.cpp:
        (WebCore::toV8):
        * bindings/v8/custom/V8DOMSelectionCustom.cpp: Removed.
        * bindings/v8/custom/V8HistoryCustom.cpp:
        * bindings/v8/custom/V8LocationCustom.cpp:
        * bindings/v8/custom/V8MessageChannelConstructor.cpp:
        (WebCore::V8MessageChannel::constructorCallback):
        * bindings/v8/custom/V8NamedNodeMapCustom.cpp:
        (WebCore::toV8):
        * bindings/v8/custom/V8NavigatorCustom.cpp: Removed.
        * bindings/v8/custom/V8ScreenCustom.cpp: Removed.
        * bindings/v8/custom/V8StyleSheetCustom.cpp:
        (WebCore::toV8):
        * loader/appcache/DOMApplicationCache.h:
        (WebCore::DOMApplicationCache::frame): Add frame() accessor.


git-svn-id: svn://svn.chromium.org/blink/trunk@57004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/WebCore.gypi
@@ -676,7 +676,6 @@
             'bindings/v8/ChildThreadDOMData.cpp',
             'bindings/v8/ChildThreadDOMData.h',
             'bindings/v8/custom/V8AttrCustom.cpp',
-            'bindings/v8/custom/V8BarInfoCustom.cpp',
             'bindings/v8/custom/V8CanvasPixelArrayCustom.cpp',
             'bindings/v8/custom/V8WebGLArrayCustom.h',
             'bindings/v8/custom/V8WebGLArrayCustom.cpp',
@@ -722,7 +721,6 @@
             'bindings/v8/custom/V8DedicatedWorkerContextCustom.cpp',
             'bindings/v8/custom/V8DocumentLocationCustom.cpp',
             'bindings/v8/custom/V8DOMFormDataCustom.cpp',
-            'bindings/v8/custom/V8DOMSelectionCustom.cpp',
             'bindings/v8/custom/V8DOMWindowCustom.cpp',
             'bindings/v8/custom/V8DocumentCustom.cpp',
             'bindings/v8/custom/V8ElementCustom.cpp',
@@ -762,7 +760,6 @@
             'bindings/v8/custom/V8NamedNodeMapCustom.cpp',
             'bindings/v8/custom/V8NamedNodesCollection.cpp',
             'bindings/v8/custom/V8NamedNodesCollection.h',
-            'bindings/v8/custom/V8NavigatorCustom.cpp',
             'bindings/v8/custom/V8NodeCustom.cpp',
             'bindings/v8/custom/V8NodeFilterCustom.cpp',
             'bindings/v8/custom/V8NodeIteratorCustom.cpp',
@@ -777,7 +774,6 @@
             'bindings/v8/custom/V8SVGLengthCustom.cpp',
             'bindings/v8/custom/V8SVGMatrixCustom.cpp',
             'bindings/v8/custom/V8SVGPathSegCustom.cpp',
-            'bindings/v8/custom/V8ScreenCustom.cpp',
             'bindings/v8/custom/V8SharedWorkerCustom.cpp',
             'bindings/v8/custom/V8StyleSheetCustom.cpp',
             'bindings/v8/custom/V8StyleSheetListCustom.cpp',

WebCore/Android.v8bindings.mk
@@ -85,7 +85,6 @@ LOCAL_SRC_FILES += \
 	bindings/v8/npruntime.cpp \
 	\
 	bindings/v8/custom/V8AttrCustom.cpp \
-	bindings/v8/custom/V8BarInfoCustom.cpp \
 	bindings/v8/custom/V8CSSRuleCustom.cpp \
 	bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp \
 	bindings/v8/custom/V8CSSStyleSheetCustom.cpp \
@@ -104,7 +103,6 @@ LOCAL_SRC_FILES += \
 	bindings/v8/custom/V8CustomSQLTransactionErrorCallback.cpp \
 	bindings/v8/custom/V8CustomVoidCallback.cpp \
   bindings/v8/custom/V8DOMFormDataCustom.cpp \
-	bindings/v8/custom/V8DOMSelectionCustom.cpp \
 	bindings/v8/custom/V8DOMWindowCustom.cpp \
 	bindings/v8/custom/V8DataGridColumnListCustom.cpp \
 	bindings/v8/custom/V8DatabaseCallback.cpp \
@@ -145,7 +143,6 @@ LOCAL_SRC_FILES += \
 	bindings/v8/custom/V8NodeFilterCustom.cpp \
 	bindings/v8/custom/V8NodeIteratorCustom.cpp \
 	bindings/v8/custom/V8NodeListCustom.cpp \
-	bindings/v8/custom/V8ScreenCustom.cpp \
 	bindings/v8/custom/V8SQLResultSetRowListCustom.cpp \
 	bindings/v8/custom/V8SQLTransactionCustom.cpp \
 	bindings/v8/custom/V8WebSocketCustom.cpp

WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -349,14 +349,8 @@ sub GetInternalFields
         if ($name eq "HTMLDocument") {
             push(@customInternalFields, ("markerIndex", "shadowIndex"));
         }
-    } elsif (IsSubType($dataNode, "StyleSheet") || $name eq "NamedNodeMap") {
-        push(@customInternalFields, "ownerNodeIndex");
-    } elsif ($name eq "MessageChannel") {
-        push(@customInternalFields, ("port1Index", "port2Index"));
     } elsif ($name eq "DOMWindow") {
-        push(@customInternalFields, ("consoleIndex", "historyIndex", "locationbarIndex", "menubarIndex", "navigatorIndex", 
-            "personalbarIndex", "screenIndex", "scrollbarsIndex", "selectionIndex", "statusbarIndex", "toolbarIndex", "locationIndex",
-            "domSelectionIndex", "enteredIsolatedWorldIndex"));
+        push(@customInternalFields, "enteredIsolatedWorldIndex");
     }
     return @customInternalFields;
 }
@@ -766,6 +760,31 @@ END
         } else {
             # Can inline the function call into the return statement to avoid overhead of using a Ref<> temporary
             $result = $getterString;
+        }
+ 
+        # Special case for readonly or Replaceable attributes (with a few exceptions). This attempts to ensure that JS wrappers don't get
+        # garbage-collected prematurely when their lifetime is strongly tied to their owner. We accomplish this by inserting a reference to
+        # the newly created wrapper into an internal field of the holder object.
+        if (!IsNodeSubType($dataNode) && $attrName ne "self" && (IsWrapperType($returnType) && ($attribute->type =~ /^readonly/ || $attribute->signature->extendedAttributes->{"Replaceable"})
+            && $returnType ne "EventTarget" && $returnType ne "SerializedScriptValue" && $returnType ne "DOMWindow" 
+            && $returnType !~ /SVG/ && $returnType !~ /HTML/ && !IsDOMNodeType($returnType))) {
+            AddIncludesForType($returnType);
+            my $domMapFunction = GetDomMapFunction(0, $returnType);
+            # Check for a wrapper in the wrapper cache. If there is one, we know that a hidden reference has already
+            # been created. If we don't find a wrapper, we create both a wrapper and a hidden reference.
+            push(@implContentDecls, "    RefPtr<$returnType> result = ${getterString};\n");
+            push(@implContentDecls, "    v8::Handle<v8::Value> wrapper = result.get() ? ${domMapFunction}.get(result.get()) : v8::Handle<v8::Value>();\n");
+            push(@implContentDecls, "    if (wrapper.IsEmpty()) {\n");
+            push(@implContentDecls, "        wrapper = toV8(result.get());\n");
+            if ($dataNode->name eq "DOMWindow") {
+                push(@implContentDecls, "        V8DOMWrapper::setHiddenWindowReference(imp->frame(), wrapper);\n");
+            } else {
+                push(@implContentDecls, "        V8DOMWrapper::setHiddenReference(info.Holder(), wrapper);\n");
+            }
+            push(@implContentDecls, "    }\n");
+            push(@implContentDecls, "    return wrapper;\n");
+            push(@implContentDecls, "}\n\n");
+            return;
         }
     }
 
@@ -2078,21 +2097,16 @@ sub HasCustomToV8Implementation {
     $interfaceName = shift;
 
     # We generate a custom converter (but JSC doesn't) for the following:
-    return 1 if $interfaceName eq "BarInfo";
     return 1 if $interfaceName eq "CSSStyleSheet";
     return 1 if $interfaceName eq "CanvasPixelArray";
-    return 1 if $interfaceName eq "DOMSelection";
     return 1 if $interfaceName eq "DOMWindow";
     return 1 if $interfaceName eq "Element";
-    return 1 if $interfaceName eq "Location";
     return 1 if $interfaceName eq "HTMLDocument";
     return 1 if $interfaceName eq "HTMLElement";
-    return 1 if $interfaceName eq "History";
+    return 1 if $interfaceName eq "Location";
     return 1 if $interfaceName eq "NamedNodeMap";
-    return 1 if $interfaceName eq "Navigator";
     return 1 if $interfaceName eq "SVGDocument";
     return 1 if $interfaceName eq "SVGElement";
-    return 1 if $interfaceName eq "Screen";
     return 1 if $interfaceName eq "WorkerContext";
     # We don't generate a custom converter (but JSC does) for the following:
     return 0 if $interfaceName eq "AbstractWorker";
@@ -2109,7 +2123,7 @@ sub GetDomMapFunction
     my $dataNode = shift;
     my $type = shift;
     return "getDOMSVGElementInstanceMap()" if $type eq "SVGElementInstance";
-    return "getDOMNodeMap()" if IsNodeSubType($dataNode);
+    return "getDOMNodeMap()" if ($dataNode && IsNodeSubType($dataNode));
     # Only use getDOMSVGObjectWithContextMap() for non-node svg objects
     return "getDOMSVGObjectWithContextMap()" if $type =~ /SVG/;
     return "" if $type eq "DOMImplementation";
@@ -2587,6 +2601,8 @@ sub RequiresCustomSignature
 my %non_wrapper_types = (
     'float' => 1,
     'double' => 1,
+    'int' => 1,
+    'unsigned int' => 1,
     'short' => 1,
     'unsigned short' => 1,
     'long' => 1,

WebCore/bindings/v8/V8DOMWrapper.cpp
@@ -175,7 +175,18 @@ v8::Local<v8::Function> V8DOMWrapper::getConstructor(WrapperTypeInfo* type, Work
 }
 #endif
 
-void V8DOMWrapper::setHiddenWindowReference(Frame* frame, const int internalIndex, v8::Handle<v8::Object> jsObject)
+void V8DOMWrapper::setHiddenReference(v8::Handle<v8::Object> parent, v8::Handle<v8::Value> child)
+{
+    v8::Local<v8::Value> hiddenReferenceObject = parent->GetInternalField(v8DOMHiddenReferenceArrayIndex);
+    if (hiddenReferenceObject->IsNull() || hiddenReferenceObject->IsUndefined()) {
+        hiddenReferenceObject = v8::Array::New();
+        parent->SetInternalField(v8DOMHiddenReferenceArrayIndex, hiddenReferenceObject);
+    }
+    v8::Local<v8::Array> hiddenReferenceArray = v8::Local<v8::Array>::Cast(hiddenReferenceObject);
+    hiddenReferenceArray->Set(v8::Integer::New(hiddenReferenceArray->Length()), child);
+}
+
+void V8DOMWrapper::setHiddenWindowReference(Frame* frame, v8::Handle<v8::Value> jsObject)
 {
     // Get DOMWindow
     if (!frame)
@@ -184,14 +195,12 @@ void V8DOMWrapper::setHiddenWindowReference(Frame* frame, const int internalInde
     if (context.IsEmpty())
         return;
 
-    ASSERT(internalIndex < V8DOMWindow::internalFieldCount);
-
     v8::Handle<v8::Object> global = context->Global();
     // Look for real DOM wrapper.
     global = V8DOMWrapper::lookupDOMWrapper(V8DOMWindow::GetTemplate(), global);
     ASSERT(!global.IsEmpty());
-    ASSERT(global->GetInternalField(internalIndex)->IsUndefined());
-    global->SetInternalField(internalIndex, jsObject);
+
+    setHiddenReference(global, jsObject);
 }
 
 WrapperTypeInfo* V8DOMWrapper::domWrapperType(v8::Handle<v8::Object> object)
@@ -219,13 +228,18 @@ PassRefPtr<NodeFilter> V8DOMWrapper::wrapNativeNodeFilter(v8::Handle<v8::Value> 
 
 static bool globalObjectPrototypeIsDOMWindow(v8::Handle<v8::Object> objectPrototype)
 {
-#if ENABLE(SHARED_WORKERS)
     // We can identify what type of context the global object is wrapping by looking at the
     // internal field count of its prototype. This assumes WorkerContexts and DOMWindows have different numbers
-    // of internal fields, so a COMPILE_ASSERT is included to warn if this ever changes. DOMWindow has
-    // traditionally had far more internal fields than any other class.
-    COMPILE_ASSERT(V8DOMWindow::internalFieldCount != V8WorkerContext::internalFieldCount && V8DOMWindow::internalFieldCount != V8SharedWorkerContext::internalFieldCount,
+    // of internal fields, so a COMPILE_ASSERT is included to warn if this ever changes.
+#if ENABLE(WORKERS)
+    COMPILE_ASSERT(V8DOMWindow::internalFieldCount != V8WorkerContext::internalFieldCount,
         DOMWindowAndWorkerContextHaveUnequalFieldCounts);
+    COMPILE_ASSERT(V8DOMWindow::internalFieldCount != V8DedicatedWorkerContext::internalFieldCount,
+        DOMWindowAndDedicatedWorkerContextHaveUnequalFieldCounts);
+#endif
+#if ENABLE(SHARED_WORKERS)
+    COMPILE_ASSERT(V8DOMWindow::internalFieldCount != V8SharedWorkerContext::internalFieldCount,
+        DOMWindowAndSharedWorkerContextHaveUnequalFieldCounts);
 #endif
     return objectPrototype->InternalFieldCount() == V8DOMWindow::internalFieldCount;
 }
@@ -379,9 +393,6 @@ v8::Handle<v8::Value> V8DOMWrapper::convertEventTargetToV8Object(EventTarget* ta
         ASSERT(!wrapper.IsEmpty());
         return wrapper;
     }
-
-    if (XMLHttpRequest* xhr = target->toXMLHttpRequest())
-        return toV8(xhr);
 
     // MessagePort is created within its JS counterpart
     if (MessagePort* port = target->toMessagePort()) {

WebCore/bindings/v8/V8DOMWrapper.h
@@ -123,8 +123,10 @@ namespace WebCore {
         // Check whether a V8 value is a wrapper of type |classType|.
         static bool isWrapperOfType(v8::Handle<v8::Value>, WrapperTypeInfo*);
 
+        static void setHiddenReference(v8::Handle<v8::Object> parent, v8::Handle<v8::Value> child);
+
         // Set hidden references in a DOMWindow object of a frame.
-        static void setHiddenWindowReference(Frame*, const int internalIndex, v8::Handle<v8::Object>);
+        static void setHiddenWindowReference(Frame*, v8::Handle<v8::Value>);
 
         static v8::Local<v8::Object> instantiateV8Object(V8Proxy* proxy, WrapperTypeInfo*, void* impl);
 

WebCore/bindings/v8/WrapperTypeInfo.h
@@ -39,7 +39,8 @@ namespace WebCore {
     
     static const int v8DOMWrapperTypeIndex = 0;
     static const int v8DOMWrapperObjectIndex = 1;
-    static const int v8DefaultWrapperInternalFieldCount = 2;
+    static const int v8DOMHiddenReferenceArrayIndex = 2;
+    static const int v8DefaultWrapperInternalFieldCount = 3;
     
     typedef v8::Persistent<v8::FunctionTemplate> (*GetTemplateFunction)();
     typedef void (*DerefObjectFunction)(void*);

WebCore/bindings/v8/custom/V8BarInfoCustom.cpp
@@ -1,73 +1,0 @@
-/*
- * Copyright (C) 2010 Google Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "V8BarInfo.h"
-
-#include "V8DOMWindow.h"
-#include "V8DOMWrapper.h"
-
-namespace WebCore {
-
-v8::Handle<v8::Value> toV8(BarInfo* impl)
-{
-    if (!impl)
-        return v8::Null();
-    v8::Handle<v8::Object> wrapper = getDOMObjectMap().get(impl);
-    if (wrapper.IsEmpty()) {
-        wrapper = V8BarInfo::wrap(impl);
-        if (!wrapper.IsEmpty()) {
-            Frame* frame = impl->frame();
-            switch (impl->type()) {
-            case BarInfo::Locationbar:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::locationbarIndex, wrapper);
-                break;
-            case BarInfo::Menubar:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::menubarIndex, wrapper);
-                break;
-            case BarInfo::Personalbar:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::personalbarIndex, wrapper);
-                break;
-            case BarInfo::Scrollbars:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::scrollbarsIndex, wrapper);
-                break;
-            case BarInfo::Statusbar:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::statusbarIndex, wrapper);
-                break;
-            case BarInfo::Toolbar:
-                V8DOMWrapper::setHiddenWindowReference(frame, V8DOMWindow::toolbarIndex, wrapper);
-                break;
-            }
-        }
-    }
-    return wrapper;
-}
-
-} // namespace WebCore

WebCore/bindings/v8/custom/V8CSSStyleSheetCustom.cpp
@@ -31,6 +31,7 @@
 #include "config.h"
 #include "V8CSSStyleSheet.h"
 
+#include "V8DOMWrapper.h"
 #include "V8Node.h"
 
 namespace WebCore {
@@ -43,7 +44,7 @@ v8::Handle<v8::Value> toV8(CSSStyleSheet* impl)
     // Add a hidden reference from stylesheet object to its owner node.
     Node* ownerNode = impl->ownerNode();
     if (ownerNode && !wrapper.IsEmpty())
-        wrapper->SetInternalField(V8CSSStyleSheet::ownerNodeIndex, toV8(ownerNode));
+        V8DOMWrapper::setHiddenReference(wrapper, toV8(ownerNode));
     return wrapper;
 }
 

commit 1887f2613b028e0cc1b466007c0927320d82ef2f

2010-12-30  Mikhail Naganov  <mnaganov@chromium.org>
        Reviewed by Pavel Feldman.

        Fix leak of MemoryInfo wrappers introduced in r57004 (see issue 51712).

        https://bugs.webkit.org/show_bug.cgi?id=51713

        * WebCore.gypi:
        * WebCore.pro:
        * bindings/v8/custom/V8ConsoleCustom.cpp:
        (WebCore::V8Console::memoryAccessorGetter):
        * bindings/v8/custom/V8PerformanceCustom.cpp: Copied from V8ConsoleCustom.cpp.
        (WebCore::V8Performance::memoryAccessorGetter):
        * page/Console.idl:
        * page/Performance.idl:

git-svn-id: svn://svn.chromium.org/blink/trunk@74795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

WebCore/WebCore.gypi
@@ -860,6 +860,7 @@
             'bindings/v8/custom/V8NodeCustom.cpp',
             'bindings/v8/custom/V8NodeListCustom.cpp',
             'bindings/v8/custom/V8NotificationCenterCustom.cpp',
+            'bindings/v8/custom/V8PerformanceCustom.cpp',
             'bindings/v8/custom/V8PopStateEventCustom.cpp',
             'bindings/v8/custom/V8ScriptProfileCustom.cpp',
             'bindings/v8/custom/V8ScriptProfileNodeCustom.cpp',

WebCore/WebCore.pro
@@ -470,6 +470,7 @@ v8 {
         bindings/v8/custom/V8NamedNodesCollection.cpp \
         bindings/v8/custom/V8NodeCustom.cpp \
         bindings/v8/custom/V8NodeListCustom.cpp \
+        bindings/v8/custom/V8PerformanceCustom.cpp \
         bindings/v8/custom/V8SQLResultSetRowListCustom.cpp \
         bindings/v8/custom/V8SQLTransactionCustom.cpp \
         bindings/v8/custom/V8WebSocketCustom.cpp \

WebCore/bindings/v8/custom/V8ConsoleCustom.cpp
@@ -39,6 +39,7 @@
 #include "ScriptProfile.h"
 #include "V8Binding.h"
 #include "V8BindingMacros.h"
+#include "V8MemoryInfo.h"
 #include "V8Proxy.h"
 #include "V8ScriptProfile.h"
 
@@ -107,5 +108,12 @@ v8::Handle<v8::Value> V8Console::profileEndCallback(const v8::Arguments& args)
     return v8::Handle<v8::Value>();
 }
 #endif
+
+v8::Handle<v8::Value> V8Console::memoryAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
+{
+    INC_STATS("DOM.Console.memoryAccessorGetter");
+    Console* imp = V8Console::toNative(info.Holder());
+    return toV8(imp->memory());
+}
 
 } // namespace WebCore

WebCore/bindings/v8/custom/V8PerformanceCustom.cpp
@@ -28,13 +28,22 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-module window {
+#include "config.h"
 
-    // See: http://dev.w3.org/2006/webapi/WebTiming/
-    interface [Conditional=WEB_TIMING, OmitConstructor] Performance {
-        readonly attribute PerformanceNavigation navigation;
-        readonly attribute PerformanceTiming timing;
-        readonly attribute [V8CustomGetter] MemoryInfo memory;
-    };
+#include "V8Performance.h"
 
+#include "Performance.h"
+#include "V8Binding.h"
+#include "V8BindingMacros.h"
+#include "V8MemoryInfo.h"
+
+namespace WebCore {
+
+v8::Handle<v8::Value> V8Performance::memoryAccessorGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
+{
+    INC_STATS("DOM.Performance.memoryAccessorGetter");
+    Performance* imp = V8Performance::toNative(info.Holder());
+    return toV8(imp->memory());
 }
+
+} // namespace WebCore

WebCore/page/Console.idl
@@ -58,7 +58,7 @@ module window {
         [CustomArgumentHandling] void groupCollapsed();
         void groupEnd();
 
-        readonly attribute MemoryInfo memory;
+        readonly attribute [V8CustomGetter] MemoryInfo memory;
     };
 
 }