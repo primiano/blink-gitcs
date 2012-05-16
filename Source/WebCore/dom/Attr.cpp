primiano/blink-gitcs

commit 3e8e153eedfa3a128da36990fe309e13feb1a66a

RadioNodeList support in HTMLFormElement::elements https://bugs.webkit.org/show_bug.cgi?id=81854
Patch by Rakesh KN <rakesh.kn@motorola.com> on 2012-05-08
Reviewed by Ryosuke Niwa.

Source/WebCore:

Implement RadioNodeList support spec'ed at
http://www.whatwg.org/specs/web-apps/current-work/multipage/common-dom-interfaces.html#radionodelist

Test: fast/forms/form-collection-radio-node-list.html

* CMakeLists.txt:
Added entries for new files.
* DerivedSources.cpp: Ditto.
* DerivedSources.make: Ditto.
* DerivedSources.pri: Ditto.
* GNUmakefile.list.am: Ditto.
* Target.pri: Ditto.
* WebCore.gypi: Ditto.
* WebCore.vcproj/WebCore.vcproj: Ditto.
* WebCore.xcodeproj/project.pbxproj: Ditto.
* bindings/js/JSHTMLCollectionCustom.cpp:
(WebCore::getNamedItems):
Modified to create RadioNodeList object when FormControlCollection has more than
one element of same name/id.
* bindings/scripts/CodeGeneratorJS.pm:
(GenerateImplementation):
Added code to include Node.h and JSNode.h in JSRadioNodeElement.cpp.
* bindings/v8/custom/V8HTMLCollectionCustom.cpp:
(WebCore::getNamedItems):
Modified to create RadioNodeList object when FormControlCollection has more than
one element of same name/id.
* dom/Node.cpp:
(WebCore::Node::invalidateNodeListsCacheAfterAttributeChanged):
Invalidate lists even for change in id, type, checked attributes.
(WebCore::NodeListsNodeData::invalidateCachesThatDependOnAttributes):
Invalidate radioNodeList cache.
(WebCore::NodeListsNodeData::isEmpty):
Changes for radioNodeList.
(WebCore::Node::radioNodeList):
Creates if needed a RadioNodeList and adds it to the cache.
(WebCore::Node::removeCachedRadioNodeList):
Removes a cached radioNodeList.
* dom/Node.h: Ditto
* dom/NodeRareData.h:
(WebCore):
(NodeListsNodeData):
Added radioNodeList list.
* html/CollectionType.h:
Added new FormControls type.
* html/HTMLCollection.cpp:
(WebCore::HTMLCollection::shouldIncludeChildren):
(WebCore::HTMLCollection::isAcceptableElement):
Handle FormControls collection type.

Source/WebCore/dom/Node.cpp
@@ -77,6 +77,7 @@
 #include "PlatformWheelEvent.h"
 #include "ProcessingInstruction.h"
 #include "ProgressEvent.h"
+#include "RadioNodeList.h"
 #include "RegisteredEventListener.h"
 #include "RenderBlock.h"
 #include "RenderBox.h"
@@ -991,6 +992,9 @@ void Node::invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName& at
         && attrName != itempropAttr
         && attrName != itemtypeAttr
 #endif
+        && attrName != idAttr
+        && attrName != typeAttr
+        && attrName != checkedAttr
         && attrName != nameAttr
         && attrName != forAttr)
         return;
@@ -2329,6 +2333,10 @@ void NodeListsNodeData::invalidateCachesThatDependOnAttributes()
     for (MicroDataItemListCache::iterator it = m_microDataItemListCache.begin(); it != itemListCacheEnd; ++it)
         it->second->invalidateCache();
 #endif
+
+    RadioNodeListCache::iterator radioNodeListCacheEnd = m_radioNodeListCache.end();
+    for (RadioNodeListCache::iterator it = m_radioNodeListCache.begin(); it != radioNodeListCacheEnd; ++it)
+        it->second->invalidateCache();
 }
 
 bool NodeListsNodeData::isEmpty() const
@@ -2351,7 +2359,10 @@ bool NodeListsNodeData::isEmpty() const
 
     if (m_labelsNodeListCache)
         return false;
-    
+
+    if (!m_radioNodeListCache.isEmpty())
+        return false;
+
     return true;
 }
 
@@ -2956,6 +2967,31 @@ void NodeRareData::clearChildNodeListCache()
 {
     if (m_childNodeList)
         m_childNodeList->invalidateCache();
+}
+
+PassRefPtr<RadioNodeList> Node::radioNodeList(const AtomicString& name)
+{
+    ASSERT(hasTagName(formTag));
+
+    NodeListsNodeData* nodeLists = ensureRareData()->ensureNodeLists(this);
+
+    NodeListsNodeData::RadioNodeListCache::AddResult result = nodeLists->m_radioNodeListCache.add(name, 0);
+    if (!result.isNewEntry)
+        return PassRefPtr<RadioNodeList>(result.iterator->second);
+
+    RefPtr<RadioNodeList> list = RadioNodeList::create(name, toElement(this));
+    result.iterator->second = list.get();
+    return list.release();
+}
+
+void Node::removeCachedRadioNodeList(RadioNodeList* list, const AtomicString& name)
+{
+    ASSERT(rareData());
+    ASSERT(rareData()->nodeLists());
+
+    NodeListsNodeData* data = rareData()->nodeLists();
+    ASSERT_UNUSED(list, list == data->m_radioNodeListCache.get(name));
+    data->m_radioNodeListCache.remove(name);
 }
 
 } // namespace WebCore

Source/WebCore/dom/Node.h
@@ -75,6 +75,7 @@ class PlatformKeyboardEvent;
 class PlatformMouseEvent;
 class PlatformWheelEvent;
 class QualifiedName;
+class RadioNodeList;
 class RegisteredEventListener;
 class RenderArena;
 class RenderBox;
@@ -561,6 +562,9 @@ public:
     void removeCachedLabelsNodeList(DynamicSubtreeNodeList*);
 
     void removeCachedChildNodeList();
+
+    PassRefPtr<RadioNodeList> radioNodeList(const AtomicString&);
+    void removeCachedRadioNodeList(RadioNodeList*, const AtomicString&);
 
     PassRefPtr<NodeList> getElementsByTagName(const AtomicString&);
     PassRefPtr<NodeList> getElementsByTagNameNS(const AtomicString& namespaceURI, const AtomicString& localName);

LayoutTests/fast/forms/form-collection-radio-node-list-expected.txt
@@ -1,0 +1,60 @@
+This test is for RadioNodeList specified at http://www.whatwg.org/specs/web-apps/current-work/multipage/common-dom-interfaces.html#radionodelist
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+ 
+                                       
+
+
+PASS owner.elements.length is 23
+PASS radioNodeList.length is 4
+PASS radioNodeList[0].value is 'searching'
+PASS radioNodeList[1].value is '123'
+PASS radioNodeList[2].value is 'inputRadioValue'
+PASS radioNodeList[3].value is 'buttonValue'
+
+Changing the input value to check RadioNodeList is live view of FormCollection
+PASS radioNodeList[1].value is '456'
+
+Checking value IDL attribute on the RadioNodeList
+PASS radioNodeList.value is ""
+PASS radioNodeList.value = "inputRadioValue"; radioNodeList[2].checked is true
+PASS Object.prototype.toString.call(radioNodeList[2]) is '[object HTMLInputElement]'
+PASS radioNodeList[2].type is 'radio'
+PASS radioNodeList.value is ""
+PASS radioNodeList[2].checked = true; radioNodeList.value is 'inputRadioValue'
+
+Check RadioNodeList is updated after adding a new element
+PASS owner.appendChild(newElement); radioNodeList.length is 5
+PASS radioNodeList[4].value is 'new element'
+
+Check RadioNodeList is updated after remove an element
+PASS owner.removeChild(newElement); radioNodeList.length is 4
+PASS radioNodeList[3].value is 'buttonValue'
+
+Check RadioNodeList is updated after adding a new element
+PASS container.appendChild(nonSubtreeElement); radioNodeList.length is 5
+PASS owner.elements.length is 24
+PASS radioNodeList[4].value is 'non subtree element'
+
+Check RadioNodeList is updated after change in id, type and checked state of an element
+PASS radioNodeList.length is 5
+After changing the id
+PASS radioNodeList.length is 4
+PASS elementsList[13].checked = false; radioNodeList.value is ""
+PASS elementsList[13].checked = true; radioNodeList.value is 'inputRadioValue'
+PASS elementsList[13].type = "date"; radioNodeList.value is ''
+
+Check second RadioNodeList is also created properly
+PASS radioNodeList2.length is 3
+PASS radioNodeList2[0].type is 'text'
+PASS radioNodeList2[1].type is 'email'
+PASS radioNodeList2[2].type is 'reset'
+After changing the id
+PASS radioNodeList2.length is 2
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/form-collection-radio-node-list.html
@@ -1,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../js/resources/js-test-pre.js"></script>
+<p id="description"></p>
+<div id="divId">
+<form id="form1">
+    <button id=button1></button>
+    <fieldset id=fieldset1><legend id=legend1></legend></fieldset>
+    <input id=inputhidden type=hidden>
+    <input id=commoninput type=text>
+    <input id=inputcommon type=search value=searching>
+    <input id=inputurl type=url>
+    <input id=commoninput type=email>
+    <input id=inputpassword type=password>
+    <input id=inputdate type=date>
+    <input id=numberId name=inputcommon type=number value=123>
+    <input id=inputrange type=range>
+    <input id=inputcolor type=color>
+    <input id=inputcheckbox type=checkbox>
+    <input id=inputcommon type=radio value="inputRadioValue">
+    <input id=inputfile type=file>
+    <input id=inputsubmit type=submit>
+    <input id=inputcommon type=image>
+    <input id=commoninput type=reset>
+    <input id=inputcommon type=button value=buttonValue>
+    <keygen id=keygen1></keygen>
+    <label id=label1></label>
+    <meter id=meter1></meter>
+    <object id=object1></object>
+    <output id=output1></output>
+    <progress id=progress1></progress>
+    <select id=select1>
+        <optgroup id=optgroup1>group1</optgroup>
+        <option id=option1>option1</option>
+    </select>
+    <textarea id=textarea1></textarea>
+</form>
+</div>
+<div id="console"></div>
+<script>
+description("This test is for RadioNodeList specified at http://www.whatwg.org/specs/web-apps/current-work/multipage/common-dom-interfaces.html#radionodelist ");
+debug("");
+var owner = document.getElementById('form1');
+
+shouldBe('owner.elements.length', '23');
+
+var elementsList = owner.elements;
+var radioNodeList = elementsList.namedItem("inputcommon");
+shouldBe('radioNodeList.length', '4');
+
+shouldBe('radioNodeList[0].value', "'searching'");
+shouldBe('radioNodeList[1].value', "'123'");
+shouldBe('radioNodeList[2].value', "'inputRadioValue'");
+shouldBe('radioNodeList[3].value', "'buttonValue'");
+
+debug("");
+debug("Changing the input value to check RadioNodeList is live view of FormCollection");
+document.getElementById("numberId").value = 456;
+shouldBe('radioNodeList[1].value', "'456'");
+
+debug("");
+debug("Checking value IDL attribute on the RadioNodeList");
+shouldBe('radioNodeList.value', '""');
+shouldBe('radioNodeList.value = "inputRadioValue"; radioNodeList[2].checked', 'true');
+shouldBe('Object.prototype.toString.call(radioNodeList[2])', "'[object HTMLInputElement]'");
+shouldBe('radioNodeList[2].type', "'radio'");
+
+radioNodeList[2].checked = false;
+shouldBe('radioNodeList.value', '""');
+shouldBe('radioNodeList[2].checked = true; radioNodeList.value', "'inputRadioValue'");
+
+var newElement = document.createElement("input");
+newElement.setAttribute("type", "text");
+newElement.setAttribute("value", "new element");
+newElement.setAttribute("id", "inputcommon");
+
+debug("");
+debug("Check RadioNodeList is updated after adding a new element");
+shouldBe('owner.appendChild(newElement); radioNodeList.length', '5');
+shouldBe('radioNodeList[4].value', "'new element'");
+
+debug("");
+debug("Check RadioNodeList is updated after remove an element");
+shouldBe('owner.removeChild(newElement); radioNodeList.length', '4');
+shouldBe('radioNodeList[3].value', "'buttonValue'");
+
+var nonSubtreeElement = document.createElement("input");
+nonSubtreeElement.setAttribute("type", "text");
+nonSubtreeElement.setAttribute("value", "non subtree element");
+nonSubtreeElement.setAttribute("id", "inputcommon");
+nonSubtreeElement.setAttribute("form", "form1");
+
+var container = document.getElementById("divId");
+
+debug("");
+debug("Check RadioNodeList is updated after adding a new element");
+shouldBe('container.appendChild(nonSubtreeElement); radioNodeList.length', '5');
+shouldBe('owner.elements.length', '24');
+shouldBe('radioNodeList[4].value', "'non subtree element'");
+
+debug("");
+debug("Check RadioNodeList is updated after change in id, type and checked state of an element");
+shouldBe('radioNodeList.length', '5');
+debug("After changing the id");
+radioNodeList[4].id = "changedName";
+shouldBe('radioNodeList.length', '4');
+
+shouldBe('elementsList[13].checked = false; radioNodeList.value', '""');
+shouldBe('elementsList[13].checked = true; radioNodeList.value', "'inputRadioValue'");
+shouldBe('elementsList[13].type = "date"; radioNodeList.value', "''");
+
+debug("");
+debug("Check second RadioNodeList is also created properly");
+var radioNodeList2 = elementsList.namedItem("commoninput");
+shouldBe('radioNodeList2.length', '3');
+
+shouldBe('radioNodeList2[0].type', "'text'");
+shouldBe('radioNodeList2[1].type', "'email'");
+shouldBe('radioNodeList2[2].type', "'reset'");
+radioNodeList2[2].id = "idChanged";
+debug("After changing the id");
+shouldBe('radioNodeList2.length', '2');
+debug("");
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

Source/WebCore/CMakeLists.txt
@@ -331,6 +331,7 @@ SET(WebCore_IDL_FILES
     html/ImageData.idl
     html/MediaController.idl
     html/MediaError.idl
+    html/RadioNodeList.idl
     html/TextMetrics.idl
     html/TimeRanges.idl
     html/ValidityState.idl
@@ -855,6 +856,7 @@ SET(WebCore_SOURCES
     html/PasswordInputType.cpp
     html/PluginDocument.cpp
     html/RadioInputType.cpp
+    html/RadioNodeList.cpp
     html/RangeInputType.cpp
     html/ResetInputType.cpp
     html/SearchInputType.cpp

Source/WebCore/DerivedSources.cpp
@@ -265,6 +265,7 @@
 #include "JSPositionErrorCallback.cpp"
 #include "JSProcessingInstruction.cpp"
 #include "JSProgressEvent.cpp"
+#include "JSRadioNodeList.cpp"
 #include "JSRange.cpp"
 #include "JSRangeException.cpp"
 #include "JSRect.cpp"

Source/WebCore/DerivedSources.make
@@ -335,6 +335,7 @@ BINDING_IDLS = \
     $(WebCore)/html/ImageData.idl \
     $(WebCore)/html/MediaController.idl \
     $(WebCore)/html/MediaError.idl \
+    $(WebCore)/html/RadioNodeList.idl \
     $(WebCore)/html/TextMetrics.idl \
     $(WebCore)/html/TimeRanges.idl \
     $(WebCore)/html/ValidityState.idl \

Source/WebCore/DerivedSources.pri
@@ -383,6 +383,7 @@ IDL_BINDINGS += \
     $$PWD/html/ImageData.idl \
     $$PWD/html/MediaController.idl \
     $$PWD/html/MediaError.idl \
+    $$PWD/html/RadioNodeList.idl \
     $$PWD/html/TextMetrics.idl \
     $$PWD/html/TimeRanges.idl \
     $$PWD/html/ValidityState.idl \

commit 078c4a61700a4718b7701ae3b9545e6eef2c6d91

Fix perf regression from r116487 https://bugs.webkit.org/show_bug.cgi?id=86680
Reviewed by Ryosuke Niwa.

http://trac.webkit.org/changeset/116487 caused a 6% regression on
Dromaeo's dom-attr test. The issue is that we invalidated NodeList
caches whenever an id/checked/type attribute changed.

First, we don't need to invalidate on checked/type since that only
affects the values return by NodeList items, not the list of items.
Second, we only need to invalidate NodeList caches when an id attribute
changes on a FormControlElement.

Incidentally, we also don't need to invalidate caches for changes
to attributes that don't have an ownerElement.

No new tests. This is strictly a performance improvement.

* dom/Attr.cpp:
(WebCore::Attr::setValue):
(WebCore::Attr::childrenChanged):
* dom/Element.cpp:
(WebCore::Element::attributeChanged):
* dom/Node.cpp:
(WebCore::Node::invalidateNodeListsCacheAfterAttributeChanged):
* dom/Node.h:
(Node):

git-svn-id: svn://svn.chromium.org/blink/trunk@117353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/dom/Attr.cpp
@@ -119,7 +119,7 @@ void Attr::setValue(const AtomicString& value)
     createTextChild();
     m_ignoreChildrenChanged--;
 
-    invalidateNodeListsCacheAfterAttributeChanged(m_name);
+    invalidateNodeListsCacheAfterAttributeChanged(m_name, m_element);
 }
 
 void Attr::setValue(const AtomicString& value, ExceptionCode&)
@@ -162,7 +162,7 @@ void Attr::childrenChanged(bool, Node*, Node*, int)
     if (m_ignoreChildrenChanged > 0)
         return;
 
-    invalidateNodeListsCacheAfterAttributeChanged(qualifiedName());
+    invalidateNodeListsCacheAfterAttributeChanged(qualifiedName(), m_element);
 
     // FIXME: We should include entity references in the value
 

Source/WebCore/dom/Element.cpp
@@ -716,7 +716,7 @@ void Element::attributeChanged(const Attribute& attribute)
             setNeedsStyleRecalc();
     }
 
-    invalidateNodeListsCacheAfterAttributeChanged(attribute.name());
+    invalidateNodeListsCacheAfterAttributeChanged(attribute.name(), this);
 
     if (!AXObjectCache::accessibilityEnabled())
         return;

Source/WebCore/dom/Node.cpp
@@ -976,7 +976,7 @@ void Node::unregisterDynamicSubtreeNodeList(DynamicSubtreeNodeList* list)
     removeNodeListCacheIfPossible(this, data);
 }
 
-void Node::invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName& attrName)
+void Node::invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName& attrName, Element* attributeOwnerElement)
 {
     if (hasRareData() && isAttributeNode()) {
         NodeRareData* data = rareData();
@@ -984,18 +984,25 @@ void Node::invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName& at
         data->clearChildNodeListCache();
     }
 
-    // This list should be sync'ed with NodeListsNodeData.
+    // Modifications to attributes that are not associated with an Element can't invalidate NodeList caches.
+    if (!attributeOwnerElement)
+        return;
+
+    // FIXME: Move the list of attributes each NodeList type cares about to be a static on the
+    // appropriate NodeList class. Then use those lists here and in invalidateCachesThatDependOnAttributes
+    // to only invalidate the cache types that depend on the attribute that changed.
+    // FIXME: Keep track of when we have no caches of a given type so that we can avoid the for-loop
+    // below even if a related attribute changed (e.g. if we have no RadioNodeLists, we don't need
+    // to invalidate any caches when id attributes change.)
     if (attrName != classAttr
 #if ENABLE(MICRODATA)
         && attrName != itemscopeAttr
         && attrName != itempropAttr
         && attrName != itemtypeAttr
 #endif
-        && attrName != idAttr
-        && attrName != typeAttr
-        && attrName != checkedAttr
         && attrName != nameAttr
-        && attrName != forAttr)
+        && attrName != forAttr
+        && (attrName != idAttr || !attributeOwnerElement->isFormControlElement()))
         return;
 
     if (!treeScope()->hasNodeListCaches())

Source/WebCore/dom/Node.h
@@ -557,7 +557,7 @@ public:
 
     void registerDynamicSubtreeNodeList(DynamicSubtreeNodeList*);
     void unregisterDynamicSubtreeNodeList(DynamicSubtreeNodeList*);
-    void invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName&);
+    void invalidateNodeListsCacheAfterAttributeChanged(const QualifiedName&, Element* attributeOwnerElement);
     void invalidateNodeListsCacheAfterChildrenChanged();
     void removeCachedClassNodeList(ClassNodeList*, const String&);
 