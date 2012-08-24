primiano/blink-gitcs

commit 40388b7edd4d718e446b92716c679aaf4496891a

Web Inspector: resolve URLs upon creation, get rid of populateHrefContextMenu https://bugs.webkit.org/show_bug.cgi?id=94900
Reviewed by Vsevolod Vlasov.

- This change makes sure we resolve URLs upon adding the to the UI. Then we treat them as absolute links in context menu, etc.
- There is no need to override context menu for links anymore - system menu will do.

* inspector/front-end/AuditResultView.js:
(WebInspector.AuditResultView):
* inspector/front-end/ConsoleView.js:
* inspector/front-end/DOMAgent.js:
(WebInspector.DOMNode.prototype.descendantUserPropertyCount):
(WebInspector.DOMNode.prototype.resolveURL):
* inspector/front-end/ElementsPanel.js:
* inspector/front-end/ElementsTreeOutline.js:
(WebInspector.ElementsTreeOutline.prototype.populateContextMenu):
(WebInspector.ElementsTreeElement.prototype._buildAttributeDOM):
* inspector/front-end/HandlerRegistry.js:
(WebInspector.HandlerRegistry.prototype.appendApplicableItems):
(WebInspector.HandlerRegistry.prototype._appendContentProviderItems):
(WebInspector.HandlerRegistry.prototype._appendHrefItems):
* inspector/front-end/ParsedURL.js:
(WebInspector.ParsedURL.completeURL):
* inspector/front-end/ResourceUtils.js:
(WebInspector.linkifyRequestAsNode):
* inspector/front-end/StylesSidebarPane.js:
(WebInspector.StylesSidebarPane):
(WebInspector.StylePropertyTreeElement.prototype.updateTitle.linkifyURL):
* inspector/front-end/UIUtils.js:

git-svn-id: svn://svn.chromium.org/blink/trunk@126572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/inspector/front-end/DOMAgent.js
@@ -736,6 +736,22 @@ WebInspector.DOMNode.prototype = {
     descendantUserPropertyCount: function(name)
     {
         return this._descendantUserPropertyCounters && this._descendantUserPropertyCounters[name] ? this._descendantUserPropertyCounters[name] : 0;
+    },
+
+    /**
+     * @param {string} url
+     * @return {?string}
+     */
+    resolveURL: function(url)
+    {
+        if (!url)
+            return url;
+        for (var frameOwnerCandidate = this; frameOwnerCandidate; frameOwnerCandidate = frameOwnerCandidate.parentNode) {
+            if (frameOwnerCandidate.documentURL)
+                return WebInspector.ParsedURL.completeURL(frameOwnerCandidate.documentURL, url);
+        }
+        console.error("Could not resolve DOM URL: " + url);
+        return null;
     }
 }
 

LayoutTests/inspector/elements/elements-panel-rewrite-href-expected.txt
@@ -1,5 +1,6 @@
 Tests that src and href element targets are rewritten properly.
 
+null
 ,bogusSheet1.css
 inspector,elements,bogusSheet2.css
 tests,inspector,inspector-test.js

LayoutTests/inspector/elements/elements-panel-rewrite-href.html
@@ -19,7 +19,7 @@ function test()
             var node = innerMapping[nodeId];
             if (node.nodeName() === "LINK" || node.nodeName() === "SCRIPT") {
                 var segments = [];
-                var href = WebInspector.resourceURLForRelatedNode(node, node.getAttribute("src") || node.getAttribute("href"));
+                var href = node.resolveURL(node.getAttribute("src") || node.getAttribute("href"));
                 if (!href) {
                     segments.push("<empty>");
                     continue;
@@ -32,6 +32,8 @@ function test()
                     segments.push(split[i]);
                 InspectorTest.addResult(segments.reverse());
             }
+            if (node.nodeName() === "A")
+                InspectorTest.addResult(node.resolveURL(node.getAttribute("href")));
         }
         InspectorTest.completeTest();
     }
@@ -44,6 +46,8 @@ function test()
 <p>
 Tests that src and href element targets are rewritten properly.
 </p>
+
+<a style="display:none" href=" javascript:alert('foo') "></a>
 
 </body>
 </html>

LayoutTests/inspector/styles/styles-url-linkify-expected.txt
@@ -19,7 +19,6 @@ http://example.com/foo?a=b
 http://example.com/cat.jpeg
 http://example.com/cat.jpeg
 data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEIAAABCAgMAAACeOuh7AAAABGdBTUEAAK/INwWK6QAAAAlQTFRF////AAAA////fu+PTwAAAAF0Uk5TAEDm2GYAAACHSURBVDjLxdLbDYAgDAVQGELn0R3oEHYf2KGdUqtE46OFRCP3oyTng1xCnWsaD5JRRtCkQ2YmkBkHRXqWJBn0j0TICbrsWVoWhRShCdcGyZCtHxMaUnVPRZ9KSbmBJdsX2vJVnwqRD0Rb4rpzgIbE/AI5NTnWAMvy5l0dXrfuLh5OCe5BmmYGXhTUxlQ5xJ8AAAAASUVORK5CYII=
-javascript:alert('foo');
 null
 blob:http%3A//example.com/f91b7b00-00-0000-0000-3b7c87055d7a
 blob:http%3A//example.com/f91b7b00-00-0000-0000-3b7c87055d7a

LayoutTests/inspector/styles/styles-url-linkify.html
@@ -35,9 +35,8 @@ function test()
     const blobURL = "blob:http%3A//example.com/f91b7b00-00-0000-0000-3b7c87055d7a";
     completeURL("https://example.com/foo", dataURL);
     completeURL("http://example.com/foo", "javascript:alert('foo');");
-    InspectorTest.addResult(WebInspector.resourceURLForRelatedNode(null, " javascript:alert('foo'); "));
     completeURL("http://example.com/foo", blobURL);
-    InspectorTest.addResult(WebInspector.resourceURLForRelatedNode(null, blobURL));
+    InspectorTest.addResult(WebInspector.ParsedURL.completeURL(blobURL));
 
     function dumpHref(dumpLinkClass)
     {

Source/WebCore/inspector/front-end/AdvancedSearchController.js
@@ -500,7 +500,7 @@ WebInspector.FileBasedSearchResultsPane.prototype = {
     {
         var anchor = document.createElement("a");
         anchor.preferredPanel = "scripts";
-        anchor.href = uiSourceCode.url;
+        anchor.href = sanitizeHref(uiSourceCode.url);
         anchor.uiSourceCode = uiSourceCode;
         anchor.lineNumber = lineNumber;
         return anchor;

Source/WebCore/inspector/front-end/AuditFormatters.js
@@ -60,7 +60,7 @@ WebInspector.AuditFormatters.Registry = {
     url: function(url, displayText, allowExternalNavigation)
     {
         var a = document.createElement("a");
-        a.href = url;
+        a.href = sanitizeHref(url);
         a.title = url;
         a.textContent = displayText || url;
         if (allowExternalNavigation)

Source/WebCore/inspector/front-end/AuditResultView.js
@@ -43,17 +43,9 @@ WebInspector.AuditResultView = function(categoryResults)
     categoryResults.sort(categorySorter);
     for (var i = 0; i < categoryResults.length; ++i)
         this.element.appendChild(new WebInspector.AuditCategoryResultPane(categoryResults[i]).element);
-
-    this.element.addEventListener("contextmenu", this._contextMenuEventFired.bind(this), true);
 }
 
 WebInspector.AuditResultView.prototype = {
-    _contextMenuEventFired: function(event)
-    {
-        var contextMenu = new WebInspector.ContextMenu();
-        if (WebInspector.populateHrefContextMenu(contextMenu, null, event))
-            contextMenu.show(event);
-    }
 }
 
 WebInspector.AuditResultView.prototype.__proto__ = WebInspector.View.prototype;

commit 31b3ddd1ef5dc2bec6e3c7081fd4176508e15ff4

Not reviewed: fixing inspector/elements/iframe-load-event.html broken by r126572.
* inspector/front-end/DOMAgent.js:
(WebInspector.DOMNode.prototype.resolveURL):

git-svn-id: svn://svn.chromium.org/blink/trunk@126576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/inspector/front-end/DOMAgent.js
@@ -750,7 +750,6 @@ WebInspector.DOMNode.prototype = {
             if (frameOwnerCandidate.documentURL)
                 return WebInspector.ParsedURL.completeURL(frameOwnerCandidate.documentURL, url);
         }
-        console.error("Could not resolve DOM URL: " + url);
         return null;
     }
 }