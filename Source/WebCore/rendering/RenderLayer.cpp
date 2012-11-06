primiano/blink-gitcs

commit a634b85302734bdd03f5b5733aaf5269ea33266a

Fix RenderGeometryMap assertion when layers are scrolled during layout https://bugs.webkit.org/show_bug.cgi?id=101292
Reviewed by Beth Dakin.

Source/WebCore:

When we set RenderLayer scroll positions as part of layout, we don't want
to update compositing layers right away. Updating compositing layers
requires that the entire layer tree has been updated from renderers,
so that the geometry of all RenderLayers can be trusted. When this state
was violated, RenderGeometryMap asserts.

Fix by bailing from updateCompositingLayersAfterScroll() if FrameView
tells us that we're doing layout. A full update of the compositing layers
will happen later anyway.

Test: compositing/geometry/geometry-map-scroll-during-layout-assertion.html

* rendering/RenderLayer.cpp:
(WebCore::frameViewFromLayer):
(WebCore::RenderLayer::updateCompositingLayersAfterScroll):

LayoutTests:

Test that hit the assertion before the fix.

* compositing/geometry/geometry-map-scroll-during-layout-assertion-expected.txt: Added.
* compositing/geometry/geometry-map-scroll-during-layout-assertion.html: Added.

git-svn-id: svn://svn.chromium.org/blink/trunk@133628 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/rendering/RenderLayer.cpp
@@ -1885,10 +1885,25 @@ void RenderLayer::scrollRectToVisible(const LayoutRect& rect, const ScrollAlignm
         frameView->resumeScheduledEvents();
 }
 
+static FrameView* frameViewFromLayer(const RenderLayer* layer)
+{
+    Frame* frame = layer->renderer()->frame();
+    if (!frame)
+        return 0;
+
+    return frame->view();
+}
+
 void RenderLayer::updateCompositingLayersAfterScroll()
 {
 #if USE(ACCELERATED_COMPOSITING)
     if (compositor()->inCompositingMode()) {
+        // If we're in the middle of layout, we'll just update compositiong once layout has finished.
+        if (FrameView* frameView = frameViewFromLayer(this)) {
+            if (frameView->isInLayout())
+                return;
+        }
+
         // Our stacking context is guaranteed to contain all of our descendants that may need
         // repositioning, so update compositing layers from there.
         if (RenderLayer* compositingAncestor = stackingContext()->enclosingCompositingLayer()) {

LayoutTests/compositing/geometry/geometry-map-scroll-during-layout-assertion-expected.txt
@@ -1,0 +1,3 @@
+This test should not hit an assertion in debug builds.
+
+

LayoutTests/compositing/geometry/geometry-map-scroll-during-layout-assertion.html
@@ -1,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        #wpadminbar {
+            height: 28px;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            min-width: 600px;
+            z-index: 99999;
+            background-color: gray;
+        }
+        
+        #overflow {
+            overflow: scroll;
+            height: 300px;
+        }
+        
+        #contents {
+            height: 500px;
+            background-color: green;
+        }
+    </style>
+    <script>
+        if (window.testRunner)
+            testRunner.dumpAsText();
+
+        function doTest()
+        {
+            document.getElementById('overflow').scrollTop = 200;
+            var contents = document.getElementById('contents');
+            contents.style.height = '200' + 'px';
+        }
+        window.addEventListener('load', doTest, false);
+    </script>
+</head>
+<body>
+    <p>This test should not hit an assertion in debug builds.</p>
+    <div id="wpadminbar" class="" role="navigation"></div>
+    <div id="overflow" style="width: 430px; margin-left: 100px; margin-top: 100px;">
+        <div id="contents">
+        </div>
+    </div>
+
+</body>
+</html>

commit 16d3f665bccf94dc6e88a37a52d5f42eb2200742

Fix build warning in RenderLayer.cpp caused by r133628 https://bugs.webkit.org/show_bug.cgi?id=101401
Patch by KyungTae Kim <ktf.kim@samsung.com> on 2012-11-06
Reviewed by Simon Fraser.

The static function WebCore::frameViewFromLayer is only used when USE(ACCELERATED_COMPOSITING),
so define it only that case for removing -Wunused-function warning

* rendering/RenderLayer.cpp:
(WebCore):

git-svn-id: svn://svn.chromium.org/blink/trunk@133683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/WebCore/rendering/RenderLayer.cpp
@@ -1885,6 +1885,7 @@ void RenderLayer::scrollRectToVisible(const LayoutRect& rect, const ScrollAlignm
         frameView->resumeScheduledEvents();
 }
 
+#if USE(ACCELERATED_COMPOSITING)
 static FrameView* frameViewFromLayer(const RenderLayer* layer)
 {
     Frame* frame = layer->renderer()->frame();
@@ -1893,6 +1894,7 @@ static FrameView* frameViewFromLayer(const RenderLayer* layer)
 
     return frame->view();
 }
+#endif
 
 void RenderLayer::updateCompositingLayersAfterScroll()
 {