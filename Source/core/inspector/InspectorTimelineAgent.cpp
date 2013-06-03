primiano/blink-gitcs

commit a22da94d7385c2b057ac86e7c1a263947007c2dd

Pushed main frame check from InspectorInstrumentation::domContentLoadedEventFired into agents. Also unified agent method signatures for loadEventFired.
BUG=232160

Review URL: https://chromiumcodereview.appspot.com/15467005

git-svn-id: svn://svn.chromium.org/blink/trunk@150768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/inspector/InspectorTimelineAgent.cpp
@@ -553,13 +553,16 @@ void InspectorTimelineAgent::timeEnd(Frame* frame, const String& message)
     appendRecord(TimelineRecordFactory::createTimeStampData(message), TimelineRecordType::TimeEnd, true, frame);
 }
 
-void InspectorTimelineAgent::didMarkDOMContentEvent(Frame* frame)
+void InspectorTimelineAgent::domContentLoadedEventFired(Frame* frame)
 {
+    if (frame->page()->mainFrame() != frame)
+        return;
+
     bool isMainFrame = frame && m_pageAgent && (frame == m_pageAgent->mainFrame());
     appendRecord(TimelineRecordFactory::createMarkData(isMainFrame), TimelineRecordType::MarkDOMContent, false, frame);
 }
 
-void InspectorTimelineAgent::didMarkLoadEvent(Frame* frame)
+void InspectorTimelineAgent::loadEventFired(Frame* frame)
 {
     bool isMainFrame = frame && m_pageAgent && (frame == m_pageAgent->mainFrame());
     appendRecord(TimelineRecordFactory::createMarkData(isMainFrame), TimelineRecordType::MarkLoad, false, frame);

Source/core/inspector/InspectorAgent.cpp
@@ -133,8 +133,11 @@ void InspectorAgent::disable(ErrorString*)
     m_state->setBoolean(InspectorAgentState::inspectorAgentEnabled, false);
 }
 
-void InspectorAgent::domContentLoadedEventFired()
+void InspectorAgent::domContentLoadedEventFired(Frame* frame)
 {
+    if (frame->page()->mainFrame() != frame)
+        return;
+
     m_injectedScriptManager->injectedScriptHost()->clearInspectedObjects();
 }
 

Source/core/inspector/InspectorAgent.h
@@ -76,7 +76,7 @@ public:
     void didClearWindowObjectInWorld(Frame*, DOMWrapperWorld*);
 
     void didCommitLoad();
-    void domContentLoadedEventFired();
+    void domContentLoadedEventFired(Frame*);
 
     bool hasFrontend() const { return m_frontend; }
 

Source/core/inspector/InspectorDOMAgent.cpp
@@ -1577,17 +1577,20 @@ bool InspectorDOMAgent::isWhitespace(Node* node)
     return node && node->nodeType() == Node::TEXT_NODE && node->nodeValue().stripWhiteSpace().length() == 0;
 }
 
-void InspectorDOMAgent::mainFrameDOMContentLoaded()
+void InspectorDOMAgent::domContentLoadedEventFired(Frame* frame)
 {
+    if (frame->page()->mainFrame() != frame)
+        return;
+
     // Re-push document once it is loaded.
     discardFrontendBindings();
     if (m_state->getBoolean(DOMAgentState::documentRequested))
         m_frontend->documentUpdated();
 }
 
-void InspectorDOMAgent::loadEventFired(Document* document)
+void InspectorDOMAgent::loadEventFired(Frame* frame)
 {
-    Element* frameOwner = document->ownerElement();
+    Element* frameOwner = frame->document()->ownerElement();
     if (!frameOwner)
         return;
 

Source/core/inspector/InspectorDOMAgent.h
@@ -162,8 +162,8 @@ public:
     void setDocument(Document*);
     void releaseDanglingNodes();
 
-    void mainFrameDOMContentLoaded();
-    void loadEventFired(Document*);
+    void domContentLoadedEventFired(Frame*);
+    void loadEventFired(Frame*);
 
     void didInsertDOMNode(Node*);
     void didRemoveDOMNode(Node*);

Source/core/inspector/InspectorInstrumentation.cpp
@@ -797,34 +797,28 @@ void didReceiveScriptResponseImpl(InstrumentingAgents* instrumentingAgents, unsi
 void domContentLoadedEventFiredImpl(InstrumentingAgents* instrumentingAgents, Frame* frame)
 {
     if (InspectorTimelineAgent* timelineAgent = instrumentingAgents->inspectorTimelineAgent())
-        timelineAgent->didMarkDOMContentEvent(frame);
-
-    if (frame->page()->mainFrame() != frame)
-        return;
+        timelineAgent->domContentLoadedEventFired(frame);
 
     if (InspectorAgent* inspectorAgent = instrumentingAgents->inspectorAgent())
-        inspectorAgent->domContentLoadedEventFired();
+        inspectorAgent->domContentLoadedEventFired(frame);
 
     if (InspectorDOMAgent* domAgent = instrumentingAgents->inspectorDOMAgent())
-        domAgent->mainFrameDOMContentLoaded();
+        domAgent->domContentLoadedEventFired(frame);
 
     if (InspectorPageAgent* pageAgent = instrumentingAgents->inspectorPageAgent())
-        pageAgent->domContentEventFired();
+        pageAgent->domContentLoadedEventFired(frame);
 }
 
 void loadEventFiredImpl(InstrumentingAgents* instrumentingAgents, Frame* frame)
 {
     if (InspectorDOMAgent* domAgent = instrumentingAgents->inspectorDOMAgent())
-        domAgent->loadEventFired(frame->document());
+        domAgent->loadEventFired(frame);
 
     if (InspectorTimelineAgent* timelineAgent = instrumentingAgents->inspectorTimelineAgent())
-        timelineAgent->didMarkLoadEvent(frame);
-
-    if (frame->page()->mainFrame() != frame)
-        return;
+        timelineAgent->loadEventFired(frame);
 
     if (InspectorPageAgent* pageAgent = instrumentingAgents->inspectorPageAgent())
-        pageAgent->loadEventFired();
+        pageAgent->loadEventFired(frame);
 }
 
 void frameDetachedFromParentImpl(InstrumentingAgents* instrumentingAgents, Frame* frame)

Source/core/inspector/InspectorPageAgent.cpp
@@ -817,13 +817,16 @@ void InspectorPageAgent::didClearWindowObjectInWorld(Frame* frame, DOMWrapperWor
         frame->script()->executeScript(m_scriptToEvaluateOnLoadOnce);
 }
 
-void InspectorPageAgent::domContentEventFired()
+void InspectorPageAgent::domContentLoadedEventFired(Frame* frame)
 {
+    if (frame->page()->mainFrame() != frame)
+        return;
+
     m_isFirstLayoutAfterOnLoad = true;
     m_frontend->domContentEventFired(currentTime());
 }
 
-void InspectorPageAgent::loadEventFired()
+void InspectorPageAgent::loadEventFired(Frame*)
 {
     m_frontend->loadEventFired(currentTime());
 }

Source/core/inspector/InspectorPageAgent.h
@@ -131,8 +131,8 @@ public:
 
     // InspectorInstrumentation API
     void didClearWindowObjectInWorld(Frame*, DOMWrapperWorld*);
-    void domContentEventFired();
-    void loadEventFired();
+    void domContentLoadedEventFired(Frame*);
+    void loadEventFired(Frame*);
     void frameNavigated(DocumentLoader*);
     void frameDetachedFromParent(Frame*);
     void loaderDetachedFromFrame(DocumentLoader*);

commit 143b67521a08feae958af208319f2f30d6519820

Fix timeline to send content loaded for all frames
We used to send DOMContentLoaded for all frames, however this was
regressed with r150768.
This mostly fixes timeline-script-tag-1.html, but we also filter-out
all rendering events (Layout & co) to deflake it.
Drive-by: remove suppression for timeline-network-resource.html

R=kaznacheev@chromium.org, pfeldman@chromium.org

Review URL: https://codereview.chromium.org/15745028

git-svn-id: svn://svn.chromium.org/blink/trunk@151663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/inspector/InspectorTimelineAgent.cpp
@@ -566,9 +566,6 @@ void InspectorTimelineAgent::stopConsoleTiming(Frame* frame, const String& messa
 
 void InspectorTimelineAgent::domContentLoadedEventFired(Frame* frame)
 {
-    if (frame->page()->mainFrame() != frame)
-        return;
-
     bool isMainFrame = frame && m_pageAgent && (frame == m_pageAgent->mainFrame());
     appendRecord(TimelineRecordFactory::createMarkData(isMainFrame), TimelineRecordType::MarkDOMContent, false, frame);
 }

LayoutTests/TestExpectations
@@ -886,8 +886,6 @@ webkit.org/b/111023 fast/loader/unload-hyperlink-targeted.html [ Pass Timeout ]
 webkit.org/b/111023 fast/dom/Window/Location/set-location-after-close.html [ Pass Timeout ]
 webkit.org/b/111044 fast/loader/display-image-unset-can-block-image-and-can-reload-in-place.html [ Pass Timeout ]
 webkit.org/b/111044 fast/loader/images-enabled-unset-can-block-image-and-can-reload-in-place.html [ Pass Timeout ]
-webkit.org/b/110907 [ Release ] inspector/timeline/timeline-script-tag-1.html [ Failure Pass ]
-webkit.org/b/110907 [ Debug ] inspector/timeline/timeline-script-tag-1.html [ Failure Pass Timeout ]
 
 webkit.org/b/70988 [ Mac Win Debug ] fast/forms/submit-to-blank-multiple-times.html [ Failure Pass ]
 
@@ -1665,7 +1663,6 @@ crbug.com/231929 http/tests/navigation/no-referrer-target-blank.html [ Failure ]
 crbug.com/231931 http/tests/security/local-user-CSS-from-remote.html [ Failure ]
 crbug.com/231932 http/tests/security/redirect-BLOCKED-to-localURL.html [ Failure ]
 crbug.com/231935 inspector/storage-panel-dom-storage-update.html [ Failure ]
-crbug.com/231937 inspector/timeline/timeline-network-resource.html [ Failure ]
 crbug.com/231940 storage/websql/sql-error-codes.html [ Failure ]
 crbug.com/231942 svg/dom/fuzz-path-parser.html [ Failure ]
 crbug.com/231859 virtual/softwarecompositing/plugins/invalidate_rect.html [ Failure ]

LayoutTests/inspector/timeline/timeline-script-tag-1-expected.txt
@@ -3,10 +3,7 @@ Tests the Timeline API instrumentation of an HTML script tag.
 
 
 ParseHTML
-----> ScheduleStyleRecalculation
-----> InvalidateLayout
 ParseHTML
-----> ScheduleStyleRecalculation
 ----> EvaluateScript
 --------> TimeStamp : SCRIPT TAG
 EvaluateScript Properties:
@@ -24,14 +21,10 @@ EvaluateScript Properties:
     usedHeapSize : <number>
 }
 ParseHTML
-----> InvalidateLayout
 ParseHTML
-----> InvalidateLayout
 ----> EventDispatch
 --------> FunctionCall
 ------------> FunctionCall
 ----> MarkLoad
-----> Layout
---------> Layout
 ----> MarkDOMContent
 

LayoutTests/inspector/timeline/timeline-script-tag-1.html
@@ -33,8 +33,9 @@ function test()
                 InspectorTest.printTimelineRecordProperties(record);
             else if (record.type === WebInspector.TimelineModel.RecordType.ParseHTML) {
                 var children = [];
+                var skipCategories = { rendering: true, painting: true };
                 for (var i = 0; i < record.children.length; ++i) {
-                    if (record.children[i].type !== WebInspector.TimelineModel.RecordType.RecalculateStyles)
+                    if (!skipCategories[WebInspector.TimelinePresentationModel.categoryForRecord(record.children[i]).name])
                         children.push(record.children[i]);
                 }
                 record.children = children;