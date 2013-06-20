primiano/blink-gitcs

commit b7ee7da3f965d23193ae0ef6250d65954ab36738

Move PolicyChecker::checkNavicationPolicy to DocumentLoader.
Delete PolicyChecker and PolicyCallback, as there's nothing meaningful left in
them. Remove some unused function parameters, and try to simplify a little bit
of FrameLoader's logic.

BUG=none
TEST=none

Review URL: https://codereview.chromium.org/14358007

git-svn-id: svn://svn.chromium.org/blink/trunk@148684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/FrameLoader.cpp
@@ -85,7 +85,6 @@
 #include "PageTransitionEvent.h"
 #include "PluginData.h"
 #include "PluginDocument.h"
-#include "PolicyChecker.h"
 #include "ProgressTracker.h"
 #include "ResourceHandle.h"
 #include "ResourceRequest.h"
@@ -204,7 +203,6 @@ private:
 FrameLoader::FrameLoader(Frame* frame, FrameLoaderClient* client)
     : m_frame(frame)
     , m_client(client)
-    , m_policyChecker(frame)
     , m_history(frame)
     , m_notifer(frame)
     , m_subframeLoader(frame)
@@ -1170,8 +1168,6 @@ void FrameLoader::loadURL(const KURL& newURL, const String& referrer, const Stri
         return;
     }
 
-    RefPtr<DocumentLoader> oldDocumentLoader = m_documentLoader;
-
     bool sameURL = shouldTreatURLAsSameAsCurrent(newURL);
     const String& httpMethod = request.httpMethod();
     
@@ -1179,11 +1175,8 @@ void FrameLoader::loadURL(const KURL& newURL, const String& referrer, const Stri
     // exactly the same so pages with '#' links and DHTML side effects
     // work properly.
     if (shouldPerformFragmentNavigation(isFormSubmission, httpMethod, newLoadType, newURL)) {
-        oldDocumentLoader->setTriggeringAction(action);
-        policyChecker()->stopCheck();
-        policyChecker()->setLoadType(newLoadType);
-        policyChecker()->checkNavigationPolicy(request, oldDocumentLoader.get(), formState.release(),
-            callContinueFragmentScrollAfterNavigationPolicy, this);
+        m_loadType = newLoadType;
+        checkNavigationPolicyAndContinueFragmentScroll(action);
     } else {
         // must grab this now, since this load may stop the previous load and clear this flag
         bool isRedirect = m_quickRedirectComing;
@@ -1297,45 +1290,24 @@ void FrameLoader::loadWithDocumentLoader(DocumentLoader* loader, FrameLoadType t
     if (m_frame->document())
         m_previousURL = m_frame->document()->url();
 
-    policyChecker()->setLoadType(type);
+    m_loadType = type;
     RefPtr<FormState> formState = prpFormState;
     bool isFormSubmission = formState;
 
     const KURL& newURL = loader->request().url();
     const String& httpMethod = loader->request().httpMethod();
 
-    if (shouldPerformFragmentNavigation(isFormSubmission, httpMethod, policyChecker()->loadType(), newURL)) {
-        RefPtr<DocumentLoader> oldDocumentLoader = m_documentLoader;
-        NavigationAction action(loader->request(), policyChecker()->loadType(), isFormSubmission);
-
-        oldDocumentLoader->setTriggeringAction(action);
-        policyChecker()->stopCheck();
-        policyChecker()->checkNavigationPolicy(loader->request(), oldDocumentLoader.get(), formState,
-            callContinueFragmentScrollAfterNavigationPolicy, this);
-    } else {
+    if (shouldPerformFragmentNavigation(isFormSubmission, httpMethod, type, newURL))
+        checkNavigationPolicyAndContinueFragmentScroll(NavigationAction(loader->request(), type, isFormSubmission));
+    else {
         if (Frame* parent = m_frame->tree()->parent())
             loader->setOverrideEncoding(parent->loader()->documentLoader()->overrideEncoding());
 
-        policyChecker()->stopCheck();
         setPolicyDocumentLoader(loader);
         if (loader->triggeringAction().isEmpty())
-            loader->setTriggeringAction(NavigationAction(loader->request(), policyChecker()->loadType(), isFormSubmission));
+            loader->setTriggeringAction(NavigationAction(loader->request(), type, isFormSubmission));
 
-        if (Element* ownerElement = m_frame->ownerElement()) {
-            // We skip dispatching the beforeload event if we've already
-            // committed a real document load because the event would leak
-            // subsequent activity by the frame which the parent frame isn't
-            // supposed to learn. For example, if the child frame navigated to
-            // a new URL, the parent frame shouldn't learn the URL.
-            if (!m_stateMachine.committedFirstRealDocumentLoad()
-                && !ownerElement->dispatchBeforeLoadEvent(loader->request().url().string())) {
-                continueLoadAfterNavigationPolicy(loader->request(), formState, false);
-                return;
-            }
-        }
-
-        policyChecker()->checkNavigationPolicy(loader->request(), loader, formState,
-            callContinueLoadAfterNavigationPolicy, this);
+        checkNavigationPolicyAndContinueLoad(formState);
     }
 }
 
@@ -1374,7 +1346,7 @@ bool FrameLoader::shouldReloadToHandleUnreachableURL(DocumentLoader* docLoader)
     if (unreachableURL.isEmpty())
         return false;
 
-    if (!isBackForwardLoadType(policyChecker()->loadType()))
+    if (!isBackForwardLoadType(m_loadType))
         return false;
 
     // We only treat unreachableURLs specially during the delegate callbacks
@@ -1383,9 +1355,7 @@ bool FrameLoader::shouldReloadToHandleUnreachableURL(DocumentLoader* docLoader)
     // case handles malformed URLs and unknown schemes. Loading alternate content
     // at other times behaves like a standard load.
     DocumentLoader* compareDocumentLoader = 0;
-    if (policyChecker()->delegateIsDecidingNavigationPolicy() || policyChecker()->delegateIsHandlingUnimplementablePolicy())
-        compareDocumentLoader = m_policyDocumentLoader.get();
-    else if (m_delegateIsHandlingProvisionalLoadError)
+    if (m_delegateIsHandlingProvisionalLoadError)
         compareDocumentLoader = m_provisionalDocumentLoader.get();
 
     return compareDocumentLoader && unreachableURL == compareDocumentLoader->request().url();
@@ -1482,8 +1452,6 @@ void FrameLoader::stopAllLoaders(ClearProvisionalItemPolicy clearProvisionalItem
     RefPtr<Frame> protect(m_frame);
 
     m_inStopAllLoaders = true;
-
-    policyChecker()->stopCheck();
 
     // If no new load is in progress, we should clear the provisional item from history
     // before we call stopLoading.
@@ -2500,18 +2468,13 @@ void FrameLoader::receivedMainResourceError(const ResourceError& error)
         checkLoadComplete();
 }
 
-void FrameLoader::callContinueFragmentScrollAfterNavigationPolicy(void* argument,
-    const ResourceRequest& request, PassRefPtr<FormState>, bool shouldContinue)
-{
-    FrameLoader* loader = static_cast<FrameLoader*>(argument);
-    loader->continueFragmentScrollAfterNavigationPolicy(request, shouldContinue);
-}
-
-void FrameLoader::continueFragmentScrollAfterNavigationPolicy(const ResourceRequest& request, bool shouldContinue)
+void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction& action)
 {
     m_quickRedirectComing = false;
+    m_documentLoader->setTriggeringAction(action);
 
-    if (!shouldContinue)
+    const ResourceRequest& request = action.resourceRequest();
+    if (!m_documentLoader->shouldContinueForNavigationPolicy(request))
         return;
 
     // If we have a provisional request for a different document, a fragment scroll should cancel it.
@@ -2520,7 +2483,7 @@ void FrameLoader::continueFragmentScrollAfterNavigationPolicy(const ResourceRequ
         setProvisionalDocumentLoader(0);
     }
 
-    bool isRedirect = m_quickRedirectComing || policyChecker()->loadType() == FrameLoadTypeRedirectWithLockedBackForwardList;    
+    bool isRedirect = m_quickRedirectComing || m_loadType == FrameLoadTypeRedirectWithLockedBackForwardList;
     loadInSameDocument(request.url(), 0, !isRedirect);
 }
 
@@ -2558,13 +2521,6 @@ void FrameLoader::scrollToFragmentWithParentBoundary(const KURL& url)
 
     if (boundaryFrame)
         boundaryFrame->view()->setSafeToPropagateScrollToParent(true);
-}
-
-void FrameLoader::callContinueLoadAfterNavigationPolicy(void* argument,
-    const ResourceRequest& request, PassRefPtr<FormState> formState, bool shouldContinue)
-{
-    FrameLoader* loader = static_cast<FrameLoader*>(argument);
-    loader->continueLoadAfterNavigationPolicy(request, formState, shouldContinue);
 }
 
 bool FrameLoader::shouldClose()
@@ -2626,7 +2582,7 @@ bool FrameLoader::fireBeforeUnloadEvent(Chrome* chrome)
     return chrome->runBeforeUnloadConfirmPanel(text, m_frame);
 }
 
-void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, PassRefPtr<FormState> formState, bool shouldContinue)
+void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> formState)
 {
     // If we loaded an alternate page to replace an unreachableURL, we'll get in here with a
     // nil policyDataSource because loading the alternate page will have passed
@@ -2637,6 +2593,17 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
     RefPtr<Frame> protect(m_frame);
 
     bool isTargetItem = history()->provisionalItem() ? history()->provisionalItem()->isTargetItem() : false;
+
+    bool shouldContinue = false;
+    if (m_stateMachine.committedFirstRealDocumentLoad() || !m_frame->ownerElement()
+        || m_frame->ownerElement()->dispatchBeforeLoadEvent(m_policyDocumentLoader->request().url().string())) {
+        // We skip dispatching the beforeload event if we've already
+        // committed a real document load because the event would leak
+        // subsequent activity by the frame which the parent frame isn't
+        // supposed to learn. For example, if the child frame navigated to
+        // a new URL, the parent frame shouldn't learn the URL.
+        shouldContinue = m_policyDocumentLoader->shouldContinueForNavigationPolicy(m_policyDocumentLoader->request());
+    }
 
     // Two reasons we can't continue:
     //    1) Navigation policy delegate said we can't so request is nil. A primary case of this 
@@ -2655,7 +2622,7 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
         // If the navigation request came from the back/forward menu, and we punt on it, we have the 
         // problem that we have optimistically moved the b/f cursor already, so move it back.  For sanity, 
         // we only do this when punting a navigation for the target frame or top-level frame.  
-        if ((isTargetItem || isLoadingMainFrame()) && isBackForwardLoadType(policyChecker()->loadType())) {
+        if ((isTargetItem || isLoadingMainFrame()) && isBackForwardLoadType(m_loadType)) {
             if (Page* page = m_frame->page()) {
                 Frame* mainFrame = page->mainFrame();
                 if (HistoryItem* resetItem = mainFrame->loader()->history()->currentItem())
@@ -2665,7 +2632,6 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
         return;
     }
 
-    FrameLoadType type = policyChecker()->loadType();
     // A new navigation is in progress, so don't clear the history's provisional item.
     stopAllLoaders(ShouldNotClearProvisionalItem);
     
@@ -2680,12 +2646,11 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
     }
 
     setProvisionalDocumentLoader(m_policyDocumentLoader.get());
-    m_loadType = type;
     setState(FrameStateProvisional);
 
     setPolicyDocumentLoader(0);
 
-    if (isBackForwardLoadType(type) && history()->provisionalItem()->isInPageCache()) {
+    if (isBackForwardLoadType(m_loadType) && history()->provisionalItem()->isInPageCache()) {
         loadProvisionalItemFromCachedPage();
         return;
     }

Source/core/loader/FrameLoader.h
@@ -40,7 +40,6 @@
 #include "IconURL.h"
 #include "LayoutMilestones.h"
 #include "MixedContentChecker.h"
-#include "PolicyChecker.h"
 #include "ResourceHandle.h"
 #include "ResourceLoadNotifier.h"
 #include "SecurityContext.h"
@@ -89,7 +88,6 @@ public:
 
     Frame* frame() const { return m_frame; }
 
-    PolicyChecker* policyChecker() const { return &m_policyChecker; }
     HistoryController* history() const { return &m_history; }
     ResourceLoadNotifier* notifier() const { return &m_notifer; }
     SubframeLoader* subframeLoader() const { return &m_subframeLoader; }
@@ -174,6 +172,7 @@ public:
     bool shouldTreatURLAsSrcdocDocument(const KURL&) const;
 
     FrameLoadType loadType() const;
+    void setLoadType(FrameLoadType loadType) { m_loadType = loadType; }
 
     CachePolicy subresourceCachePolicy() const;
 
@@ -302,14 +301,11 @@ private:
     void frameLoadCompleted();
 
     SubstituteData defaultSubstituteDataForURL(const KURL&);
-
-    static void callContinueLoadAfterNavigationPolicy(void*, const ResourceRequest&, PassRefPtr<FormState>, bool shouldContinue);
-    static void callContinueFragmentScrollAfterNavigationPolicy(void*, const ResourceRequest&, PassRefPtr<FormState>, bool shouldContinue);
     
     bool fireBeforeUnloadEvent(Chrome*);
 
-    void continueLoadAfterNavigationPolicy(const ResourceRequest&, PassRefPtr<FormState>, bool shouldContinue);
-    void continueFragmentScrollAfterNavigationPolicy(const ResourceRequest&, bool shouldContinue);
+    void checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState>);
+    void checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction&);
     void checkNewWindowPolicyAndContinue(PassRefPtr<FormState>, const String& frameName, const NavigationAction&);
 
     bool shouldPerformFragmentNavigation(bool isFormSubmission, const String& httpMethod, FrameLoadType, const KURL&);
@@ -371,7 +367,6 @@ private:
     // FIXME: These should be OwnPtr<T> to reduce build times and simplify
     // header dependencies unless performance testing proves otherwise.
     // Some of these could be lazily created for memory savings on devices.
-    mutable PolicyChecker m_policyChecker;
     mutable HistoryController m_history;
     mutable ResourceLoadNotifier m_notifer;
     mutable SubframeLoader m_subframeLoader;

LayoutTests/http/tests/misc/redirect-to-external-url-expected.txt
@@ -1,13 +1,0 @@
-Policy delegate: attempt to load http://127.0.0.1:8000/misc/resources/redirect-to-external-url.php with navigation type 'link clicked'
-Policy delegate: attempt to load spaceballs://the-flamethrower/ with navigation type 'link clicked'
-Policy delegate: unable to implement policy with error domain 'WebKitErrorDomain', error code 101, in frame '<!--framePath //<!--frame0-->-->'
-This page has an iframe. It loads then navigates to an http url which redirects to an externally handled url.
-The navigation policy delegate should be consulted about the http url and the external url.
-
-
---------
-Frame: '<!--framePath //<!--frame0-->-->'
---------
-This is a page with a link in it. When the link it clicked it will navigate to another page. That page redirects to an externally handled URL.
-The policy delegate should be asked about the initial navigation and the redirect.
-The kids love this one

LayoutTests/http/tests/misc/redirect-to-external-url.html
@@ -1,19 +1,0 @@
-<html>
-<head>
-<script>
-
-if (window.testRunner) {
-    testRunner.dumpAsText();
-    testRunner.dumpChildFramesAsText();
-    testRunner.waitUntilDone();
-}
-
-</script>
-
-</head>
-<body>
-This page has an iframe.  It loads then navigates to an http url which redirects to an externally handled url.<br>
-The navigation policy delegate should be consulted about the http url and the external url.<br>
-<iframe src="resources/redirect-to-external-url-iframe.html"></iframe>
-</body>
-</html>

LayoutTests/http/tests/misc/resources/redirect-to-external-url-iframe.html
@@ -1,26 +1,0 @@
-<html>
-<head>
-<script>
-
-if (window.testRunner)
-    testRunner.setCustomPolicyDelegate(true, true);
-
-function runTest()
-{
-    var link = document.getElementById('testlink');
-    var clickEvent = document.createEvent('MouseEvents');
-    clickEvent.initEvent('click', false, true);
-    link.dispatchEvent(clickEvent);
-
-    if (window.testRunner)
-        testRunner.notifyDone();
-}
-
-</script>
-
-<body onload="runTest();">
-This is a page with a link in it.  When the link it clicked it will navigate to another page.  That page redirects to an externally handled URL.<br>
-The policy delegate should be asked about the initial navigation and the redirect.<br>
-<a id="testlink" href="redirect-to-external-url.php">The kids love this one</a>
-</body>
-</html>

LayoutTests/http/tests/misc/resources/redirect-to-external-url.php
@@ -1,4 +1,0 @@
-<?php
-    header('Location: spaceballs://the-flamethrower/');
-    header('HTTP/1.0 302 Found');
-?>

Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -870,12 +870,9 @@ PolicyAction FrameLoaderClientImpl::policyForNewWindowAction(
     return PolicyUse;
 }
 
-void FrameLoaderClientImpl::dispatchDecidePolicyForNavigationAction(
-    FramePolicyFunction function,
+PolicyAction FrameLoaderClientImpl::decidePolicyForNavigationAction(
     const NavigationAction& action,
-    const ResourceRequest& request,
-    PassRefPtr<FormState> formState) {
-    PolicyAction policyAction = PolicyIgnore;
+    const ResourceRequest& request) {
 
     // It is valid for this function to be invoked in code paths where the
     // webview is closed.
@@ -904,19 +901,16 @@ void FrameLoaderClientImpl::dispatchDecidePolicyForNavigationAction(
         }
 
         if (navigationPolicy == WebNavigationPolicyCurrentTab)
-            policyAction = PolicyUse;
+            return PolicyUse;
         else if (navigationPolicy == WebNavigationPolicyDownload)
-            policyAction = PolicyDownload;
-        else {
-            if (navigationPolicy != WebNavigationPolicyIgnore) {
+            return PolicyDownload;
+        else if (navigationPolicy != WebNavigationPolicyIgnore) {
                 WrappedResourceRequest webreq(request);
                 m_webFrame->client()->loadURLExternally(m_webFrame, webreq, navigationPolicy);
-            }
-            policyAction = PolicyIgnore;
         }
     }
 
-    (m_webFrame->frame()->loader()->policyChecker()->*function)(policyAction);
+    return PolicyIgnore;
 }
 
 void FrameLoaderClientImpl::dispatchUnableToImplementPolicy(const ResourceError& error)

Source/WebKit/chromium/src/FrameLoaderClientImpl.h
@@ -101,7 +101,7 @@ public:
     virtual WebCore::Frame* dispatchCreatePage(const WebCore::NavigationAction&);
     virtual void dispatchShow();
     virtual WebCore::PolicyAction policyForNewWindowAction(const WebCore::NavigationAction& action, const WTF::String& frame_name);
-    virtual void dispatchDecidePolicyForNavigationAction(WebCore::FramePolicyFunction function, const WebCore::NavigationAction& action, const WebCore::ResourceRequest& request, PassRefPtr<WebCore::FormState> form_state);
+    virtual WebCore::PolicyAction decidePolicyForNavigationAction(const WebCore::NavigationAction& action, const WebCore::ResourceRequest& request);
     virtual void dispatchUnableToImplementPolicy(const WebCore::ResourceError&);
     virtual void dispatchWillRequestResource(WebCore::CachedResourceRequest*);
     virtual void dispatchWillSendSubmitEvent(PassRefPtr<WebCore::FormState>);

commit 2431a68a72d646828d7a75023d0ce22fd838324f

Fix a history bug where a reload is given a standard load's history entry.
If a resource load is started after the load event and a reload is triggered
before that resource load finishes, the load will be cancelled and in doing
so will incorrectly revert the new load's load type to FrameLoadTypeStandard.
This was regressed in r148684

BUG=241581
TEST=http/tests/history/reload-during-load-after-load-event.html

Review URL: https://chromiumcodereview.appspot.com/17463006

git-svn-id: svn://svn.chromium.org/blink/trunk@152781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/FrameLoader.cpp
@@ -1125,14 +1125,13 @@ void FrameLoader::loadWithNavigationAction(const ResourceRequest& request, const
     if (type == FrameLoadTypeRedirectWithLockedBackForwardList)
         loader->setIsClientRedirect(true);
 
-    m_loadType = type;
     bool isFormSubmission = formState;
 
     if (shouldPerformFragmentNavigation(isFormSubmission, request.httpMethod(), type, request.url()))
-        checkNavigationPolicyAndContinueFragmentScroll(action);
+        checkNavigationPolicyAndContinueFragmentScroll(action, type != FrameLoadTypeRedirectWithLockedBackForwardList);
     else {
         setPolicyDocumentLoader(loader.get());
-        checkNavigationPolicyAndContinueLoad(formState);
+        checkNavigationPolicyAndContinueLoad(formState, type);
     }
 }
 
@@ -1930,7 +1929,7 @@ void FrameLoader::receivedMainResourceError(const ResourceError& error)
         checkLoadComplete();
 }
 
-void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction& action)
+void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction& action, bool isNewNavigation)
 {
     m_documentLoader->setTriggeringAction(action);
 
@@ -1943,7 +1942,7 @@ void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const Navigatio
         m_provisionalDocumentLoader->stopLoading();
         setProvisionalDocumentLoader(0);
     }
-    loadInSameDocument(request.url(), 0, m_loadType != FrameLoadTypeRedirectWithLockedBackForwardList);
+    loadInSameDocument(request.url(), 0, isNewNavigation);
 }
 
 bool FrameLoader::shouldPerformFragmentNavigation(bool isFormSubmission, const String& httpMethod, FrameLoadType loadType, const KURL& url)
@@ -2040,7 +2039,7 @@ bool FrameLoader::fireBeforeUnloadEvent(Chrome& chrome)
     return chrome.runBeforeUnloadConfirmPanel(text, m_frame);
 }
 
-void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> formState)
+void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> formState, FrameLoadType type)
 {
     // If we loaded an alternate page to replace an unreachableURL, we'll get in here with a
     // nil policyDataSource because loading the alternate page will have passed
@@ -2104,6 +2103,7 @@ void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> for
     }
 
     setProvisionalDocumentLoader(m_policyDocumentLoader.get());
+    m_loadType = type;
     setState(FrameStateProvisional);
 
     setPolicyDocumentLoader(0);

Source/core/loader/FrameLoader.h
@@ -276,8 +276,8 @@ private:
     
     bool fireBeforeUnloadEvent(Chrome&);
 
-    void checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState>);
-    void checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction&);
+    void checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState>, FrameLoadType);
+    void checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction&, bool isNewNavigation);
     void checkNewWindowPolicyAndContinue(PassRefPtr<FormState>, const String& frameName, const NavigationAction&);
 
     bool shouldPerformFragmentNavigation(bool isFormSubmission, const String& httpMethod, FrameLoadType, const KURL&);

LayoutTests/http/tests/history/reload-during-load-after-load-event-expected.txt
@@ -1,0 +1,5 @@
+This tests starting a new resource load after the load event has finished, then reloading while that load is still in progress. Due to a regression in http://src.chromium.org/viewvc/blink?revision=148684&view=revision, we would overwrite the load type such that the reload will still be executed correctly, but the history entry will be created as though it was a standard load.
+
+============== Back Forward List ==============
+curr->  http://127.0.0.1:8000/history/reload-during-load-after-load-event.html  **nav target**
+===============================================

LayoutTests/http/tests/history/reload-during-load-after-load-event.html
@@ -1,0 +1,30 @@
+<html>
+<body onload="setTimeout(test, 0);">
+This tests starting a new resource load after the load event has finished, then reloading while that load is still in progress.
+Due to a regression in http://src.chromium.org/viewvc/blink?revision=148684&view=revision, we would overwrite the load type such that
+the reload will still be executed correctly, but the history entry will be created as though it was a standard load.
+<script>
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+}
+
+function test() {
+    if (window.localStorage.getItem("finish-during-reload") == null) {
+        window.localStorage.setItem("finish-during-reload", "reload")
+        var img = document.createElement("img");
+        img.src = "resources/slow-image.php";
+        document.body.appendChild(img);
+        if (window.internals)
+            internals.forceReload(false);
+    } else {
+        window.localStorage.clear();
+        if (window.testRunner) {
+            testRunner.dumpBackForwardList();
+            testRunner.notifyDone();
+        }
+    }
+}
+</script>
+</body>
+</html>