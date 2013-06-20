primiano/blink-gitcs

commit f35f8699220bc75f70a37154d0bafdd63a172ba0

Move revalidationSucceeded() and revalidationFailed() to CachedResource
Also, some associated functions that access CachedResource members more than
Memorycache members. This reduces the number of places that need to talk to
MemoryCache, and makes ResourceLoader always talk directly to its
CachedResource.

BUG=none

Review URL: https://chromiumcodereview.appspot.com/15416006

git-svn-id: svn://svn.chromium.org/blink/trunk@150701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/cache/CachedResource.cpp
@@ -201,7 +201,7 @@ void CachedResource::failBeforeStarting()
     // FIXME: What if resources in other frames were waiting for this revalidation?
     LOG(ResourceLoading, "Cannot start loading '%s'", url().string().latin1().data());
     if (m_resourceToRevalidate) 
-        memoryCache()->revalidationFailed(this); 
+        revalidationFailed();
     error(CachedResource::LoadError);
 }
 
@@ -693,6 +693,62 @@ void CachedResource::updateResponseAfterRevalidation(const ResourceResponse& val
             continue;
         m_response.setHTTPHeaderField(it->key, it->value);
     }
+}
+
+void CachedResource::revalidationSucceeded(const ResourceResponse& response)
+{
+    ASSERT(m_resourceToRevalidate);
+    ASSERT(!m_resourceToRevalidate->inCache());
+    ASSERT(m_resourceToRevalidate->isLoaded());
+    ASSERT(inCache());
+
+    // Calling evict() can potentially delete revalidatingResource, which we use
+    // below. This mustn't be the case since revalidation means it is loaded
+    // and so canDelete() is false.
+    ASSERT(!canDelete());
+
+    m_resourceToRevalidate->updateResponseAfterRevalidation(response);
+    memoryCache()->remove(this);
+    memoryCache()->add(m_resourceToRevalidate);
+    int delta = m_resourceToRevalidate->size();
+    if (m_resourceToRevalidate->decodedSize() && m_resourceToRevalidate->hasClients())
+        memoryCache()->insertInLiveDecodedResourcesList(m_resourceToRevalidate);
+    if (delta)
+        memoryCache()->adjustSize(m_resourceToRevalidate->hasClients(), delta);
+
+    switchClientsToRevalidatedResource();
+    ASSERT(!m_deleted);
+    // clearResourceToRevalidate deletes this.
+    clearResourceToRevalidate();
+}
+
+void CachedResource::revalidationFailed()
+{
+    ASSERT(WTF::isMainThread());
+    LOG(ResourceLoading, "Revalidation failed for %p", this);
+    ASSERT(resourceToRevalidate());
+    clearResourceToRevalidate();
+    if (!m_error.isNull() && (m_error.isCancellation() || !isPreloaded()))
+        memoryCache()->remove(this);
+}
+
+void CachedResource::updateForAccess()
+{
+    ASSERT(inCache());
+
+    // Need to make sure to remove before we increase the access count, since
+    // the queue will possibly change. However, if this is a resource that is being
+    // added back in due to a successful revalidation, it is not present in the LRU list
+    // at this point, so we don't need to remove it.
+    if (!m_proxyResource && m_accessCount)
+        memoryCache()->removeFromLRUList(this);
+
+    // If this is the first time the resource has been accessed, adjust the size of the cache to account for its initial size.
+    if (!m_accessCount)
+        memoryCache()->adjustSize(hasClients(), size());
+
+    m_accessCount++;
+    memoryCache()->insertInLRUList(this);
 }
 
 void CachedResource::registerHandle(CachedResourceHandleBase* h)

Source/core/loader/ResourceLoader.cpp
@@ -37,7 +37,6 @@
 #include "core/loader/UniqueIdentifier.h"
 #include "core/loader/appcache/ApplicationCacheHost.h"
 #include "core/loader/cache/CachedResourceLoader.h"
-#include "core/loader/cache/MemoryCache.h"
 #include "core/page/Frame.h"
 #include "core/page/Page.h"
 #include "core/platform/Logging.h"
@@ -261,10 +260,9 @@ void ResourceLoader::cancel(const ResourceError& error)
     if (m_state == Initialized) {
         LOG(ResourceLoading, "Cancelled load of '%s'.\n", m_resource->url().string().latin1().data());
         m_state = Finishing;
-        if (m_resource->resourceToRevalidate())
-            memoryCache()->revalidationFailed(m_resource);
         m_resource->setResourceError(nonNullError);
-        memoryCache()->remove(m_resource);
+        if (m_resource->resourceToRevalidate())
+            m_resource->revalidationFailed();
     }
 
     // If we re-enter cancel() from inside didFailToLoad(), we want to pick up from where we 
@@ -360,7 +358,7 @@ void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse&
             // 304 Not modified / Use local copy
             // Existing resource is ok, just use it updating the expiration time.
             m_resource->setResponse(response);
-            memoryCache()->revalidationSucceeded(m_resource, response);
+            m_resource->revalidationSucceeded(response);
             if (reachedTerminalState())
                 return;
 
@@ -369,7 +367,7 @@ void ResourceLoader::didReceiveResponse(ResourceHandle*, const ResourceResponse&
             return;
         }
         // Did not get 304 response, continue as a regular resource load.
-        memoryCache()->revalidationFailed(m_resource);
+        m_resource->revalidationFailed();
     }
 
     m_resource->responseReceived(response);
@@ -460,12 +458,10 @@ void ResourceLoader::didFail(ResourceHandle*, const ResourceError& error)
     RefPtr<ResourceLoader> protect(this);
     CachedResourceHandle<CachedResource> protectResource(m_resource);
     m_state = Finishing;
-    if (m_resource->resourceToRevalidate())
-        memoryCache()->revalidationFailed(m_resource);
     m_resource->setResourceError(error);
+    if (m_resource->resourceToRevalidate())
+        m_resource->revalidationFailed();
     m_resource->error(CachedResource::LoadError);
-    if (!m_resource->isPreloaded())
-        memoryCache()->remove(m_resource);
 
     if (m_cancelled)
         return;

Source/core/loader/cache/CachedResource.h
@@ -151,8 +151,8 @@ public:
             || type() == RawResource;
     }
 
+    void updateForAccess();
     unsigned accessCount() const { return m_accessCount; }
-    void increaseAccessCount() { m_accessCount++; }
 
     // Computes the status of an object after loading.  
     // Updates the expire date on the cache entry file
@@ -232,9 +232,8 @@ public:
     
     // HTTP revalidation support methods for CachedResourceLoader.
     void setResourceToRevalidate(CachedResource*);
-    virtual void switchClientsToRevalidatedResource();
-    void clearResourceToRevalidate();
-    void updateResponseAfterRevalidation(const ResourceResponse& validatingResponse);
+    void revalidationSucceeded(const ResourceResponse&);
+    void revalidationFailed();
     
     virtual void didSendData(unsigned long long /* bytesSent */, unsigned long long /* totalBytesToBeSent */) { }
     virtual void didDownloadData(int) { }
@@ -255,6 +254,10 @@ protected:
     void didAccessDecodedData(double timeStamp);
 
     bool isSafeToMakePurgeable() const;
+
+    virtual void switchClientsToRevalidatedResource();
+    void clearResourceToRevalidate();
+    void updateResponseAfterRevalidation(const ResourceResponse& validatingResponse);
     
     HashCountedSet<CachedResourceClient*> m_clients;
 

Source/core/loader/cache/CachedResourceLoader.cpp
@@ -380,7 +380,7 @@ CachedResourceHandle<CachedResource> CachedResourceLoader::requestResource(Cache
         resource = revalidateResource(request, resource.get());
         break;
     case Use:
-        memoryCache()->resourceAccessed(resource.get());
+        resource->updateForAccess();
         notifyLoadedFromMemoryCache(resource.get());
         break;
     }

Source/core/loader/cache/MemoryCache.cpp
@@ -95,50 +95,9 @@ void MemoryCache::add(CachedResource* resource)
     ASSERT(WTF::isMainThread());
     m_resources.set(resource->url(), resource);
     resource->setInCache(true);
-    
-    resourceAccessed(resource);
+    resource->updateForAccess();
     
     LOG(ResourceLoading, "MemoryCache::add Added '%s', resource %p\n", resource->url().string().latin1().data(), resource);
-}
-
-void MemoryCache::revalidationSucceeded(CachedResource* revalidatingResource, const ResourceResponse& response)
-{
-    CachedResource* resource = revalidatingResource->resourceToRevalidate();
-    ASSERT(resource);
-    ASSERT(!resource->inCache());
-    ASSERT(resource->isLoaded());
-    ASSERT(revalidatingResource->inCache());
-
-    // Calling evict() can potentially delete revalidatingResource, which we use
-    // below. This mustn't be the case since revalidation means it is loaded
-    // and so canDelete() is false.
-    ASSERT(!revalidatingResource->canDelete());
-
-    evict(revalidatingResource);
-
-    ASSERT(!m_resources.get(resource->url()));
-    m_resources.set(resource->url(), resource);
-    resource->setInCache(true);
-    resource->updateResponseAfterRevalidation(response);
-    insertInLRUList(resource);
-    int delta = resource->size();
-    if (resource->decodedSize() && resource->hasClients())
-        insertInLiveDecodedResourcesList(resource);
-    if (delta)
-        adjustSize(resource->hasClients(), delta);
-    
-    revalidatingResource->switchClientsToRevalidatedResource();
-    ASSERT(!revalidatingResource->m_deleted);
-    // this deletes the revalidating resource
-    revalidatingResource->clearResourceToRevalidate();
-}
-
-void MemoryCache::revalidationFailed(CachedResource* revalidatingResource)
-{
-    ASSERT(WTF::isMainThread());
-    LOG(ResourceLoading, "Revalidation failed for %p", revalidatingResource);
-    ASSERT(revalidatingResource->resourceToRevalidate());
-    revalidatingResource->clearResourceToRevalidate();
 }
 
 CachedResource* MemoryCache::resourceForURL(const KURL& resourceURL)
@@ -445,25 +404,6 @@ void MemoryCache::insertInLRUList(CachedResource* resource)
     ASSERT(found);
 #endif
 
-}
-
-void MemoryCache::resourceAccessed(CachedResource* resource)
-{
-    ASSERT(resource->inCache());
-    
-    // Need to make sure to remove before we increase the access count, since
-    // the queue will possibly change.
-    removeFromLRUList(resource);
-    
-    // If this is the first time the resource has been accessed, adjust the size of the cache to account for its initial size.
-    if (!resource->accessCount())
-        adjustSize(resource->hasClients(), resource->size());
-    
-    // Add to our access count.
-    resource->increaseAccessCount();
-    
-    // Now insert into the new queue.
-    insertInLRUList(resource);
 }
 
 void MemoryCache::removeResourcesWithOrigin(SecurityOrigin* origin)

Source/core/loader/cache/MemoryCache.h
@@ -96,9 +96,6 @@ public:
 
     static KURL removeFragmentIdentifierIfNeeded(const KURL& originalURL);
     
-    void revalidationSucceeded(CachedResource* revalidatingResource, const ResourceResponse&);
-    void revalidationFailed(CachedResource* revalidatingResource);
-    
     // Sets the cache's memory capacities, in bytes. These will hold only approximately, 
     // since the decoded cost of resources like scripts and stylesheets is not known.
     //  - minDeadBytes: The maximum number of bytes that dead resources should consume when the cache is under pressure.
@@ -132,8 +129,6 @@ public:
 
     // Function to collect cache statistics for the caches window in the Safari Debug menu.
     Statistics getStatistics();
-    
-    void resourceAccessed(CachedResource*);
 
     typedef HashSet<RefPtr<SecurityOrigin> > SecurityOriginSet;
     void removeResourcesWithOrigin(SecurityOrigin*);

commit 0a673db063347d9b8a1477f508ef83f9a460db5f

Cancelled resources sometimes get stuck in the cache.
Normally, when a load is cancelled or failed, we remove the resource from
MemoryCache, so that future loads don't try to reuse a failure (the exception
being preloads that genuinely failed). However, we are incorrectly leaving
the resources in the cache in certain cases for non-revalidation loads. This
was regressed in r150701.

BUG=247295

Review URL: https://chromiumcodereview.appspot.com/17154004

git-svn-id: svn://svn.chromium.org/blink/trunk@152833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/cache/CachedResource.cpp
@@ -169,10 +169,7 @@ CachedResource::~CachedResource()
 
 void CachedResource::failBeforeStarting()
 {
-    // FIXME: What if resources in other frames were waiting for this revalidation?
     LOG(ResourceLoading, "Cannot start loading '%s'", url().string().latin1().data());
-    if (m_resourceToRevalidate) 
-        revalidationFailed();
     error(CachedResource::LoadError);
 }
 
@@ -234,6 +231,9 @@ void CachedResource::error(CachedResource::Status status)
 {
     if (m_resourceToRevalidate)
         revalidationFailed();
+
+    if (!m_error.isNull() && (m_error.isCancellation() || !isPreloaded()))
+        memoryCache()->remove(this);
 
     setStatus(status);
     ASSERT(errorOccurred());
@@ -700,8 +700,6 @@ void CachedResource::revalidationFailed()
     LOG(ResourceLoading, "Revalidation failed for %p", this);
     ASSERT(resourceToRevalidate());
     clearResourceToRevalidate();
-    if (!m_error.isNull() && (m_error.isCancellation() || !isPreloaded()))
-        memoryCache()->remove(this);
 }
 
 void CachedResource::updateForAccess()

LayoutTests/http/tests/xmlhttprequest/reentrant-cancel-expected.txt
@@ -1,2 +1,1 @@
-CONSOLE MESSAGE: line 15: Uncaught AbortError: The user aborted a request.
 XThis tests that when we re-entrantly create and cancel XHRs, we don't try to disconnect the same CachedResourceClient multiple times from its CachedResource. We pass if we don't crash. XX