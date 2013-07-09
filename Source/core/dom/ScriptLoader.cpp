primiano/blink-gitcs

commit 1d47e799f15063b4775d970a7ee9583edfe1f466

Refactoring: Decouple ScriptElement from HTMLScriptElement and SVGScriptElement.
This change pulls ScriptElement out from Element inheritance chain.
This give us an opportunity to decouple the lifetime of the element and
script loading and execution. Various states which is realted to script loading
won't be needed and can be discarded once it is feed to the script engine.
Such refactoring will follow.

Also in a coming change, ScriptElement will be renamed to ElementScriptLoader.

TEST=none
BUG=240592
R=eseidel,dglazkov,abarth

Review URL: https://chromiumcodereview.appspot.com/18676002

git-svn-id: svn://svn.chromium.org/blink/trunk@153748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/html/HTMLScriptElement.cpp
@@ -37,7 +37,7 @@ using namespace HTMLNames;
 
 inline HTMLScriptElement::HTMLScriptElement(const QualifiedName& tagName, Document* document, bool wasInsertedByParser, bool alreadyStarted)
     : HTMLElement(tagName, document)
-    , ScriptElement(this, wasInsertedByParser, alreadyStarted)
+    , m_scriptElement(ScriptElement::create(this, wasInsertedByParser, alreadyStarted))
 {
     ASSERT(hasTagName(scriptTag));
     ScriptWrappable::init(this);
@@ -56,15 +56,15 @@ bool HTMLScriptElement::isURLAttribute(const Attribute& attribute) const
 void HTMLScriptElement::childrenChanged(bool changedByParser, Node* beforeChange, Node* afterChange, int childCountDelta)
 {
     HTMLElement::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
-    ScriptElement::childrenChanged();
+    m_scriptElement->childrenChanged();
 }
 
 void HTMLScriptElement::parseAttribute(const QualifiedName& name, const AtomicString& value)
 {
     if (name == srcAttr)
-        handleSourceAttribute(value);
+        m_scriptElement->handleSourceAttribute(value);
     else if (name == asyncAttr)
-        handleAsyncAttribute();
+        m_scriptElement->handleAsyncAttribute();
     else if (name == onbeforeloadAttr)
         setAttributeEventListener(eventNames().beforeloadEvent, createAttributeEventListener(this, name, value));
     else
@@ -74,7 +74,7 @@ void HTMLScriptElement::parseAttribute(const QualifiedName& name, const AtomicSt
 Node::InsertionNotificationRequest HTMLScriptElement::insertedInto(ContainerNode* insertionPoint)
 {
     HTMLElement::insertedInto(insertionPoint);
-    ScriptElement::insertedInto(insertionPoint);
+    m_scriptElement->insertedInto(insertionPoint);
     return InsertionDone;
 }
 
@@ -98,12 +98,12 @@ void HTMLScriptElement::setText(const String &value)
 void HTMLScriptElement::setAsync(bool async)
 {
     setBooleanAttribute(asyncAttr, async);
-    handleAsyncAttribute();
+    m_scriptElement->handleAsyncAttribute();
 }
 
 bool HTMLScriptElement::async() const
 {
-    return fastHasAttribute(asyncAttr) || forceAsync();
+    return fastHasAttribute(asyncAttr) || (m_scriptElement->forceAsync());
 }
 
 KURL HTMLScriptElement::src() const
@@ -165,15 +165,12 @@ bool HTMLScriptElement::hasSourceAttribute() const
 
 void HTMLScriptElement::dispatchLoadEvent()
 {
-    ASSERT(!haveFiredLoadEvent());
-    setHaveFiredLoadEvent(true);
-
     dispatchEvent(Event::create(eventNames().loadEvent, false, false));
 }
 
 PassRefPtr<Element> HTMLScriptElement::cloneElementWithoutAttributesAndChildren()
 {
-    return adoptRef(new HTMLScriptElement(tagQName(), document(), false, alreadyStarted()));
+    return adoptRef(new HTMLScriptElement(tagQName(), document(), false, m_scriptElement->alreadyStarted()));
 }
 
 }

Source/core/dom/ScriptElement.cpp
@@ -66,8 +66,8 @@ ScriptElement::ScriptElement(Element* element, bool parserInserted, bool already
     , m_willExecuteInOrder(false)
 {
     ASSERT(m_element);
-    if (parserInserted && m_element->document()->scriptableDocumentParser() && !m_element->document()->isInDocumentWrite())
-        m_startLineNumber = m_element->document()->scriptableDocumentParser()->lineNumber();
+    if (parserInserted && element->document()->scriptableDocumentParser() && !element->document()->isInDocumentWrite())
+        m_startLineNumber = element->document()->scriptableDocumentParser()->lineNumber();
 }
 
 ScriptElement::~ScriptElement()
@@ -136,14 +136,22 @@ void ScriptElement::dispatchErrorEvent()
     m_element->dispatchEvent(Event::create(eventNames().errorEvent, false, false));
 }
 
+void ScriptElement::dispatchLoadEvent()
+{
+    ASSERT(!haveFiredLoadEvent());
+    if (ScriptElementClient* client = this->client())
+        client->dispatchLoadEvent();
+    setHaveFiredLoadEvent(true);
+}
+
 bool ScriptElement::isScriptTypeSupported(LegacyTypeSupport supportLegacyTypes) const
 {
     // FIXME: isLegacySupportedJavaScriptLanguage() is not valid HTML5. It is used here to maintain backwards compatibility with existing layout tests. The specific violations are:
     // - Allowing type=javascript. type= should only support MIME types, such as text/javascript.
     // - Allowing a different set of languages for language= and type=. language= supports Javascript 1.1 and 1.4-1.6, but type= does not.
 
-    String type = typeAttributeValue();
-    String language = languageAttributeValue();
+    String type = client()->typeAttributeValue();
+    String language = client()->languageAttributeValue();
     if (type.isEmpty() && language.isEmpty())
         return true; // Assume text/javascript.
     if (type.isEmpty()) {
@@ -161,6 +169,8 @@ bool ScriptElement::prepareScript(const TextPosition& scriptStartPosition, Legac
     if (m_alreadyStarted)
         return false;
 
+    ScriptElementClient* client = this->client();
+
     bool wasParserInserted;
     if (m_parserInserted) {
         wasParserInserted = true;
@@ -168,11 +178,11 @@ bool ScriptElement::prepareScript(const TextPosition& scriptStartPosition, Legac
     } else
         wasParserInserted = false;
 
-    if (wasParserInserted && !asyncAttributeValue())
+    if (wasParserInserted && !client->asyncAttributeValue())
         m_forceAsync = true;
 
     // FIXME: HTML5 spec says we should check that all children are either comments or empty text nodes.
-    if (!hasSourceAttribute() && !m_element->firstChild())
+    if (!client->hasSourceAttribute() && !m_element->firstChild())
         return false;
 
     if (!m_element->inDocument())
@@ -203,29 +213,29 @@ bool ScriptElement::prepareScript(const TextPosition& scriptStartPosition, Legac
     if (!isScriptForEventSupported())
         return false;
 
-    if (!charsetAttributeValue().isEmpty())
-        m_characterEncoding = charsetAttributeValue();
+    if (!client->charsetAttributeValue().isEmpty())
+        m_characterEncoding = client->charsetAttributeValue();
     else
         m_characterEncoding = document->charset();
 
-    if (hasSourceAttribute())
-        if (!requestScript(sourceAttributeValue()))
+    if (client->hasSourceAttribute())
+        if (!requestScript(client->sourceAttributeValue()))
             return false;
 
-    if (hasSourceAttribute() && deferAttributeValue() && m_parserInserted && !asyncAttributeValue()) {
+    if (client->hasSourceAttribute() && client->deferAttributeValue() && m_parserInserted && !client->asyncAttributeValue()) {
         m_willExecuteWhenDocumentFinishedParsing = true;
         m_willBeParserExecuted = true;
-    } else if (hasSourceAttribute() && m_parserInserted && !asyncAttributeValue())
+    } else if (client->hasSourceAttribute() && m_parserInserted && !client->asyncAttributeValue())
         m_willBeParserExecuted = true;
-    else if (!hasSourceAttribute() && m_parserInserted && !document->haveStylesheetsAndImportsLoaded()) {
+    else if (!client->hasSourceAttribute() && m_parserInserted && !document->haveStylesheetsAndImportsLoaded()) {
         m_willBeParserExecuted = true;
         m_readyToBeParserExecuted = true;
-    } else if (hasSourceAttribute() && !asyncAttributeValue() && !m_forceAsync) {
+    } else if (client->hasSourceAttribute() && !client->asyncAttributeValue() && !m_forceAsync) {
         m_willExecuteInOrder = true;
         document->scriptRunner()->queueScriptForExecution(this, m_cachedScript, ScriptRunner::IN_ORDER_EXECUTION);
         m_cachedScript->addClient(this);
-    } else if (hasSourceAttribute()) {
-        m_element->document()->scriptRunner()->queueScriptForExecution(this, m_cachedScript, ScriptRunner::ASYNC_EXECUTION);
+    } else if (client->hasSourceAttribute()) {
+        document->scriptRunner()->queueScriptForExecution(this, m_cachedScript, ScriptRunner::ASYNC_EXECUTION);
         m_cachedScript->addClient(this);
     } else {
         // Reset line numbering for nested writes.
@@ -239,6 +249,8 @@ bool ScriptElement::prepareScript(const TextPosition& scriptStartPosition, Legac
 
 bool ScriptElement::requestScript(const String& sourceUrl)
 {
+    ASSERT(m_element);
+
     RefPtr<Document> originalDocument = m_element->document();
     if (!m_element->dispatchBeforeLoadEvent(sourceUrl))
         return false;
@@ -247,7 +259,7 @@ bool ScriptElement::requestScript(const String& sourceUrl)
 
     ASSERT(!m_cachedScript);
     if (!stripLeadingAndTrailingHTMLSpaces(sourceUrl).isEmpty()) {
-        CachedResourceRequest request(ResourceRequest(m_element->document()->completeURL(sourceUrl)), element()->localName());
+        CachedResourceRequest request(ResourceRequest(m_element->document()->completeURL(sourceUrl)), m_element->localName());
 
         String crossOriginMode = m_element->fastGetAttribute(HTMLNames::crossoriginAttr);
         if (!crossOriginMode.isNull()) {
@@ -370,13 +382,13 @@ void ScriptElement::notifyFinished(CachedResource* resource)
 
 bool ScriptElement::ignoresLoadRequest() const
 {
-    return m_alreadyStarted || m_isExternalScript || m_parserInserted || !m_element->inDocument();
+    return m_alreadyStarted || m_isExternalScript || m_parserInserted || !element() || !element()->inDocument();
 }
 
 bool ScriptElement::isScriptForEventSupported() const
 {
-    String eventAttribute = eventAttributeValue();
-    String forAttribute = forAttributeValue();
+    String eventAttribute = client()->eventAttributeValue();
+    String forAttribute = client()->forAttributeValue();
     if (!eventAttribute.isEmpty() && !forAttribute.isEmpty()) {
         forAttribute = forAttribute.stripWhiteSpace();
         if (!equalIgnoringCase(forAttribute, "window"))
@@ -394,13 +406,25 @@ String ScriptElement::scriptContent() const
     return m_element->textFromChildren();
 }
 
+ScriptElementClient* ScriptElement::client() const
+{
+    if (isHTMLScriptElement(m_element))
+        return toHTMLScriptElement(m_element);
+
+    if (isSVGScriptElement(m_element))
+        return toSVGScriptElement(m_element);
+
+    ASSERT_NOT_REACHED();
+    return 0;
+}
+
 ScriptElement* toScriptElementIfPossible(Element* element)
 {
     if (isHTMLScriptElement(element))
-        return toHTMLScriptElement(element);
+        return toHTMLScriptElement(element)->scriptElement();
 
     if (isSVGScriptElement(element))
-        return toSVGScriptElement(element);
+        return toSVGScriptElement(element)->scriptElement();
 
     return 0;
 }

Source/core/dom/ScriptElement.h
@@ -34,9 +34,26 @@ class Element;
 class ScriptElement;
 class ScriptSourceCode;
 
+class ScriptElementClient {
+public:
+    virtual ~ScriptElementClient() { }
+
+    virtual void dispatchLoadEvent() = 0;
+
+    virtual String sourceAttributeValue() const = 0;
+    virtual String charsetAttributeValue() const = 0;
+    virtual String typeAttributeValue() const = 0;
+    virtual String languageAttributeValue() const = 0;
+    virtual String forAttributeValue() const = 0;
+    virtual String eventAttributeValue() const = 0;
+    virtual bool asyncAttributeValue() const = 0;
+    virtual bool deferAttributeValue() const = 0;
+    virtual bool hasSourceAttribute() const = 0;
+};
+
 class ScriptElement : private CachedResourceClient {
 public:
-    ScriptElement(Element*, bool createdByParser, bool isEvaluated);
+    static PassOwnPtr<ScriptElement> create(Element*, bool createdByParser, bool isEvaluated);
     virtual ~ScriptElement();
 
     Element* element() const { return m_element; }
@@ -50,7 +67,7 @@ public:
     void execute(CachedScript*);
 
     // XML parser calls these
-    virtual void dispatchLoadEvent() = 0;
+    void dispatchLoadEvent();
     void dispatchErrorEvent();
     bool isScriptTypeSupported(LegacyTypeSupport) const;
 
@@ -60,7 +77,6 @@ public:
     bool willExecuteWhenDocumentFinishedParsing() const { return m_willExecuteWhenDocumentFinishedParsing; }
     CachedResourceHandle<CachedScript> cachedScript() { return m_cachedScript; }
 
-protected:
     void setHaveFiredLoadEvent(bool haveFiredLoad) { m_haveFiredLoad = haveFiredLoad; }
     bool isParserInserted() const { return m_parserInserted; }
     bool alreadyStarted() const { return m_alreadyStarted; }
@@ -73,23 +89,18 @@ protected:
     void handleAsyncAttribute();
 
 private:
+    ScriptElement(Element*, bool createdByParser, bool isEvaluated);
+
     bool ignoresLoadRequest() const;
     bool isScriptForEventSupported() const;
 
     bool requestScript(const String& sourceUrl);
     void stopLoadRequest();
 
-    virtual void notifyFinished(CachedResource*);
+    ScriptElementClient* client() const;
 
-    virtual String sourceAttributeValue() const = 0;
-    virtual String charsetAttributeValue() const = 0;
-    virtual String typeAttributeValue() const = 0;
-    virtual String languageAttributeValue() const = 0;
-    virtual String forAttributeValue() const = 0;
-    virtual String eventAttributeValue() const = 0;
-    virtual bool asyncAttributeValue() const = 0;
-    virtual bool deferAttributeValue() const = 0;
-    virtual bool hasSourceAttribute() const = 0;
+    // CachedResourceClient
+    virtual void notifyFinished(CachedResource*) OVERRIDE;
 
     Element* m_element;
     CachedResourceHandle<CachedScript> m_cachedScript;
@@ -109,6 +120,12 @@ private:
 
 ScriptElement* toScriptElementIfPossible(Element*);
 
+inline PassOwnPtr<ScriptElement> ScriptElement::create(Element* element, bool createdByParser, bool isEvaluated)
+{
+    return adoptPtr(new ScriptElement(element, createdByParser, isEvaluated));
 }
+
+}
+
 
 #endif

Source/core/html/HTMLScriptElement.h
@@ -29,17 +29,19 @@
 
 namespace WebCore {
 
-class HTMLScriptElement FINAL : public HTMLElement, public ScriptElement {
+class HTMLScriptElement FINAL : public HTMLElement, public ScriptElementClient {
 public:
     static PassRefPtr<HTMLScriptElement> create(const QualifiedName&, Document*, bool wasInsertedByParser, bool alreadyStarted = false);
 
-    String text() const { return scriptContent(); }
+    String text() { return textFromChildren(); }
     void setText(const String&);
 
     KURL src() const;
 
     void setAsync(bool);
     bool async() const;
+
+    ScriptElement* scriptElement() const { return m_scriptElement.get(); }
 
 private:
     HTMLScriptElement(const QualifiedName&, Document*, bool wasInsertedByParser, bool alreadyStarted);
@@ -65,6 +67,8 @@ private:
     virtual void dispatchLoadEvent();
 
     virtual PassRefPtr<Element> cloneElementWithoutAttributesAndChildren();
+
+    OwnPtr<ScriptElement> m_scriptElement;
 };
 
 inline HTMLScriptElement* toHTMLScriptElement(Node* node)

Source/core/svg/SVGScriptElement.cpp
@@ -44,8 +44,8 @@ END_REGISTER_ANIMATED_PROPERTIES
 
 inline SVGScriptElement::SVGScriptElement(const QualifiedName& tagName, Document* document, bool wasInsertedByParser, bool alreadyStarted)
     : SVGElement(tagName, document)
-    , ScriptElement(this, wasInsertedByParser, alreadyStarted)
     , m_svgLoadEventTimer(this, &SVGElement::svgLoadEventTimerFired)
+    , m_scriptElement(ScriptElement::create(this, wasInsertedByParser, alreadyStarted))
 {
     ASSERT(hasTagName(SVGNames::scriptTag));
     ScriptWrappable::init(this);
@@ -107,7 +107,7 @@ void SVGScriptElement::svgAttributeChanged(const QualifiedName& attrName)
         return;
 
     if (SVGURIReference::isKnownAttribute(attrName)) {
-        handleSourceAttribute(href());
+        m_scriptElement->handleSourceAttribute(href());
         return;
     }
 
@@ -120,7 +120,7 @@ void SVGScriptElement::svgAttributeChanged(const QualifiedName& attrName)
 Node::InsertionNotificationRequest SVGScriptElement::insertedInto(ContainerNode* rootParent)
 {
     SVGElement::insertedInto(rootParent);
-    ScriptElement::insertedInto(rootParent);
+    m_scriptElement->insertedInto(rootParent);
     if (rootParent->inDocument())
         SVGExternalResourcesRequired::insertedIntoDocument(this);
     return InsertionDone;
@@ -129,7 +129,7 @@ Node::InsertionNotificationRequest SVGScriptElement::insertedInto(ContainerNode*
 void SVGScriptElement::childrenChanged(bool changedByParser, Node* beforeChange, Node* afterChange, int childCountDelta)
 {
     SVGElement::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
-    ScriptElement::childrenChanged();
+    m_scriptElement->childrenChanged();
 }
 
 bool SVGScriptElement::isURLAttribute(const Attribute& attribute) const
@@ -207,7 +207,27 @@ bool SVGScriptElement::hasSourceAttribute() const
 
 PassRefPtr<Element> SVGScriptElement::cloneElementWithoutAttributesAndChildren()
 {
-    return adoptRef(new SVGScriptElement(tagQName(), document(), false, alreadyStarted()));
+    return adoptRef(new SVGScriptElement(tagQName(), document(), false, m_scriptElement->alreadyStarted()));
+}
+
+void SVGScriptElement::setHaveFiredLoadEvent(bool haveFiredLoadEvent)
+{
+    m_scriptElement->setHaveFiredLoadEvent(haveFiredLoadEvent);
+}
+
+bool SVGScriptElement::isParserInserted() const
+{
+    return m_scriptElement->isParserInserted();
+}
+
+bool SVGScriptElement::haveFiredLoadEvent() const
+{
+    return m_scriptElement->haveFiredLoadEvent();
+}
+
+Timer<SVGElement>* SVGScriptElement::svgLoadEventTimer()
+{
+    return &m_svgLoadEventTimer;
 }
 
 }

Source/core/svg/SVGScriptElement.h
@@ -31,15 +31,18 @@
 
 namespace WebCore {
 
-class SVGScriptElement FINAL : public SVGElement
-                             , public SVGURIReference
-                             , public SVGExternalResourcesRequired
-                             , public ScriptElement {
+class SVGScriptElement FINAL
+    : public SVGElement
+    , public SVGURIReference
+    , public SVGExternalResourcesRequired
+    , public ScriptElementClient {
 public:
     static PassRefPtr<SVGScriptElement> create(const QualifiedName&, Document*, bool wasInsertedByParser);
 
     String type() const;
     void setType(const String&);
+
+    ScriptElement* scriptElement() const { return m_scriptElement.get(); }
 
 private:
     SVGScriptElement(const QualifiedName&, Document*, bool wasInsertedByParser, bool alreadyStarted);
@@ -72,10 +75,10 @@ private:
     virtual PassRefPtr<Element> cloneElementWithoutAttributesAndChildren();
 
     // SVGExternalResourcesRequired
-    virtual void setHaveFiredLoadEvent(bool haveFiredLoadEvent) { ScriptElement::setHaveFiredLoadEvent(haveFiredLoadEvent); }
-    virtual bool isParserInserted() const { return ScriptElement::isParserInserted(); }
-    virtual bool haveFiredLoadEvent() const { return ScriptElement::haveFiredLoadEvent(); }
-    virtual Timer<SVGElement>* svgLoadEventTimer() OVERRIDE { return &m_svgLoadEventTimer; }
+    virtual void setHaveFiredLoadEvent(bool) OVERRIDE;
+    virtual bool isParserInserted() const OVERRIDE;
+    virtual bool haveFiredLoadEvent() const OVERRIDE;
+    virtual Timer<SVGElement>* svgLoadEventTimer() OVERRIDE;
 
     BEGIN_DECLARE_ANIMATED_PROPERTIES(SVGScriptElement)
         DECLARE_ANIMATED_STRING(Href, href)
@@ -84,6 +87,7 @@ private:
 
     String m_type;
     Timer<SVGElement> m_svgLoadEventTimer;
+    OwnPtr<ScriptElement> m_scriptElement;
 };
 
 inline SVGScriptElement* toSVGScriptElement(Node* node)

commit 0506f91348bdb0b45c656a0c22cc70cd9ad31f81

Fixed crashes on SVG tests which is caused by r153748.
r153748 moved an ASSERT() from HTMLScriptElement to ScriptLoader,
which is wrong for SVGScriptElement case. This change moves it back
to HTMLScriptElement.

TBR=abarth
BUG=none
TEST=svg/*

Review URL: https://codereview.chromium.org/18132022

git-svn-id: svn://svn.chromium.org/blink/trunk@153770 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/ScriptLoader.cpp
@@ -139,7 +139,6 @@ void ScriptLoader::dispatchErrorEvent()
 
 void ScriptLoader::dispatchLoadEvent()
 {
-    ASSERT(!haveFiredLoadEvent());
     if (ScriptLoaderClient* client = this->client())
         client->dispatchLoadEvent();
     setHaveFiredLoadEvent(true);

Source/core/html/HTMLScriptElement.cpp
@@ -166,6 +166,7 @@ bool HTMLScriptElement::hasSourceAttribute() const
 
 void HTMLScriptElement::dispatchLoadEvent()
 {
+    ASSERT(!m_loader->haveFiredLoadEvent());
     dispatchEvent(Event::create(eventNames().loadEvent, false, false));
 }
 