primiano/blink-gitcs

commit 694f6438aaee94fc0cd2d72c4b3b269b5429d1eb

Reland "Merge the private FrameLoader::urlSelected() into loadFrameRequest()."
This reverts commit 7e855be4cef6db387204f6bb94fb331d91202f39. Originally landed
in r153802.

BUG=none

Review URL: https://chromiumcodereview.appspot.com/18991010

git-svn-id: svn://svn.chromium.org/blink/trunk@153933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/FrameLoader.cpp
@@ -221,37 +221,9 @@ void FrameLoader::setDefersLoading(bool defers)
 void FrameLoader::changeLocation(SecurityOrigin* securityOrigin, const KURL& url, const String& referrer, bool lockBackForwardList, bool refresh)
 {
     m_startingClientRedirect = true;
-    urlSelected(FrameLoadRequest(securityOrigin, ResourceRequest(url, referrer, refresh ? ReloadIgnoringCacheData : UseProtocolCachePolicy), "_self"),
-        0, lockBackForwardList, MaybeSendReferrer);
+    loadFrameRequest(FrameLoadRequest(securityOrigin, ResourceRequest(url, referrer, refresh ? ReloadIgnoringCacheData : UseProtocolCachePolicy), "_self"),
+        lockBackForwardList, 0, 0, MaybeSendReferrer);
     m_startingClientRedirect = false;
-}
-
-void FrameLoader::urlSelected(const KURL& url, const String& passedTarget, PassRefPtr<Event> triggeringEvent, bool lockBackForwardList, ShouldSendReferrer shouldSendReferrer)
-{
-    urlSelected(FrameLoadRequest(m_frame->document()->securityOrigin(), ResourceRequest(url), passedTarget),
-        triggeringEvent, lockBackForwardList, shouldSendReferrer);
-}
-
-void FrameLoader::urlSelected(const FrameLoadRequest& passedRequest, PassRefPtr<Event> triggeringEvent, bool lockBackForwardList, ShouldSendReferrer shouldSendReferrer)
-{
-    ASSERT(!m_suppressOpenerInNewFrame);
-
-    RefPtr<Frame> protect(m_frame);
-    FrameLoadRequest frameRequest(passedRequest);
-
-    if (m_frame->script()->executeScriptIfJavaScriptURL(frameRequest.resourceRequest().url()))
-        return;
-
-    if (frameRequest.frameName().isEmpty())
-        frameRequest.setFrameName(m_frame->document()->baseTarget());
-
-    if (shouldSendReferrer == NeverSendReferrer)
-        m_suppressOpenerInNewFrame = true;
-    addHTTPOriginIfNeeded(frameRequest.resourceRequest(), outgoingOrigin());
-
-    loadFrameRequest(frameRequest, lockBackForwardList, triggeringEvent, 0, shouldSendReferrer);
-
-    m_suppressOpenerInNewFrame = false;
 }
 
 void FrameLoader::submitForm(PassRefPtr<FormSubmission> submission)
@@ -964,15 +936,22 @@ void FrameLoader::prepareForHistoryNavigation()
     }
 }
 
-void FrameLoader::loadFrameRequest(const FrameLoadRequest& request, bool lockBackForwardList,
+void FrameLoader::loadFrameRequest(const FrameLoadRequest& passedRequest, bool lockBackForwardList,
     PassRefPtr<Event> event, PassRefPtr<FormState> formState, ShouldSendReferrer shouldSendReferrer)
-{    
+{
+    ASSERT(!m_suppressOpenerInNewFrame);
+
     // Protect frame from getting blown away inside dispatchBeforeLoadEvent in loadWithDocumentLoader.
     RefPtr<Frame> protect(m_frame);
 
+    FrameLoadRequest request(passedRequest);
     KURL url = request.resourceRequest().url();
 
+    if (m_frame->script()->executeScriptIfJavaScriptURL(url))
+        return;
+
     ASSERT(m_frame->document());
+    // FIXME: There are a bunch of FrameLoaderRequest::requester() checks in this function, and it's messy.
     if (request.requester() && !request.requester()->canDisplay(url)) {
         reportLocalLoadFailed(m_frame, url.elidedString());
         return;
@@ -1004,18 +983,13 @@ void FrameLoader::loadFrameRequest(const FrameLoadRequest& request, bool lockBac
     else
         loadType = FrameLoadTypeStandard;
 
-    loadURL(resourceRequest, request.frameName(), loadType, event, formState.get(), request.substituteData());
+    if (request.requester() && request.frameName().isEmpty())
+        request.setFrameName(m_frame->document()->baseTarget());
+    if (shouldSendReferrer == NeverSendReferrer)
+        m_suppressOpenerInNewFrame = true;
 
-    // FIXME: It's possible this targetFrame will not be the same frame that was targeted by the actual
-    // load if frame names have changed.
-    Frame* sourceFrame = formState ? formState->sourceDocument()->frame() : m_frame;
-    if (!sourceFrame)
-        sourceFrame = m_frame;
-    Frame* targetFrame = sourceFrame->loader()->findFrameForNavigation(request.frameName());
-    if (targetFrame && targetFrame != sourceFrame) {
-        if (Page* page = targetFrame->page())
-            page->chrome().focus();
-    }
+    loadURL(resourceRequest, request.frameName(), loadType, event, formState.get(), request.substituteData());
+    m_suppressOpenerInNewFrame = false;
 }
 
 void FrameLoader::loadURL(const ResourceRequest& request, const String& frameName, FrameLoadType newLoadType, PassRefPtr<Event> event, PassRefPtr<FormState> formState, const SubstituteData& substituteData)

LayoutTests/fast/frames/sandboxed-iframe-navigation-targetlink-expected.txt
@@ -1,7 +1,5 @@
 CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'about:blank' from frame with URL 'sandboxed-iframe-navigation-targetlink.html'. The frame attempting navigation is sandboxed, and is therefore disallowed from navigating its ancestors.
 
-CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'about:blank' from frame with URL 'sandboxed-iframe-navigation-targetlink.html'. The frame attempting navigation is sandboxed, and is therefore disallowed from navigating its ancestors.
-
 This test verifies that a sandboxed IFrame cannot open a link in another frame using the target attribute of a link.
 
 This is done by loading ten non-sandboxed IFrames, and a single sandboxed one. In addition each of these frames have a target frame (so, 22 frames in total). Expect ten frames to be able to open a link in their corresponding target frame, but the sandboxed one to not be one of them.

LayoutTests/http/tests/security/frameNavigation/xss-DENIED-plugin-navigation-expected.txt
@@ -1,7 +1,5 @@
 CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'http://127.0.0.1:8000/security/resources/cross-frame-iframe.html' from frame with URL 'http://localhost:8000/security/frameNavigation/resources/frame-with-plugin-to-navigate.html'. The frame attempting navigation is neither same-origin with the target, nor is it the target's parent or opener.
 
-CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'http://127.0.0.1:8000/security/resources/cross-frame-iframe.html' from frame with URL 'http://localhost:8000/security/frameNavigation/resources/frame-with-plugin-to-navigate.html'. The frame attempting navigation is neither same-origin with the target, nor is it the target's parent or opener.
-
  
 
 --------

LayoutTests/http/tests/security/frameNavigation/xss-DENIED-targeted-link-navigation-expected.txt
@@ -1,7 +1,5 @@
 CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'http://127.0.0.1:8000/security/resources/cross-frame-iframe.html' from frame with URL 'http://localhost:8000/security/frameNavigation/resources/frame-with-link-to-navigate.html'. The frame attempting navigation is neither same-origin with the target, nor is it the target's parent or opener.
 
-CONSOLE MESSAGE: Unsafe JavaScript attempt to initiate navigation for frame with URL 'http://127.0.0.1:8000/security/resources/cross-frame-iframe.html' from frame with URL 'http://localhost:8000/security/frameNavigation/resources/frame-with-link-to-navigate.html'. The frame attempting navigation is neither same-origin with the target, nor is it the target's parent or opener.
-
  
 
 --------

Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -60,6 +60,8 @@
 #include "core/dom/MouseEvent.h"
 #include "core/dom/Range.h"
 #include "core/editing/FrameSelection.h"
+#include "core/html/HTMLFormElement.h"
+#include "core/loader/FrameLoadRequest.h"
 #include "core/page/EventHandler.h"
 #include "core/page/Frame.h"
 #include "core/page/FrameView.h"
@@ -3377,7 +3379,8 @@ TEST_F(WebFrameTest, SimulateFragmentAnchorMiddleClick)
 
     RefPtr<WebCore::Event> event = WebCore::MouseEvent::create(WebCore::eventNames().clickEvent, false, false,
         document->defaultView(), 0, 0, 0, 0, 0, 0, 0, false, false, false, false, 1, 0, 0);
-    webViewImpl->page()->mainFrame()->loader()->urlSelected(destination, "", event.release(), false, WebCore::MaybeSendReferrer);
+    WebCore::FrameLoadRequest frameRequest(document->securityOrigin(), WebCore::ResourceRequest(destination));
+    webViewImpl->page()->mainFrame()->loader()->loadFrameRequest(frameRequest, false, event.release(), 0, WebCore::MaybeSendReferrer);
 
     m_webView->close();
     m_webView = 0;

Source/core/html/HTMLAnchorElement.cpp
@@ -34,8 +34,10 @@
 #include "core/dom/KeyboardEvent.h"
 #include "core/dom/MouseEvent.h"
 #include "core/editing/FrameSelection.h"
+#include "core/html/HTMLFormElement.h"
 #include "core/html/HTMLImageElement.h"
 #include "core/html/parser/HTMLParserIdioms.h"
+#include "core/loader/FrameLoadRequest.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/FrameLoaderClient.h"
 #include "core/loader/FrameLoaderTypes.h"
@@ -566,22 +568,24 @@ void HTMLAnchorElement::handleClick(Event* event)
     StringBuilder url;
     url.append(stripLeadingAndTrailingHTMLSpaces(fastGetAttribute(hrefAttr)));
     appendServerMapMousePosition(url, event);
-    KURL kurl = document()->completeURL(url.toString());
+    KURL completedURL = document()->completeURL(url.toString());
 
     if (hasAttribute(downloadAttr)) {
-        ResourceRequest request(kurl);
+        ResourceRequest request(completedURL);
 
         if (!hasRel(RelationNoReferrer)) {
-            String referrer = SecurityPolicy::generateReferrerHeader(document()->referrerPolicy(), kurl, frame->loader()->outgoingReferrer());
+            String referrer = SecurityPolicy::generateReferrerHeader(document()->referrerPolicy(), completedURL, frame->loader()->outgoingReferrer());
             if (!referrer.isEmpty())
                 request.setHTTPReferrer(referrer);
         }
 
         frame->loader()->client()->loadURLExternally(request, NavigationPolicyDownload, fastGetAttribute(downloadAttr));
-    } else
-        frame->loader()->urlSelected(kurl, target(), event, false, hasRel(RelationNoReferrer) ? NeverSendReferrer : MaybeSendReferrer);
+    } else {
+        FrameLoadRequest frameRequest(document()->securityOrigin(), ResourceRequest(completedURL), target());
+        frame->loader()->loadFrameRequest(frameRequest, false, event, 0, hasRel(RelationNoReferrer) ? NeverSendReferrer : MaybeSendReferrer);
+    }
 
-    sendPings(kurl);
+    sendPings(completedURL);
 }
 
 HTMLAnchorElement::EventType HTMLAnchorElement::eventType(Event* event)

Source/core/loader/FrameLoader.h
@@ -100,7 +100,6 @@ public:
     unsigned long loadResourceSynchronously(const ResourceRequest&, StoredCredentials, ResourceError&, ResourceResponse&, Vector<char>& data);
 
     void changeLocation(SecurityOrigin*, const KURL&, const String& referrer, bool lockBackForwardList = true, bool refresh = false);
-    void urlSelected(const KURL&, const String& target, PassRefPtr<Event>, bool lockBackForwardList, ShouldSendReferrer);
     void submitForm(PassRefPtr<FormSubmission>);
 
     void reload(bool endToEndReload = false, const KURL& overrideURL = KURL(), const String& overrideEncoding = String());
@@ -290,8 +289,6 @@ private:
     void closeOldDataSources();
 
     void dispatchDidCommitLoad();
-
-    void urlSelected(const FrameLoadRequest&, PassRefPtr<Event>, bool lockBackForwardList, ShouldSendReferrer);
 
     // Calls continueLoadAfterNavigationPolicy
     void loadWithNavigationAction(const ResourceRequest&, const NavigationAction&,

Source/core/loader/NavigationScheduler.cpp
@@ -186,9 +186,10 @@ public:
         OwnPtr<UserGestureIndicator> gestureIndicator = createUserGestureIndicator();
 
         if (!m_historySteps) {
+            FrameLoadRequest frameRequest(frame->document()->securityOrigin(), ResourceRequest(frame->document()->url()));
             // Special case for go(0) from a frame -> reload only the frame
             // To follow Firefox and IE's behavior, history reload can only navigate the self frame.
-            frame->loader()->urlSelected(frame->document()->url(), "_self", 0, lockBackForwardList(), MaybeSendReferrer);
+            frame->loader()->loadFrameRequest(frameRequest, lockBackForwardList(), 0, 0, MaybeSendReferrer);
             return;
         }
         // go(i!=0) from a frame navigates into the history of the frame only,

commit 0c4f728ab8e73bcf5f13f5ad65543c15057c69d7

A form's target attribute should overrule a base element's target attribute.
This was regressed in r153933.

A form finds its target frame before the form submission in scheduled. When it
actually fires and FrameLoader::load() is called, the FrameLoadRequest does not
include the target frame name. Therefore, we need to special case form
submissions when falling back to the base's target attribute.

BUG=262962

Review URL: https://chromiumcodereview.appspot.com/20059002

git-svn-id: svn://svn.chromium.org/blink/trunk@154785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/loader/FrameLoader.cpp
@@ -943,7 +943,7 @@ bool FrameLoader::prepareRequestForThisFrame(FrameLoadRequest& request)
         return false;
     }
 
-    if (request.requester() && request.frameName().isEmpty())
+    if (request.requester() && !request.formState() && request.frameName().isEmpty())
         request.setFrameName(m_frame->document()->baseTarget());
 
     // If the requesting SecurityOrigin is not this Frame's SecurityOrigin, the request was initiated by a different frame that should

LayoutTests/http/tests/misc/form-base-target-blank-expected.txt
@@ -1,0 +1,5 @@
+Test that if a form is submitted from a document with a base element that sets a target,
+the form's target attribute takes precedence over the base's target attribute.
+This was regressed in http://src.chromium.org/viewvc/blink/?view=revision&revision=153933
+This test will timeout if it fails.
+

LayoutTests/http/tests/misc/form-base-target-blank.html
@@ -1,0 +1,17 @@
+<html>
+<head>
+<base target="_blank"></base>
+</head>
+<body>
+<form target="_self" method="post" action="resources/form-base-target-blank.html" name="f">
+<input type="submit" value="Go">
+</form>
+<script>
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+}
+document.f.submit();
+</script>
+</body>
+</html>

LayoutTests/http/tests/misc/resources/form-base-target-blank.html
@@ -1,0 +1,10 @@
+<body>
+Test that if a form is submitted from a document with a base element that sets a target,<br>
+the form's target attribute takes precedence over the base's target attribute.<br>
+This was regressed in http://src.chromium.org/viewvc/blink/?view=revision&revision=153933<br>
+This test will timeout if it fails.<br>
+<script>
+if (window.testRunner)
+    testRunner.notifyDone();
+</script>
+</body>