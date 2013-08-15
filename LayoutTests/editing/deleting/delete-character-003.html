primiano/blink-gitcs

commit 4411025542af12d860957b9fa3bcbfcd41d717d8

Avoid adding placeholder when deleting last text in root
Editing code uses placeholders for all manner of different functions including
holding open content editable blocks. When there's no content in the blocks except
text, these placeholders aren't necessary and is confusing Android's IME path.

Android's IME path should probably just be fixed, but in the meantime, this
creates fewer placeholders, which is definitely an improvement.

The previous version of this patch was rolled out due to crashes, see
https://codereview.chromium.org/14969020. This patch adds a check in
cleanupAfterDeletion to ensure we don't remove ancestors of our destination node,
and yosin@ landed another fix in https://codereview.chromium.org/16053005/.

BUG=222806, 236726

Review URL: https://chromiumcodereview.appspot.com/18644008

git-svn-id: svn://svn.chromium.org/blink/trunk@154347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/editing/DeleteSelectionCommand.cpp
@@ -772,9 +772,12 @@ void DeleteSelectionCommand::doApply()
     EAffinity affinity = m_selectionToDelete.affinity();
     
     Position downstreamEnd = m_selectionToDelete.end().downstream();
-    m_needPlaceholder = isStartOfParagraph(m_selectionToDelete.visibleStart(), CanCrossEditingBoundary)
-            && isEndOfParagraph(m_selectionToDelete.visibleEnd(), CanCrossEditingBoundary)
-            && !lineBreakExistsAtVisiblePosition(m_selectionToDelete.visibleEnd());
+    bool rootWillStayOpenWithoutPlaceholder = downstreamEnd.containerNode() == downstreamEnd.containerNode()->rootEditableElement()
+        || (downstreamEnd.containerNode()->isTextNode() && downstreamEnd.containerNode()->parentNode() == downstreamEnd.containerNode()->rootEditableElement());
+    m_needPlaceholder = !rootWillStayOpenWithoutPlaceholder
+        && isStartOfParagraph(m_selectionToDelete.visibleStart(), CanCrossEditingBoundary)
+        && isEndOfParagraph(m_selectionToDelete.visibleEnd(), CanCrossEditingBoundary)
+        && !lineBreakExistsAtVisiblePosition(m_selectionToDelete.visibleEnd());
     if (m_needPlaceholder) {
         // Don't need a placeholder when deleting a selection that starts just before a table
         // and ends inside it (we do need placeholders to hold open empty cells, but that's

LayoutTests/TestExpectations
@@ -1304,6 +1304,21 @@ crbug.com/237250 [ Lion Debug ] media/video-zoom-controls.html [ Crash Pass ]
 crbug.com/237270 [ Win7 ] transforms/2d/transform-2d.html [ Pass Timeout ]
 crbug.com/237270 [ Debug ] fast/dom/SelectorAPI/resig-SelectorsAPI-test.xhtml [ Pass Timeout ]
 # was: webkit.org/b/110873 - Flaky on most platforms
+
+crbug.com/222806 editing/deleting/5272440.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-block-contents-001.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-block-contents-002.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-block-contents-003.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-3775172-fix.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-all-text-in-text-field-assertion.html [ NeedsRebaseline ]
+crbug.com/222806 editing/deleting/delete-image-004.html [ NeedsRebaseline ]
+crbug.com/222806 editing/inserting/insert-3775316-fix.html [ NeedsRebaseline ]
+crbug.com/222806 editing/inserting/insert-after-delete-001.html [ NeedsRebaseline ]
+crbug.com/222806 editing/pasteboard/4076267-3.html [ NeedsRebaseline ]
+crbug.com/222806 editing/pasteboard/smart-paste-in-text-control.html [ NeedsRebaseline ]
+crbug.com/222806 fast/forms/input-placeholder-visibility-3.html [ NeedsRebaseline ]
+crbug.com/222806 fast/forms/textarea-placeholder-visibility-1.html [ NeedsRebaseline ]
+
 crbug.com/237270 [ Debug ] fast/sub-pixel/transformed-iframe-copy-on-scroll.html [ Pass Timeout ]
 crbug.com/251129 [ Linux Win Debug ] http/tests/history/redirect-js-form-submit-0-seconds.html [ Failure Pass ]
 

LayoutTests/editing/deleting/5847330-2-expected.txt
@@ -1,3 +1,2 @@
 This tests for a crash when deleting inside an empty list item that is an editable root.
 
-

LayoutTests/editing/deleting/delete-across-editable-content-boundaries-1-expected.txt
@@ -1,2 +1,2 @@
 This tests the deletion of non-editable content that both starts and ends in editable roots - it should successfully be removed. There should be no visible content in the markup below. <radr://problem/5026848>
-| <br>
+| 

LayoutTests/editing/deleting/delete-and-cleanup-expected.txt
@@ -1,6 +1,6 @@
 This test checks that deletion does not leave unnecessary nested divs.
 
-PASS confirmedMarkup is '<br>'
+PASS confirmedMarkup is ''
 PASS confirmedMarkup is '<br>'
 PASS confirmedMarkup is '<div id="mydiv"><br></div>'
 PASS confirmedMarkup is '<br>'

LayoutTests/editing/deleting/delete-and-cleanup.html
@@ -33,7 +33,7 @@ function testDelete(tagName, originalMarkup, expected) {
     shouldBe("confirmedMarkup", "'" + expected + "'");
 }
 
-testDelete("div", "Hello", "<br>");
+testDelete("div", "Hello", "");
 testDelete("div", "<div>Hello</div>", "<br>");
 testDelete("div", "<div id=\"mydiv\">Hello</div>", "<div id=\"mydiv\"><br></div>");
 testDelete("div", "<div><div>Hello</div></div>", "<br>");

LayoutTests/editing/deleting/delete-block-merge-contents-025-expected.txt
@@ -1,5 +1,3 @@
 This is to test a usecase in which caret position after deletion and the destination position coincides. To pass this testcase it should not crash.
 
 000A0
-
-

LayoutTests/editing/deleting/delete-select-all-002-expected.txt
@@ -4,8 +4,6 @@ EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotificatio
 EDITING DELEGATE: shouldChangeSelectedDOMRange:range from 1 of #text > DIV > BODY > HTML > #document to 1 of #text > DIV > BODY > HTML > #document toDOMRange:range from 0 of #text > SPAN > DIV > BODY > HTML > #document to 17 of #text > SPAN > DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
 EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotification
 EDITING DELEGATE: shouldDeleteDOMRange:range from 0 of #text > SPAN > DIV > BODY > HTML > #document to 17 of #text > SPAN > DIV > BODY > HTML > #document
-EDITING DELEGATE: shouldChangeSelectedDOMRange:range from 0 of DIV > BODY > HTML > #document to 0 of DIV > BODY > HTML > #document toDOMRange:range from 0 of DIV > BODY > HTML > #document to 0 of DIV > BODY > HTML > #document affinity:NSSelectionAffinityDownstream stillSelecting:FALSE
 EDITING DELEGATE: webViewDidChangeSelection:WebViewDidChangeSelectionNotification
 EDITING DELEGATE: webViewDidChange:WebViewDidChangeNotification
-
-execDeleteCommand: <br>
+execDeleteCommand:

commit 9de577f23ae62b5bd1634f3d2aff5dfb658ada30

Insert placeholder when removing selection at end of editable after line break
This patch changes delete selection command to insert a placeholder when deleting selection at end of editable root after line break.

This patch fixes regression introduced by r154347, Avoid adding placeholder when deleting last text in root.

BUG=264986
TEST=LayoutTests/editing/deleting/delete-character-002.html, LayoutTests/editing/deleting/delete-character-003.html
R=leviw@chromium.org

Review URL: https://codereview.chromium.org/21044002

git-svn-id: svn://svn.chromium.org/blink/trunk@156139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/editing/DeleteSelectionCommand.cpp
@@ -814,6 +814,14 @@ void DeleteSelectionCommand::doApply()
 
     removePreviouslySelectedEmptyTableRows();
 
+    if (!m_needPlaceholder && rootWillStayOpenWithoutPlaceholder) {
+        VisiblePosition visualEnding(m_endingPosition);
+        m_needPlaceholder = !isStartOfParagraph(visualEnding, CanCrossEditingBoundary)
+            && isEndOfParagraph(visualEnding, CanCrossEditingBoundary)
+            && lineBreakExistsAtVisiblePosition(visualEnding)
+            && visualEnding.next(CannotCrossEditingBoundary).isNull();
+    }
+
     RefPtr<Node> placeholder = m_needPlaceholder ? createBreakElement(document()).get() : 0;
 
     if (placeholder) {

LayoutTests/editing/deleting/delete-character-002-expected.txt
@@ -1,0 +1,7 @@
+PASS sample.value is "a\n"
+PASS sample.selectionStart is 2
+PASS sample.selectionEnd is 2
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/delete-character-002.html
@@ -1,0 +1,20 @@
+<div id="container">
+<textarea id="sample"></textarea>
+</div>
+<script src="../../fast/js/resources/js-test-pre.js"></script>
+<script>
+function $(id) { return document.getElementById(id); }
+var sample = $('sample');
+sample.value = 'a\nb';
+sample.setSelectionRange(sample.value.length, sample.value.length);
+sample.focus();
+document.execCommand('Delete', false);
+
+shouldBeEqualToString('sample.value', 'a\n');
+shouldBe('sample.selectionStart', '2');
+shouldBe('sample.selectionEnd', '2');
+
+if (window.testRunner)
+    $('container').outerHTML = '';
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>

LayoutTests/editing/deleting/delete-character-003-expected.txt
@@ -1,0 +1,8 @@
+PASS sample.innerHTML is "a<br><br>"
+PASS selection.type is "Caret"
+PASS selection.focusNode is sample
+PASS selection.focusOffset is 2
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/delete-character-003.html
@@ -1,0 +1,26 @@
+<div id="container">
+<div id="sample" contenteditable="true"></div>
+</div>
+<script src="../../fast/js/resources/js-test-pre.js"></script>
+<script>
+function $(id) { return document.getElementById(id); }
+var sample = $('sample');
+sample.innerHTML = 'a<br>b';
+sample.focus();
+var range = document.createRange();
+range.selectNodeContents(sample);
+range.collapse(false);
+var selection = window.getSelection();
+selection.removeAllRanges();
+selection.addRange(range);
+document.execCommand('Delete', false);
+
+shouldBeEqualToString('sample.innerHTML', 'a<br><br>');
+shouldBeEqualToString('selection.type', 'Caret');
+shouldBe('selection.focusNode', 'sample');
+shouldBe('selection.focusOffset', '2');
+
+if (window.testRunner)
+    $('container').outerHTML = '';
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>