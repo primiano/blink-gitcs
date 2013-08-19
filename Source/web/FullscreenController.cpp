primiano/blink-gitcs

commit cad67a324a1072557e7b608f9a6df1b27973802d

Refactoring: Rename isMediaElement to isHTMLMediaElement, toMediaElement to toHTMLMediaElement for consistency, and use toHTMLMediaElement isntead of static_cast.
Type check functions should be named as isClassName, and cast functions should
be named as toClassName.

BUG=

Review URL: https://chromiumcodereview.appspot.com/23122004

git-svn-id: svn://svn.chromium.org/blink/trunk@156279 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/web/FullscreenController.cpp
@@ -171,7 +171,7 @@ void FullscreenController::exitFullScreenForElement(WebCore::Element* element)
         return;
 #if USE(NATIVE_FULLSCREEN_VIDEO)
     if (element && element->isMediaElement()) {
-        HTMLMediaElement* mediaElement = static_cast<HTMLMediaElement*>(element);
+        HTMLMediaElement* mediaElement = toHTMLMediaElement(element);
         if (mediaElement->player())
             mediaElement->player()->exitFullscreen();
         return;

Source/core/html/HTMLMediaElement.h
@@ -600,14 +600,14 @@ struct ValueToString<TextTrackCue*> {
 };
 #endif
 
-inline bool isMediaElement(Node* node)
+inline bool isHTMLMediaElement(Node* node)
 {
     return node && node->isElementNode() && toElement(node)->isMediaElement();
 }
 
-inline HTMLMediaElement* toMediaElement(Node* node)
+inline HTMLMediaElement* toHTMLMediaElement(Node* node)
 {
-    ASSERT_WITH_SECURITY_IMPLICATION(!node || isMediaElement(node));
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || isHTMLMediaElement(node));
     return static_cast<HTMLMediaElement*>(node);
 }
 

Source/core/html/HTMLSourceElement.cpp
@@ -57,7 +57,7 @@ Node::InsertionNotificationRequest HTMLSourceElement::insertedInto(ContainerNode
     HTMLElement::insertedInto(insertionPoint);
     Element* parent = parentElement();
     if (parent && parent->isMediaElement())
-        static_cast<HTMLMediaElement*>(parentNode())->sourceWasAdded(this);
+        toHTMLMediaElement(parentNode())->sourceWasAdded(this);
     return InsertionDone;
 }
 
@@ -67,7 +67,7 @@ void HTMLSourceElement::removedFrom(ContainerNode* removalRoot)
     if (!parent && removalRoot->isElementNode())
         parent = toElement(removalRoot);
     if (parent && parent->isMediaElement())
-        toMediaElement(parent)->sourceWasRemoved(this);
+        toHTMLMediaElement(parent)->sourceWasRemoved(this);
     HTMLElement::removedFrom(removalRoot);
 }
 

Source/core/html/HTMLTrackElement.cpp
@@ -87,8 +87,8 @@ Node::InsertionNotificationRequest HTMLTrackElement::insertedInto(ContainerNode*
 
 void HTMLTrackElement::removedFrom(ContainerNode* insertionPoint)
 {
-    if (!parentNode() && WebCore::isMediaElement(insertionPoint))
-        toMediaElement(insertionPoint)->didRemoveTrack(this);
+    if (!parentNode() && isHTMLMediaElement(insertionPoint))
+        toHTMLMediaElement(insertionPoint)->didRemoveTrack(this);
     HTMLElement::removedFrom(insertionPoint);
 }
 
@@ -358,8 +358,7 @@ HTMLMediaElement* HTMLTrackElement::mediaElement() const
 {
     Element* parent = parentElement();
     if (parent && parent->isMediaElement())
-        return static_cast<HTMLMediaElement*>(parentNode());
-
+        return toHTMLMediaElement(parentNode());
     return 0;
 }
 

Source/core/html/shadow/MediaControlElementTypes.cpp
@@ -53,7 +53,7 @@ HTMLMediaElement* toParentMediaElement(Node* node)
     if (!mediaNode || !mediaNode->isElementNode() || !toElement(mediaNode)->isMediaElement())
         return 0;
 
-    return static_cast<HTMLMediaElement*>(mediaNode);
+    return toHTMLMediaElement(mediaNode);
 }
 
 MediaControlElementType mediaControlElementType(Node* node)

Source/core/rendering/HitTestResult.cpp
@@ -317,7 +317,7 @@ HTMLMediaElement* HitTestResult::mediaElement() const
         return 0;
 
     if (isHTMLVideoElement(m_innerNonSharedNode.get()) || m_innerNonSharedNode->hasTagName(HTMLNames::audioTag))
-        return static_cast<HTMLMediaElement*>(m_innerNonSharedNode.get());
+        return toHTMLMediaElement(m_innerNonSharedNode.get());
     return 0;
 }
 

Source/core/rendering/RenderLayerBacking.cpp
@@ -450,7 +450,7 @@ bool RenderLayerBacking::updateGraphicsLayerConfiguration()
         PluginView* pluginView = toPluginView(toRenderWidget(renderer)->widget());
         m_graphicsLayer->setContentsToMedia(pluginView->platformLayer());
     } else if (renderer->isVideo()) {
-        HTMLMediaElement* mediaElement = toMediaElement(renderer->node());
+        HTMLMediaElement* mediaElement = toHTMLMediaElement(renderer->node());
         m_graphicsLayer->setContentsToMedia(mediaElement->platformLayer());
     } else if (isAcceleratedCanvas(renderer)) {
         HTMLCanvasElement* canvas = toHTMLCanvasElement(renderer->node());

Source/core/rendering/RenderMedia.cpp
@@ -52,7 +52,7 @@ RenderMedia::~RenderMedia()
 
 HTMLMediaElement* RenderMedia::mediaElement() const
 {
-    return toMediaElement(node());
+    return toHTMLMediaElement(node());
 }
 
 void RenderMedia::layout()

commit 048c972afa0b6b7dd55dbe80bcf88dc44ddbb48d

Fix Android build broken by r156279.
BUG=
TBR=dominicc@chromium.org

Review URL: https://codereview.chromium.org/22852012

git-svn-id: svn://svn.chromium.org/blink/trunk@156282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/web/FullscreenController.cpp
@@ -148,7 +148,7 @@ void FullscreenController::enterFullScreenForElement(WebCore::Element* element)
 
 #if USE(NATIVE_FULLSCREEN_VIDEO)
     if (element && element->isMediaElement()) {
-        HTMLMediaElement* mediaElement = toMediaElement(element);
+        HTMLMediaElement* mediaElement = toHTMLMediaElement(element);
         if (mediaElement->player() && mediaElement->player()->canEnterFullscreen()) {
             mediaElement->player()->enterFullscreen();
             m_provisionalFullScreenElement = element;