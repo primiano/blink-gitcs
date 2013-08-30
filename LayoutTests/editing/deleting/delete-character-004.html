primiano/blink-gitcs

commit 9de577f23ae62b5bd1634f3d2aff5dfb658ada30

Insert placeholder when removing selection at end of editable after line break
This patch changes delete selection command to insert a placeholder when deleting selection at end of editable root after line break.

This patch fixes regression introduced by r154347, Avoid adding placeholder when deleting last text in root.

BUG=264986
TEST=LayoutTests/editing/deleting/delete-character-002.html, LayoutTests/editing/deleting/delete-character-003.html
R=leviw@chromium.org

Review URL: https://codereview.chromium.org/21044002

git-svn-id: svn://svn.chromium.org/blink/trunk@156139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/editing/DeleteSelectionCommand.cpp
@@ -814,6 +814,14 @@ void DeleteSelectionCommand::doApply()
 
     removePreviouslySelectedEmptyTableRows();
 
+    if (!m_needPlaceholder && rootWillStayOpenWithoutPlaceholder) {
+        VisiblePosition visualEnding(m_endingPosition);
+        m_needPlaceholder = !isStartOfParagraph(visualEnding, CanCrossEditingBoundary)
+            && isEndOfParagraph(visualEnding, CanCrossEditingBoundary)
+            && lineBreakExistsAtVisiblePosition(visualEnding)
+            && visualEnding.next(CannotCrossEditingBoundary).isNull();
+    }
+
     RefPtr<Node> placeholder = m_needPlaceholder ? createBreakElement(document()).get() : 0;
 
     if (placeholder) {

LayoutTests/editing/deleting/delete-character-002-expected.txt
@@ -1,0 +1,7 @@
+PASS sample.value is "a\n"
+PASS sample.selectionStart is 2
+PASS sample.selectionEnd is 2
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/delete-character-002.html
@@ -1,0 +1,20 @@
+<div id="container">
+<textarea id="sample"></textarea>
+</div>
+<script src="../../fast/js/resources/js-test-pre.js"></script>
+<script>
+function $(id) { return document.getElementById(id); }
+var sample = $('sample');
+sample.value = 'a\nb';
+sample.setSelectionRange(sample.value.length, sample.value.length);
+sample.focus();
+document.execCommand('Delete', false);
+
+shouldBeEqualToString('sample.value', 'a\n');
+shouldBe('sample.selectionStart', '2');
+shouldBe('sample.selectionEnd', '2');
+
+if (window.testRunner)
+    $('container').outerHTML = '';
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>

LayoutTests/editing/deleting/delete-character-003-expected.txt
@@ -1,0 +1,8 @@
+PASS sample.innerHTML is "a<br><br>"
+PASS selection.type is "Caret"
+PASS selection.focusNode is sample
+PASS selection.focusOffset is 2
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/delete-character-003.html
@@ -1,0 +1,26 @@
+<div id="container">
+<div id="sample" contenteditable="true"></div>
+</div>
+<script src="../../fast/js/resources/js-test-pre.js"></script>
+<script>
+function $(id) { return document.getElementById(id); }
+var sample = $('sample');
+sample.innerHTML = 'a<br>b';
+sample.focus();
+var range = document.createRange();
+range.selectNodeContents(sample);
+range.collapse(false);
+var selection = window.getSelection();
+selection.removeAllRanges();
+selection.addRange(range);
+document.execCommand('Delete', false);
+
+shouldBeEqualToString('sample.innerHTML', 'a<br><br>');
+shouldBeEqualToString('selection.type', 'Caret');
+shouldBe('selection.focusNode', 'sample');
+shouldBe('selection.focusOffset', '2');
+
+if (window.testRunner)
+    $('container').outerHTML = '';
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>

commit 11ce70882e3e4594ce1169ead5b2bc18e2cc76be

Insert placeholder when removing selection at end of editable root after line break
This patch fixes regression introduced by r156139, which inserts placeholder when text after deleting ends with line break, by inserting placeholder when selection to delete followed by line break.

For example, typing backspace key in sample text, where "|" denotes caret:
 1 "ab<br>c|" => "ab<br>"
 2 "abc|<br> => "ab<br>"
r156139 inserts placeholder for example 2.


BUG=277132
TEST=LayoutTests/editing/deleting/delete-character-004.html

Review URL: https://chromiumcodereview.appspot.com/23749002

git-svn-id: svn://svn.chromium.org/blink/trunk@156945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/editing/DeleteSelectionCommand.cpp
@@ -791,6 +791,8 @@ void DeleteSelectionCommand::doApply()
     // set up our state
     initializePositionData();
 
+    bool lineBreakBeforeStart = lineBreakExistsAtVisiblePosition(VisiblePosition(m_upstreamStart).previous());
+
     // Delete any text that may hinder our ability to fixup whitespace after the delete
     deleteInsignificantTextDownstream(m_trailingWhitespace);
 
@@ -819,7 +821,8 @@ void DeleteSelectionCommand::doApply()
         m_needPlaceholder = !isStartOfParagraph(visualEnding, CanCrossEditingBoundary)
             && isEndOfParagraph(visualEnding, CanCrossEditingBoundary)
             && lineBreakExistsAtVisiblePosition(visualEnding)
-            && visualEnding.next(CannotCrossEditingBoundary).isNull();
+            && visualEnding.next(CannotCrossEditingBoundary).isNull()
+            && lineBreakBeforeStart;
     }
 
     RefPtr<Node> placeholder = m_needPlaceholder ? createBreakElement(document()).get() : 0;

LayoutTests/editing/deleting/delete-character-004-expected.txt
@@ -1,0 +1,8 @@
+PASS sample.innerHTML is "ab<br>"
+PASS selection.type is "Caret"
+PASS selection.focusNode is sample.firstChild
+PASS selection.focusOffset is 2
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/delete-character-004.html
@@ -1,0 +1,25 @@
+<div id="container">
+<div id="sample" contenteditable="true"></div>
+</div>
+<script src="../../fast/js/resources/js-test-pre.js"></script>
+<script>
+function $(id) { return document.getElementById(id); }
+var sample = $('sample');
+sample.innerHTML = 'abc<br>';
+sample.focus();
+var range = document.createRange();
+range.setStart(sample.firstChild, 3);
+var selection = window.getSelection();
+selection.removeAllRanges();
+selection.addRange(range);
+document.execCommand('Delete', false);
+
+shouldBeEqualToString('sample.innerHTML', 'ab<br>');
+shouldBeEqualToString('selection.type', 'Caret');
+shouldBe('selection.focusNode', 'sample.firstChild');
+shouldBe('selection.focusOffset', '2');
+
+if (window.testRunner)
+    $('container').outerHTML = '';
+</script>
+<script src="../../fast/js/resources/js-test-post.js"></script>