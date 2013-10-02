primiano/blink-gitcs

commit 627a864f23ed83b11fd54fdc0c561db3dc3af40c

Move geometry classes from core/platform/graphics to platform/geometry
Move the basic geometry classes; FloatPoint3D, FloatPoint, FloatQuad,
FloatRect, FloatSize, IntPoint, IntRectExtent, IntRect, IntSize,
LayoutBoxExtent, LayoutPoint, LayoutRect, and LayoutSize from
core/platform/graphics and into the new platform module.

Also move the support enums TextDirection and WritingMode needed by
BoxExtent as well as AffineTransform and TransformationMatrix as they
are needed by FloatPoint (through the SVGPoint typedef).

BUG=297477
R=abarth@chromium.org, pdr@chromium.org

Review URL: https://codereview.chromium.org/25494003

git-svn-id: svn://svn.chromium.org/blink/trunk@158725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/core.gypi
@@ -2830,7 +2830,6 @@
             'platform/graphics/FontPlatformData.h',
             'platform/graphics/FrameData.cpp',
             'platform/graphics/FrameData.h',
-            'platform/graphics/LayoutBoxExtent.cpp',
             'platform/graphics/GeneratorGeneratedImage.cpp',
             'platform/graphics/GeneratedImage.cpp',
             'platform/graphics/GeneratedImage.h',
@@ -3213,18 +3212,8 @@
             'platform/win/SystemInfo.h',
         ],
         'webcore_platform_geometry_files': [
-            'platform/graphics/FloatPoint.cpp',
             'platform/graphics/FloatPolygon.cpp',
-            'platform/graphics/FloatPoint3D.cpp',
-            'platform/graphics/FloatQuad.cpp',
-            'platform/graphics/FloatRect.cpp',
-            'platform/graphics/FloatSize.cpp',
-            'platform/graphics/IntRect.cpp',
-            'platform/graphics/IntRectExtent.h',
-            'platform/graphics/LayoutRect.cpp',
             'platform/graphics/Region.cpp',
-            'platform/graphics/transforms/TransformationMatrix.cpp',
-            'platform/graphics/transforms/AffineTransform.cpp',
         ],
         'webcore_svg_files': [
             'rendering/style/SVGRenderStyle.cpp',

commit 53a58d07768fe71a02102e98b36607d79d2dec43

Fix build breakage caused by r158725.
Remove out-of-date reference to FloatConversion.h

TBR=leviw@chromium.org
BUG=

Review URL: https://codereview.chromium.org/25741003

git-svn-id: svn://svn.chromium.org/blink/trunk@158729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/core.gypi
@@ -2605,7 +2605,6 @@
             'platform/DragData.cpp',
             'platform/DragImage.cpp',
             'platform/FileMetadata.h',
-            'platform/FloatConversion.h',
             'platform/HashTools.h',
             'platform/KillRing.h',
             'platform/KillRingNone.cpp',