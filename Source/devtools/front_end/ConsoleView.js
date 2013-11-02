primiano/blink-gitcs

commit 37a61d2cc3c641faf6237d42e7fc917e02de4602

DevTools: Click on console tab header should bring focus on console prompt.
R=pfeldman

Review URL: https://codereview.chromium.org/54303005

git-svn-id: svn://svn.chromium.org/blink/trunk@161076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/ConsoleView.js
@@ -72,7 +72,6 @@ WebInspector.ConsoleView = function(hideContextSelector)
     this.messagesElement = document.createElement("div");
     this.messagesElement.id = "console-messages";
     this.messagesElement.className = "monospace";
-    this.messagesElement.addEventListener("click", this._messagesClicked.bind(this), true);
     this.element.appendChild(this.messagesElement);
     this._scrolledToBottom = true;
 
@@ -82,6 +81,8 @@ WebInspector.ConsoleView = function(hideContextSelector)
     this.promptElement.spellcheck = false;
     this.messagesElement.appendChild(this.promptElement);
     this.messagesElement.appendChild(document.createElement("br"));
+
+    this.element.addEventListener("click", this.promptElement.focus.bind(this.promptElement), false);
 
     this.topGroup = new WebInspector.ConsoleGroup(null);
     this.messagesElement.insertBefore(this.topGroup.element, this.promptElement);
@@ -124,6 +125,14 @@ WebInspector.ConsoleView = function(hideContextSelector)
 }
 
 WebInspector.ConsoleView.prototype = {
+    /**
+     * @return {Element}
+     */
+    defaultFocusedElement: function()
+    {
+        return this.promptElement
+    },
+
     _onFiltersToggled: function(event)
     {
         var toggled = /** @type {boolean} */ (event.data);

commit 4301d5cc91b6bc40d633f0f82cb6ba13ee37f5db

DevTools: clicking on console filter input brings focus to console prompt Regressed in r161076
TBR=vsevik

BUG=314331

Review URL: https://codereview.chromium.org/49293005

git-svn-id: svn://svn.chromium.org/blink/trunk@161222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/ConsoleView.js
@@ -72,6 +72,7 @@ WebInspector.ConsoleView = function(hideContextSelector)
     this.messagesElement = document.createElement("div");
     this.messagesElement.id = "console-messages";
     this.messagesElement.className = "monospace";
+    this.messagesElement.addEventListener("click", this._messagesClicked.bind(this), true);
     this.element.appendChild(this.messagesElement);
     this._scrolledToBottom = true;
 
@@ -81,8 +82,6 @@ WebInspector.ConsoleView = function(hideContextSelector)
     this.promptElement.spellcheck = false;
     this.messagesElement.appendChild(this.promptElement);
     this.messagesElement.appendChild(document.createElement("br"));
-
-    this.element.addEventListener("click", this.promptElement.focus.bind(this.promptElement), false);
 
     this.topGroup = new WebInspector.ConsoleGroup(null);
     this.messagesElement.insertBefore(this.topGroup.element, this.promptElement);