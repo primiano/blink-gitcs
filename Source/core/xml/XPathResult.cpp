primiano/blink-gitcs

commit e6e85cadbec1e60088a279413b4851ec9be0e03b

Improve XPathResult exception messages.
BUG=270033

Review URL: https://codereview.chromium.org/77763005

git-svn-id: svn://svn.chromium.org/blink/trunk@162474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/xml/XPathResult.cpp
@@ -27,6 +27,7 @@
 #include "config.h"
 #include "core/xml/XPathResult.h"
 
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
@@ -89,14 +90,14 @@ void XPathResult::convertTo(unsigned short type, ExceptionState& exceptionState)
         case ANY_UNORDERED_NODE_TYPE:
         case FIRST_ORDERED_NODE_TYPE: // This is correct - singleNodeValue() will take care of ordering.
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_resultType = type;
             break;
         case ORDERED_NODE_ITERATOR_TYPE:
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_nodeSet.sort();
@@ -104,7 +105,7 @@ void XPathResult::convertTo(unsigned short type, ExceptionState& exceptionState)
             break;
         case ORDERED_NODE_SNAPSHOT_TYPE:
             if (!m_value.isNodeSet()) {
-                exceptionState.throwUninformativeAndGenericTypeError();
+                exceptionState.throwTypeError(ExceptionMessages::failedToExecute("evaluate", "XPathExpression", "The result is not a node set, and therefore cannot be converted to the desired type."));
                 return;
             }
             m_value.toNodeSet().sort();
@@ -121,7 +122,7 @@ unsigned short XPathResult::resultType() const
 double XPathResult::numberValue(ExceptionState& exceptionState) const
 {
     if (resultType() != NUMBER_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("numberValue", "XPathResult", "The result type is not a number."));
         return 0.0;
     }
     return m_value.toNumber();
@@ -130,7 +131,7 @@ double XPathResult::numberValue(ExceptionState& exceptionState) const
 String XPathResult::stringValue(ExceptionState& exceptionState) const
 {
     if (resultType() != STRING_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("stringValue", "XPathResult", "The result type is not a string."));
         return String();
     }
     return m_value.toString();
@@ -139,7 +140,7 @@ String XPathResult::stringValue(ExceptionState& exceptionState) const
 bool XPathResult::booleanValue(ExceptionState& exceptionState) const
 {
     if (resultType() != BOOLEAN_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("booleanValue", "XPathResult", "The result type is not a boolean."));
         return false;
     }
     return m_value.toBoolean();
@@ -148,7 +149,7 @@ bool XPathResult::booleanValue(ExceptionState& exceptionState) const
 Node* XPathResult::singleNodeValue(ExceptionState& exceptionState) const
 {
     if (resultType() != ANY_UNORDERED_NODE_TYPE && resultType() != FIRST_ORDERED_NODE_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("singleNodeValue", "XPathResult", "The result type is not a single node."));
         return 0;
     }
 
@@ -171,7 +172,7 @@ bool XPathResult::invalidIteratorState() const
 unsigned long XPathResult::snapshotLength(ExceptionState& exceptionState) const
 {
     if (resultType() != UNORDERED_NODE_SNAPSHOT_TYPE && resultType() != ORDERED_NODE_SNAPSHOT_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToGet("snapshotLength", "XPathResult", "The result type is not a snapshot."));
         return 0;
     }
 
@@ -181,12 +182,12 @@ unsigned long XPathResult::snapshotLength(ExceptionState& exceptionState) const
 Node* XPathResult::iterateNext(ExceptionState& exceptionState)
 {
     if (resultType() != UNORDERED_NODE_ITERATOR_TYPE && resultType() != ORDERED_NODE_ITERATOR_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The result type is not an iterator."));
         return 0;
     }
 
     if (invalidIteratorState()) {
-        exceptionState.throwUninformativeAndGenericDOMException(InvalidStateError);
+        exceptionState.throwDOMException(InvalidStateError, ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The document has mutated since the result was returned."));
         return 0;
     }
 
@@ -203,7 +204,7 @@ Node* XPathResult::iterateNext(ExceptionState& exceptionState)
 Node* XPathResult::snapshotItem(unsigned long index, ExceptionState& exceptionState)
 {
     if (resultType() != UNORDERED_NODE_SNAPSHOT_TYPE && resultType() != ORDERED_NODE_SNAPSHOT_TYPE) {
-        exceptionState.throwUninformativeAndGenericTypeError();
+        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The result type is not a snapshot."));
         return 0;
     }
 

LayoutTests/fast/dom/DOMException/XPathException-expected.txt
@@ -3,7 +3,7 @@ Tests the properties of the exception thrown when using XPath.
 On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
 
 
-PASS e.toString() is "TypeError: Type error"
+PASS e.toString() is "TypeError: Failed to read the 'numberValue' property from 'XPathResult': The result type is not a number."
 PASS Object.prototype.toString.call(e) is "[object Error]"
 PASS Object.prototype.toString.call(e.__proto__) is "[object Error]"
 PASS e.constructor.toString() is "function TypeError() { [native code] }"

LayoutTests/fast/dom/DOMException/resources/XPathException.js
@@ -11,7 +11,7 @@ try {
     e = err;
 }
 
-shouldBeEqualToString("e.toString()", "TypeError: Type error");
+shouldBeEqualToString("e.toString()", "TypeError: Failed to read the 'numberValue' property from 'XPathResult': The result type is not a number.");
 shouldBeEqualToString("Object.prototype.toString.call(e)", "[object Error]");
 shouldBeEqualToString("Object.prototype.toString.call(e.__proto__)", "[object Error]");
 shouldBeEqualToString("e.constructor.toString()", "function TypeError() { [native code] }");

LayoutTests/fast/xpath/detached-subtree-invalidate-iterator-expected.txt
@@ -4,7 +4,7 @@ PASS result.invalidIteratorState is false
 PASS result.iterateNext().tagName is 'span'
 Modifying the document...
 PASS result.invalidIteratorState is true
-PASS result.iterateNext() threw exception InvalidStateError: An attempt was made to use an object that is not, or is no longer, usable..
+PASS result.iterateNext() threw exception InvalidStateError: Failed to execute 'iterateNext' on 'XPathResult': The document has mutated since the result was returned..
 PASS successfullyParsed is true
 
 TEST COMPLETE

commit 0abacec305638fcc304107ef7613cd148fce478f

Correct exception message for XPathResult::snapshotItem.
It previously reported the method called as iterateNext.

This fixes a typo introduced in r162474.
https://codereview.chromium.org/77763005/

BUG=270033

Review URL: https://codereview.chromium.org/80943002

git-svn-id: svn://svn.chromium.org/blink/trunk@162479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/xml/XPathResult.cpp
@@ -204,7 +204,7 @@ Node* XPathResult::iterateNext(ExceptionState& exceptionState)
 Node* XPathResult::snapshotItem(unsigned long index, ExceptionState& exceptionState)
 {
     if (resultType() != UNORDERED_NODE_SNAPSHOT_TYPE && resultType() != ORDERED_NODE_SNAPSHOT_TYPE) {
-        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("iterateNext", "XPathResult", "The result type is not a snapshot."));
+        exceptionState.throwTypeError(ExceptionMessages::failedToExecute("snapshotItem", "XPathResult", "The result type is not a snapshot."));
         return 0;
     }
 