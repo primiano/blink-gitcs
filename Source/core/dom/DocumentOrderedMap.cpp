primiano/blink-gitcs

commit d5f51d5fa93a7bc123604e1a55f53ebcc762053e

Simplify DocumentOrderedMap
This CL simplifies DocumentOrderedMap's internal state. Rather than keeping a
map separately from the duplicate count, we now keep the duplicate count in the
same map.

This CL is a fixed version of r155823, which was reverted because it was causing
an assertion in clusterfuzz.

R=abarth, adamk
BUG=265009, 271131

Review URL: https://codereview.chromium.org/117313002

git-svn-id: svn://svn.chromium.org/blink/trunk@164029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/DocumentOrderedMap.cpp
@@ -65,7 +65,6 @@ inline bool keyMatchesLabelForAttribute(StringImpl* key, Element* element)
 void DocumentOrderedMap::clear()
 {
     m_map.clear();
-    m_duplicateCounts.clear();
 }
 
 void DocumentOrderedMap::add(StringImpl* key, Element* element)
@@ -73,29 +72,14 @@ void DocumentOrderedMap::add(StringImpl* key, Element* element)
     ASSERT(key);
     ASSERT(element);
 
-    if (!m_duplicateCounts.contains(key)) {
-        // Fast path. The key is not already in m_duplicateCounts, so we assume that it's
-        // also not already in m_map and try to add it. If that add succeeds, we're done.
-        Map::AddResult addResult = m_map.add(key, element);
-        if (addResult.isNewEntry)
-            return;
+    Map::AddResult addResult = m_map.add(key, MapEntry(element));
+    if (addResult.isNewEntry)
+        return;
 
-        // The add failed, so this key was already cached in m_map.
-        // There are multiple elements with this key. Remove the m_map
-        // cache for this key so get searches for it next time it is called.
-        m_map.remove(addResult.iterator);
-        m_duplicateCounts.add(key);
-    } else {
-        // There are multiple elements with this key. Remove the m_map
-        // cache for this key so get will search for it next time it is called.
-        Map::iterator cachedItem = m_map.find(key);
-        if (cachedItem != m_map.end()) {
-            m_map.remove(cachedItem);
-            m_duplicateCounts.add(key);
-        }
-    }
-
-    m_duplicateCounts.add(key);
+    MapEntry& entry = addResult.iterator->value;
+    ASSERT(entry.count);
+    entry.element = 0;
+    entry.count++;
 }
 
 void DocumentOrderedMap::remove(StringImpl* key, Element* element)
@@ -103,11 +87,20 @@ void DocumentOrderedMap::remove(StringImpl* key, Element* element)
     ASSERT(key);
     ASSERT(element);
 
-    Map::iterator cachedItem = m_map.find(key);
-    if (cachedItem != m_map.end() && cachedItem->value == element)
-        m_map.remove(cachedItem);
-    else
-        m_duplicateCounts.remove(key);
+    Map::iterator it = m_map.find(key);
+    if (it == m_map.end())
+        return;
+
+    MapEntry& entry = it->value;
+    ASSERT(entry.count);
+    if (entry.count == 1) {
+        ASSERT(!entry.element || entry.element == element);
+        m_map.remove(it);
+    } else {
+        if (entry.element == element)
+            entry.element = 0;
+        entry.count--;
+    }
 }
 
 template<bool keyMatches(StringImpl*, Element*)>
@@ -116,23 +109,23 @@ inline Element* DocumentOrderedMap::get(StringImpl* key, const TreeScope* scope)
     ASSERT(key);
     ASSERT(scope);
 
-    Element* element = m_map.get(key);
-    if (element)
+    Map::iterator it = m_map.find(key);
+    if (it == m_map.end())
+        return 0;
+
+    MapEntry& entry = it->value;
+    ASSERT(entry.count);
+    if (entry.element)
+        return entry.element;
+
+    // We know there's at least one node that matches; iterate to find the first one.
+    for (Element* element = ElementTraversal::firstWithin(*scope->rootNode()); element; element = ElementTraversal::next(*element)) {
+        if (!keyMatches(key, element))
+            continue;
+        entry.element = element;
         return element;
-
-    if (m_duplicateCounts.contains(key)) {
-        // We know there's at least one node that matches; iterate to find the first one.
-        ASSERT(scope->rootNode());
-        for (element = ElementTraversal::firstWithin(*scope->rootNode()); element; element = ElementTraversal::next(*element)) {
-            if (!keyMatches(key, element))
-                continue;
-            m_duplicateCounts.remove(key);
-            m_map.set(key, element);
-            return element;
-        }
-        ASSERT_NOT_REACHED();
     }
-
+    ASSERT_NOT_REACHED();
     return 0;
 }
 

Source/core/dom/DocumentOrderedMap.h
@@ -59,23 +59,35 @@ public:
 private:
     template<bool keyMatches(StringImpl*, Element*)> Element* get(StringImpl*, const TreeScope*) const;
 
-    typedef HashMap<StringImpl*, Element*> Map;
+    struct MapEntry {
+        MapEntry()
+            : element(0)
+            , count(0)
+        { }
 
-    // We maintain the invariant that m_duplicateCounts is the count of all elements with a given key
-    // excluding the one referenced in m_map, if any. This means it one less than the total count
-    // when the first node with a given key is cached, otherwise the same as the total count.
+        explicit MapEntry(Element* firstElement)
+            : element(firstElement)
+            , count(1)
+        { }
+
+        Element* element;
+        unsigned count;
+    };
+
+    typedef HashMap<StringImpl*, MapEntry> Map;
+
     mutable Map m_map;
-    mutable HashCountedSet<StringImpl*> m_duplicateCounts;
 };
 
 inline bool DocumentOrderedMap::contains(StringImpl* id) const
 {
-    return m_map.contains(id) || m_duplicateCounts.contains(id);
+    return m_map.contains(id);
 }
 
 inline bool DocumentOrderedMap::containsMultiple(StringImpl* id) const
 {
-    return m_duplicateCounts.contains(id);
+    Map::const_iterator it = m_map.find(id);
+    return it != m_map.end() && it->value.count > 1;
 }
 
 } // namespace WebCore

commit 1993230b4defdf4a996804ed8440c43ad0fa4580

Use a pointer type for values in DocumentOrderedMap
Use a pointer type for values in DocumentOrderedMap (OwnPtr<MapEntry>) instead
of a struct type (MapEntry). This should hopefully fix the performance
regression on Bindings/get-element-by-id micro benchmark caused by r164029 on
some platforms.

While this CL seems to have no performance impact on Linux Desktop, I see a 14%
improvement on Bindings/get-element-by-id micro benchmark on Android (tested on
Galaxy Note 3 device). This may be due to HashTable optimizations when the
empty value for ValueType is zero, or to better caching.

R=esprehn, abarth
BUG=330778

Review URL: https://codereview.chromium.org/123333003

git-svn-id: svn://svn.chromium.org/blink/trunk@164440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/DocumentOrderedMap.cpp
@@ -71,15 +71,15 @@ void DocumentOrderedMap::add(StringImpl* key, Element* element)
     ASSERT(key);
     ASSERT(element);
 
-    Map::AddResult addResult = m_map.add(key, MapEntry(element));
+    Map::AddResult addResult = m_map.add(key, adoptPtr(new MapEntry(element)));
     if (addResult.isNewEntry)
         return;
 
-    MapEntry& entry = addResult.iterator->value;
-    ASSERT(entry.count);
-    entry.element = 0;
-    entry.count++;
-    entry.orderedList.clear();
+    OwnPtr<MapEntry>& entry = addResult.iterator->value;
+    ASSERT(entry->count);
+    entry->element = 0;
+    entry->count++;
+    entry->orderedList.clear();
 }
 
 void DocumentOrderedMap::remove(StringImpl* key, Element* element)
@@ -91,18 +91,18 @@ void DocumentOrderedMap::remove(StringImpl* key, Element* element)
     if (it == m_map.end())
         return;
 
-    MapEntry& entry = it->value;
-    ASSERT(entry.count);
-    if (entry.count == 1) {
-        ASSERT(!entry.element || entry.element == element);
+    OwnPtr<MapEntry>& entry = it->value;
+    ASSERT(entry->count);
+    if (entry->count == 1) {
+        ASSERT(!entry->element || entry->element == element);
         m_map.remove(it);
     } else {
-        if (entry.element == element) {
-            ASSERT(entry.orderedList.isEmpty() || entry.orderedList.first() == element);
-            entry.element = entry.orderedList.size() > 1 ? entry.orderedList[1] : 0;
+        if (entry->element == element) {
+            ASSERT(entry->orderedList.isEmpty() || entry->orderedList.first() == element);
+            entry->element = entry->orderedList.size() > 1 ? entry->orderedList[1] : 0;
         }
-        entry.count--;
-        entry.orderedList.clear();
+        entry->count--;
+        entry->orderedList.clear();
     }
 }
 
@@ -112,20 +112,19 @@ inline Element* DocumentOrderedMap::get(StringImpl* key, const TreeScope* scope)
     ASSERT(key);
     ASSERT(scope);
 
-    Map::iterator it = m_map.find(key);
-    if (it == m_map.end())
+    MapEntry* entry = m_map.get(key);
+    if (!entry)
         return 0;
 
-    MapEntry& entry = it->value;
-    ASSERT(entry.count);
-    if (entry.element)
-        return entry.element;
+    ASSERT(entry->count);
+    if (entry->element)
+        return entry->element;
 
     // We know there's at least one node that matches; iterate to find the first one.
     for (Element* element = ElementTraversal::firstWithin(*scope->rootNode()); element; element = ElementTraversal::next(*element)) {
         if (!keyMatches(key, element))
             continue;
-        entry.element = element;
+        entry->element = element;
         return element;
     }
     ASSERT_NOT_REACHED();
@@ -147,22 +146,22 @@ const Vector<Element*>& DocumentOrderedMap::getAllElementsById(StringImpl* key, 
     if (it == m_map.end())
         return emptyVector;
 
-    MapEntry& entry = it->value;
-    ASSERT(entry.count);
+    OwnPtr<MapEntry>& entry = it->value;
+    ASSERT(entry->count);
 
-    if (entry.orderedList.isEmpty()) {
-        entry.orderedList.reserveCapacity(entry.count);
-        for (Element* element = entry.element ? entry.element : ElementTraversal::firstWithin(*scope->rootNode()); entry.orderedList.size() < entry.count; element = ElementTraversal::next(*element)) {
+    if (entry->orderedList.isEmpty()) {
+        entry->orderedList.reserveCapacity(entry->count);
+        for (Element* element = entry->element ? entry->element : ElementTraversal::firstWithin(*scope->rootNode()); entry->orderedList.size() < entry->count; element = ElementTraversal::next(*element)) {
             ASSERT(element);
             if (!keyMatchesId(key, element))
                 continue;
-            entry.orderedList.uncheckedAppend(element);
+            entry->orderedList.uncheckedAppend(element);
         }
-        if (!entry.element)
-            entry.element = entry.orderedList.first();
+        if (!entry->element)
+            entry->element = entry->orderedList.first();
     }
 
-    return entry.orderedList;
+    return entry->orderedList;
 }
 
 Element* DocumentOrderedMap::getElementByMapName(StringImpl* key, const TreeScope* scope) const

Source/core/dom/DocumentOrderedMap.h
@@ -62,11 +62,6 @@ private:
     template<bool keyMatches(StringImpl*, Element*)> Element* get(StringImpl*, const TreeScope*) const;
 
     struct MapEntry {
-        MapEntry()
-            : element(0)
-            , count(0)
-        { }
-
         explicit MapEntry(Element* firstElement)
             : element(firstElement)
             , count(1)
@@ -77,7 +72,7 @@ private:
         Vector<Element*> orderedList;
     };
 
-    typedef HashMap<StringImpl*, MapEntry> Map;
+    typedef HashMap<StringImpl*, OwnPtr<MapEntry> > Map;
 
     mutable Map m_map;
 };
@@ -90,7 +85,7 @@ inline bool DocumentOrderedMap::contains(StringImpl* id) const
 inline bool DocumentOrderedMap::containsMultiple(StringImpl* id) const
 {
     Map::const_iterator it = m_map.find(id);
-    return it != m_map.end() && it->value.count > 1;
+    return it != m_map.end() && it->value->count > 1;
 }
 
 } // namespace WebCore