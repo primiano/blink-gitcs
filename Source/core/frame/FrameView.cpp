primiano/blink-gitcs

commit 60d0ad2d39932c80e869b2c595ef27b27deb9b74

Settings should not call into inspector
Settings should not call into inspector,
rather inspector should cache original setting
and just change setting when required.

BUG=327476

Review URL: https://codereview.chromium.org/146683003

git-svn-id: svn://svn.chromium.org/blink/trunk@166295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/frame/FrameView.cpp
@@ -380,7 +380,8 @@ void FrameView::setFrameRect(const IntRect& newRect)
     // Autosized font sizes depend on the width of the viewing area.
     if (newRect.width() != oldRect.width()) {
         Page* page = m_frame->page();
-        if (isMainFrame() && page->settings().textAutosizingEnabled()) {
+        bool textAutosizingEnabled = InspectorInstrumentation::overrideTextAutosizing(page, page->settings().textAutosizingEnabled());
+        if (isMainFrame() && textAutosizingEnabled) {
             TextAutosizer* textAutosizer = m_frame->document()->textAutosizer();
             if (textAutosizer) {
                 for (Frame* frame = page->mainFrame(); frame; frame = frame->tree().traverseNext())

Source/bindings/v8/ScriptController.cpp
@@ -559,7 +559,7 @@ bool ScriptController::canExecuteScripts(ReasonForCallingCanExecuteScripts reaso
     }
 
     Settings* settings = m_frame->settings();
-    const bool allowed = m_frame->loader().client()->allowScript(settings && settings->isScriptEnabled());
+    const bool allowed = m_frame->loader().client()->allowScript(settings && settings->scriptEnabled());
     if (!allowed && reason == AboutToExecuteScript)
         m_frame->loader().client()->didNotAllowScript();
     return allowed;

Source/core/fetch/ResourceFetcher.cpp
@@ -506,7 +506,7 @@ bool ResourceFetcher::canRequest(Resource::Type type, const KURL& url, const Res
 
         if (frame()) {
             Settings* settings = frame()->settings();
-            if (!frame()->loader().client()->allowScriptFromSource(!settings || settings->isScriptEnabled(), url)) {
+            if (!frame()->loader().client()->allowScriptFromSource(!settings || settings->scriptEnabled(), url)) {
                 frame()->loader().client()->didNotAllowScript();
                 return false;
             }

Source/core/frame/Settings.cpp
@@ -26,7 +26,6 @@
 #include "config.h"
 #include "core/frame/Settings.h"
 
-#include "core/inspector/InspectorInstrumentation.h"
 #include "platform/scroll/ScrollbarTheme.h"
 
 namespace WebCore {
@@ -65,7 +64,7 @@ static const bool defaultSelectTrailingWhitespaceEnabled = false;
 #endif
 
 Settings::Settings()
-    : m_deviceScaleAdjustment(1.0f)
+    : m_openGLMultisamplingEnabled(false)
 #if HACK_FORCE_TEXT_AUTOSIZING_ON_DESKTOP
     , m_textAutosizingWindowSizeOverride(320, 480)
     , m_textAutosizingEnabled(true)
@@ -73,8 +72,6 @@ Settings::Settings()
     , m_textAutosizingEnabled(false)
 #endif
     SETTINGS_INITIALIZER_LIST
-    , m_isScriptEnabled(false)
-    , m_openGLMultisamplingEnabled(false)
 {
 }
 
@@ -96,14 +93,6 @@ void Settings::invalidate(SettingsDelegate::ChangeType changeType)
         m_delegate->settingsChanged(changeType);
 }
 
-// This is a total hack and should be removed.
-Page* Settings::pageOfShame() const
-{
-    if (!m_delegate)
-        return 0;
-    return m_delegate->page();
-}
-
 void Settings::setTextAutosizingEnabled(bool textAutosizingEnabled)
 {
     if (m_textAutosizingEnabled == textAutosizingEnabled)
@@ -111,11 +100,6 @@ void Settings::setTextAutosizingEnabled(bool textAutosizingEnabled)
 
     m_textAutosizingEnabled = textAutosizingEnabled;
     invalidate(SettingsDelegate::StyleChange);
-}
-
-bool Settings::textAutosizingEnabled() const
-{
-    return InspectorInstrumentation::overrideTextAutosizing(pageOfShame(), m_textAutosizingEnabled);
 }
 
 // FIXME: Move to Settings.in once make_settings can understand IntSize.
@@ -126,23 +110,6 @@ void Settings::setTextAutosizingWindowSizeOverride(const IntSize& textAutosizing
 
     m_textAutosizingWindowSizeOverride = textAutosizingWindowSizeOverride;
     invalidate(SettingsDelegate::StyleChange);
-}
-
-void Settings::setDeviceScaleAdjustment(float deviceScaleAdjustment)
-{
-    m_deviceScaleAdjustment = deviceScaleAdjustment;
-    invalidate(SettingsDelegate::TextAutosizingChange);
-}
-
-float Settings::deviceScaleAdjustment() const
-{
-    return InspectorInstrumentation::overrideFontScaleFactor(pageOfShame(), m_deviceScaleAdjustment);
-}
-
-void Settings::setScriptEnabled(bool isScriptEnabled)
-{
-    m_isScriptEnabled = isScriptEnabled;
-    InspectorInstrumentation::scriptsEnabled(pageOfShame(), m_isScriptEnabled);
 }
 
 void Settings::setMockScrollbarsEnabled(bool flag)

Source/core/frame/Settings.h
@@ -39,8 +39,6 @@
 
 namespace WebCore {
 
-class Page; // For inspector, remove after http://crbug.com/327476
-
 enum EditableLinkBehavior {
     EditableLinkDefaultBehavior,
     EditableLinkAlwaysLive,
@@ -57,23 +55,11 @@ public:
     GenericFontFamilySettings& genericFontFamilySettings() { return m_genericFontFamilySettings; }
 
     void setTextAutosizingEnabled(bool);
-    bool textAutosizingEnabled() const;
-
-    // Compensates for poor text legibility on mobile devices. This value is
-    // multiplied by the font scale factor when performing text autosizing of
-    // websites that do not set an explicit viewport description.
-    void setDeviceScaleAdjustment(float);
-    float deviceScaleAdjustment() const;
+    bool textAutosizingEnabled() const { return m_textAutosizingEnabled; }
 
     // Only set by Layout Tests, and only used if textAutosizingEnabled() returns true.
     void setTextAutosizingWindowSizeOverride(const IntSize&);
     const IntSize& textAutosizingWindowSizeOverride() const { return m_textAutosizingWindowSizeOverride; }
-
-    // Clients that execute script should call ScriptController::canExecuteScripts()
-    // instead of this function. ScriptController::canExecuteScripts() checks the
-    // HTML sandbox, plug-in sandboxing, and other important details.
-    bool isScriptEnabled() const { return m_isScriptEnabled; }
-    void setScriptEnabled(bool);
 
     SETTINGS_GETTERS_AND_SETTERS
 
@@ -93,21 +79,14 @@ private:
 
     void invalidate(SettingsDelegate::ChangeType);
 
-    // FIXME: pageOfShame() is a hack for the inspector code:
-    // http://crbug.com/327476
-    Page* pageOfShame() const;
-
     SettingsDelegate* m_delegate;
 
     GenericFontFamilySettings m_genericFontFamilySettings;
-    float m_deviceScaleAdjustment;
+    bool m_openGLMultisamplingEnabled : 1;
     IntSize m_textAutosizingWindowSizeOverride;
     bool m_textAutosizingEnabled : 1;
 
     SETTINGS_MEMBER_VARIABLES
-
-    bool m_isScriptEnabled : 1;
-    bool m_openGLMultisamplingEnabled : 1;
 };
 
 } // namespace WebCore

Source/core/frame/Settings.in
@@ -286,3 +286,13 @@ compositorDrivenAcceleratedScrollingEnabled initial=false
 # FIXME: This is a temporary flag and should be removed
 # when squashing is ready. (crbug.com/261605)
 layerSquashingEnabled initial=false
+
+# Clients that execute script should call ScriptController::canExecuteScripts()
+# instead of this function. ScriptController::canExecuteScripts() checks the
+# HTML sandbox, plug-in sandboxing, and other important details.
+scriptEnabled initial=false, invalidate=ScriptEnable
+
+# Compensates for poor text legibility on mobile devices. This value is
+# multiplied by the font scale factor when performing text autosizing of
+# websites that do not set an explicit viewport description.
+deviceScaleAdjustment type=double, initial=1.0, invalidate=TextAutosizing

Source/core/frame/SettingsDelegate.h
@@ -56,13 +56,10 @@ public:
         MultisamplingChange,
         ImageLoadingChange,
         TextAutosizingChange,
+        ScriptEnableChange,
     };
 
     virtual void settingsChanged(ChangeType) = 0;
-
-    // FIXME: This is a hack until the Inspector code can
-    // be removed from Settings. http://crbug.com/327476
-    virtual Page* page() = 0;
 
 protected:
     OwnPtr<Settings> const m_settings;

Source/core/inspector/InspectorController.cpp
@@ -453,4 +453,10 @@ void InspectorController::processGPUEvent(double timestamp, int phase, bool fore
         timelineAgent->processGPUEvent(InspectorTimelineAgent::GPUEvent(timestamp, phase, foreign, usedGPUMemoryBytes));
 }
 
+void InspectorController::scriptsEnabled(bool  enabled)
+{
+    if (InspectorPageAgent* pageAgent = m_instrumentingAgents->inspectorPageAgent())
+        pageAgent->scriptsEnabled(enabled);
+}
+
 } // namespace WebCore

commit 131df944dc3e7bce1d998ce48c0606f250fa86e2

Chrome_Win: Renderer Crash - WebCore::FrameView::setFrameRect
This crash is caused by r166295
ismainframe check needs to be added before using the page

BUG=340580

Review URL: https://codereview.chromium.org/151363003

git-svn-id: svn://svn.chromium.org/blink/trunk@166431 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/frame/FrameView.cpp
@@ -379,13 +379,15 @@ void FrameView::setFrameRect(const IntRect& newRect)
 
     // Autosized font sizes depend on the width of the viewing area.
     if (newRect.width() != oldRect.width()) {
-        Page* page = m_frame->page();
-        bool textAutosizingEnabled = InspectorInstrumentation::overrideTextAutosizing(page, page->settings().textAutosizingEnabled());
-        if (isMainFrame() && textAutosizingEnabled) {
-            TextAutosizer* textAutosizer = m_frame->document()->textAutosizer();
-            if (textAutosizer) {
-                for (Frame* frame = page->mainFrame(); frame; frame = frame->tree().traverseNext())
-                    textAutosizer->recalculateMultipliers();
+        if (isMainFrame()) {
+            Page* page = m_frame->page();
+            bool textAutosizingEnabled = InspectorInstrumentation::overrideTextAutosizing(page, page->settings().textAutosizingEnabled());
+            if (textAutosizingEnabled) {
+                TextAutosizer* textAutosizer = m_frame->document()->textAutosizer();
+                if (textAutosizer) {
+                    for (Frame* frame = page->mainFrame(); frame; frame = frame->tree().traverseNext())
+                        textAutosizer->recalculateMultipliers();
+                }
             }
         }
     }