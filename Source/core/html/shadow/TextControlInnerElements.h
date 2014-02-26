primiano/blink-gitcs

commit 758da770cff53ec14a0feb187a304de5a56ab0a1

Add Oilpan transition types for core/speech.
Add Oilpan support to the SpeechInputResult interface object along
with its list version.

R=
BUG=340522

Review URL: https://codereview.chromium.org/170423004

git-svn-id: svn://svn.chromium.org/blink/trunk@167687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/speech/SpeechInputResult.h
@@ -29,6 +29,7 @@
 #if ENABLE(INPUT_SPEECH)
 
 #include "bindings/v8/ScriptWrappable.h"
+#include "heap/Handle.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
 #include "wtf/text/WTFString.h"
@@ -37,13 +38,16 @@ namespace WebCore {
 
 // This class holds one speech recognition result including the text and other related
 // fields, as received from the embedder.
-class SpeechInputResult : public RefCounted<SpeechInputResult>, public ScriptWrappable {
+class SpeechInputResult : public RefCountedWillBeGarbageCollectedFinalized<SpeechInputResult>, public ScriptWrappable {
+    DECLARE_GC_INFO;
 public:
-    static PassRefPtr<SpeechInputResult> create(const SpeechInputResult& source);
-    static PassRefPtr<SpeechInputResult> create(const String& utterance, double confidence);
+    static PassRefPtrWillBeRawPtr<SpeechInputResult> create(const SpeechInputResult& source);
+    static PassRefPtrWillBeRawPtr<SpeechInputResult> create(const String& utterance, double confidence);
 
     double confidence() const;
     const String& utterance() const;
+
+    void trace(Visitor *) { }
 
 private:
     SpeechInputResult(const String& utterance, double confidence);
@@ -52,7 +56,7 @@ private:
     double m_confidence;
 };
 
-typedef Vector<RefPtr<SpeechInputResult> > SpeechInputResultArray;
+typedef WillBeHeapVector<RefPtrWillBeMember<SpeechInputResult> > SpeechInputResultArray;
 
 } // namespace WebCore
 

Source/core/speech/SpeechInputEvent.h
@@ -30,7 +30,7 @@
 
 #include "core/events/Event.h"
 #include "core/speech/SpeechInputResultList.h"
-
+#include "heap/Handle.h"
 #include "wtf/PassRefPtr.h"
 
 namespace WebCore {
@@ -49,7 +49,7 @@ private:
     SpeechInputEvent();
     SpeechInputEvent(const AtomicString& eventType, const SpeechInputResultArray& results);
 
-    RefPtr<SpeechInputResultList> m_results;
+    RefPtrWillBePersistent<SpeechInputResultList> m_results;
 };
 
 } // namespace WebCore

Source/core/speech/SpeechInputResult.cpp
@@ -30,14 +30,16 @@
 
 namespace WebCore {
 
-PassRefPtr<SpeechInputResult> SpeechInputResult::create(const String& utterance, double confidence)
+DEFINE_GC_INFO(SpeechInputResult);
+
+PassRefPtrWillBeRawPtr<SpeechInputResult> SpeechInputResult::create(const String& utterance, double confidence)
 {
-    return adoptRef(new SpeechInputResult(utterance, confidence));
+    return adoptRefWillBeNoop(new SpeechInputResult(utterance, confidence));
 }
 
-PassRefPtr<SpeechInputResult> SpeechInputResult::create(const SpeechInputResult& source)
+PassRefPtrWillBeRawPtr<SpeechInputResult> SpeechInputResult::create(const SpeechInputResult& source)
 {
-    return adoptRef(new SpeechInputResult(source.m_utterance, source.m_confidence));
+    return adoptRefWillBeNoop(new SpeechInputResult(source.m_utterance, source.m_confidence));
 }
 
 SpeechInputResult::SpeechInputResult(const String& utterance, double confidence)

Source/core/speech/SpeechInputResult.idl
@@ -24,6 +24,7 @@
  */
 
 [
+    WillBeGarbageCollected,
     NoInterfaceObject,
     Conditional=INPUT_SPEECH
 ] interface SpeechInputResult {

Source/core/speech/SpeechInputResultList.cpp
@@ -35,9 +35,11 @@
 
 namespace WebCore {
 
-PassRefPtr<SpeechInputResultList> SpeechInputResultList::create(const SpeechInputResultArray& results)
+DEFINE_GC_INFO(SpeechInputResultList);
+
+PassRefPtrWillBeRawPtr<SpeechInputResultList> SpeechInputResultList::create(const SpeechInputResultArray& results)
 {
-    return adoptRef(new SpeechInputResultList(results));
+    return adoptRefWillBeNoop(new SpeechInputResultList(results));
 }
 
 SpeechInputResult* SpeechInputResultList::item(unsigned index)
@@ -46,9 +48,14 @@ SpeechInputResult* SpeechInputResultList::item(unsigned index)
 }
 
 SpeechInputResultList::SpeechInputResultList(const SpeechInputResultArray& results)
-    : m_results(results) // Takes a copy of the array of RefPtrs.
+    : m_results(results) // Copies the incoming result array.
 {
     ScriptWrappable::init(this);
+}
+
+void SpeechInputResultList::trace(Visitor* visitor)
+{
+    visitor->trace(m_results);
 }
 
 } // namespace WebCore

Source/core/speech/SpeechInputResultList.h
@@ -30,18 +30,22 @@
 
 #include "bindings/v8/ScriptWrappable.h"
 #include "core/speech/SpeechInputResult.h"
+#include "heap/Handle.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
 
 namespace WebCore {
 
-class SpeechInputResultList : public RefCounted<SpeechInputResultList>, public ScriptWrappable {
+class SpeechInputResultList : public RefCountedWillBeGarbageCollectedFinalized<SpeechInputResultList>, public ScriptWrappable {
+    DECLARE_GC_INFO;
 public:
-    static PassRefPtr<SpeechInputResultList> create(const SpeechInputResultArray& results);
+    static PassRefPtrWillBeRawPtr<SpeechInputResultList> create(const SpeechInputResultArray& results);
 
     // Methods from the IDL.
     size_t length() { return m_results.size(); }
     SpeechInputResult* item(unsigned index);
+
+    void trace(Visitor *);
 
 private:
     explicit SpeechInputResultList(const SpeechInputResultArray& results);

Source/core/speech/SpeechInputResultList.idl
@@ -24,6 +24,7 @@
  */
 
 [
+    WillBeGarbageCollected,
     NoInterfaceObject,
     Conditional=INPUT_SPEECH
 ] interface SpeechInputResultList {

Source/web/SpeechInputClientImpl.cpp
@@ -98,7 +98,7 @@ void SpeechInputClientImpl::setRecognitionResult(int requestId, const WebSpeechI
     ASSERT(m_listener);
     WebCore::SpeechInputResultArray webcoreResults(results.size());
     for (size_t i = 0; i < results.size(); ++i)
-        webcoreResults[i] = results[i];
+        webcoreResults[i] = PassRefPtrWillBeRawPtr<WebCore::SpeechInputResult>(results[i]);
     m_listener->setRecognitionResult(requestId, webcoreResults);
 }
 

commit 0f9ceac471578f896360a411e8428f78609afb76

Fix error introduced in r167687 and add copy constructor to PersistentHeapVector.
R=sigbjornf@opera.com, ager@chromium.org, oilpan-reviews@chromium.org

Review URL: https://codereview.chromium.org/181093003

git-svn-id: svn://svn.chromium.org/blink/trunk@167902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/html/shadow/TextControlInnerElements.h
@@ -134,7 +134,7 @@ private:
     bool m_capturing;
     SpeechInputState m_state;
     int m_listenerId;
-    SpeechInputResultArray m_results;
+    WillBePersistentSpeechInputResultArray m_results;
 };
 
 DEFINE_TYPE_CASTS(InputFieldSpeechButtonElement, Element, element, element->isInputFieldSpeechButtonElement(), element.isInputFieldSpeechButtonElement());

Source/core/speech/SpeechInputResult.h
@@ -56,6 +56,7 @@ private:
 };
 
 typedef WillBeHeapVector<RefPtrWillBeMember<SpeechInputResult> > SpeechInputResultArray;
+typedef WillBePersistentHeapVector<RefPtrWillBeMember<SpeechInputResult> > WillBePersistentSpeechInputResultArray;
 
 } // namespace WebCore
 

Source/heap/Handle.h
@@ -314,6 +314,11 @@ class PersistentHeapCollectionBase
     // Never allocate these objects with new. Use Persistent<Collection> instead.
     DISALLOW_ALLOCATION();
 public:
+    PersistentHeapCollectionBase() { }
+
+    template<typename OtherCollection>
+    PersistentHeapCollectionBase(const OtherCollection& other) : Collection(other) { }
+
     void trace(Visitor* visitor) { visitor->trace(*static_cast<Collection*>(this)); }
 };
 
@@ -332,7 +337,16 @@ template<
 class PersistentHeapHashSet : public PersistentHeapCollectionBase<HeapHashSet<ValueArg, HashArg, TraitsArg> > { };
 
 template<typename T, size_t inlineCapacity = 0>
-class PersistentHeapVector : public PersistentHeapCollectionBase<HeapVector<T, inlineCapacity> > { };
+class PersistentHeapVector : public PersistentHeapCollectionBase<HeapVector<T, inlineCapacity> > {
+public:
+    PersistentHeapVector() { }
+
+    template<size_t otherCapacity>
+    PersistentHeapVector(const HeapVector<T, otherCapacity>& other)
+        : PersistentHeapCollectionBase<HeapVector<T, inlineCapacity> >(other)
+    {
+    }
+};
 
 // Members are used in classes to contain strong pointers to other oilpan heap
 // allocated objects.