primiano/blink-gitcs

commit 00a16002951b0ac62d08664315704e5ad22c9249

Make MutableStylePropertySet::removePropertiesInSet faster
MutableStylePropertySet::removePropertiesInSet is used by several methods
removing CSS properties and it is pointed out as a hotspot by Intel VTune
Amplifier profile, when running 'CSSPropertySetterGetterMethods.html'
performance test

This patch makes the method faster by
- Avoiding an unneeded HashSet creation
- not using 'Vector::at' calls in cycle (avoid unneeded RELEASE_ASSERT)
- use 'Vector::swap' instead of assignment operator

Results are the following (linux desktop x64, GCC 4.6.3, release content_shell)

Before:
avg 6954.44302202544 runs/s
median 6966.681499614755 runs/s
stdev 40.17190255456056 runs/s
min 6868.852615417476 runs/s
max 7014.006093452284 runs/s

After:
avg 7431.277303542255 runs/s
median 7442.596071655415 runs/s
stdev 40.61578938819914 runs/s
min 7351.983886907447 runs/s
max 7497.940019131489 runs/s

Average runs count increase is approx 7%.

Review URL: https://codereview.chromium.org/227663002

git-svn-id: svn://svn.chromium.org/blink/trunk@171246 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/css/StylePropertySet.cpp
@@ -443,33 +443,35 @@ void MutableStylePropertySet::removeBlockProperties()
     removePropertiesInSet(blockProperties().data(), blockProperties().size());
 }
 
+inline bool containsId(const CSSPropertyID* set, unsigned length, CSSPropertyID id)
+{
+    for (unsigned i = 0; i < length; ++i) {
+        if (set[i] == id)
+            return true;
+    }
+    return false;
+}
+
 bool MutableStylePropertySet::removePropertiesInSet(const CSSPropertyID* set, unsigned length)
 {
     if (m_propertyVector.isEmpty())
         return false;
 
-    // FIXME: This is always used with static sets and in that case constructing the hash repeatedly is pretty pointless.
-    HashSet<CSSPropertyID> toRemove;
-    for (unsigned i = 0; i < length; ++i)
-        toRemove.add(set[i]);
-
-    WillBeHeapVector<CSSProperty> newProperties;
+    WillBeHeapVector<CSSProperty, 4> newProperties;
     newProperties.reserveInitialCapacity(m_propertyVector.size());
 
-    unsigned size = m_propertyVector.size();
-    for (unsigned n = 0; n < size; ++n) {
-        const CSSProperty& property = m_propertyVector.at(n);
+    unsigned initialSize = m_propertyVector.size();
+    const CSSProperty* properties = m_propertyVector.data();
+    for (unsigned n = 0; n < initialSize; ++n) {
+        const CSSProperty& property = properties[n];
         // Not quite sure if the isImportant test is needed but it matches the existing behavior.
-        if (!property.isImportant()) {
-            if (toRemove.contains(property.id()))
-                continue;
-        }
+        if (!property.isImportant() && containsId(set, length, property.id()))
+            continue;
         newProperties.append(property);
     }
 
-    bool changed = newProperties.size() != m_propertyVector.size();
-    m_propertyVector = newProperties;
-    return changed;
+    m_propertyVector.swap(newProperties);
+    return initialSize != m_propertyVector.size();
 }
 
 CSSProperty* MutableStylePropertySet::findCSSPropertyWithID(CSSPropertyID propertyID)

commit 0589d02ebf72e86ef4844a07b83023ccfdc25c73

Heap-use-after-free in WebCore::MutableStylePropertySet::setProperty
This patch fixes heap-use-after-free error (regression caused by r171246)
in the 'MutableStylePropertySet::setProperty' method. This error turned
up as the 'setProperty' method argument contained a pointer from the vector
buffer which had been previously freed (as we started to use 'Vector::swap'
instead of assignment operator).

BUG=362310

Review URL: https://codereview.chromium.org/234823004

git-svn-id: svn://svn.chromium.org/blink/trunk@171466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/css/StylePropertySet.cpp
@@ -457,7 +457,7 @@ bool MutableStylePropertySet::removePropertiesInSet(const CSSPropertyID* set, un
     if (m_propertyVector.isEmpty())
         return false;
 
-    WillBeHeapVector<CSSProperty, 4> newProperties;
+    WillBeHeapVector<CSSProperty> newProperties;
     newProperties.reserveInitialCapacity(m_propertyVector.size());
 
     unsigned initialSize = m_propertyVector.size();
@@ -470,7 +470,7 @@ bool MutableStylePropertySet::removePropertiesInSet(const CSSPropertyID* set, un
         newProperties.append(property);
     }
 
-    m_propertyVector.swap(newProperties);
+    m_propertyVector = newProperties;
     return initialSize != m_propertyVector.size();
 }
 