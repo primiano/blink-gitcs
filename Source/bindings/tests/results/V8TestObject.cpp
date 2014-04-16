primiano/blink-gitcs

commit 13e911506fb50b4971862fe996c61cc4eab8c9da

select.add() should support optgroup adding and before index
According to the spec (http://www.whatwg.org/specs/web-apps/current-work/multipage/the-button-element.html#the-select-element) the idl for select.add is
void add((HTMLOptionElement or HTMLOptGroupElement) element, optional (HTMLElement or long)? before = null);

select.add should accept an optgroup as the first argument.
select.add should support an index as the second argument.

BUG=345345

Review URL: https://codereview.chromium.org/213773003

git-svn-id: svn://svn.chromium.org/blink/trunk@170259 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/html/HTMLSelectElement.idl
@@ -36,8 +36,9 @@ interface HTMLSelectElement : HTMLElement {
     getter Element item(unsigned long index);
     [RaisesException, StrictTypeChecking] setter HTMLOptionElement (unsigned long index, HTMLOptionElement? value);
     Element namedItem([Default=Undefined] optional DOMString name);
-    [RaisesException] void add([Default=Undefined] optional HTMLElement element,
-                               [Default=Undefined] optional HTMLElement before);
+    [RaisesException] void add(HTMLElement element,
+                               [Default=Undefined] optional HTMLElement? before);
+    [RaisesException, ImplementedAs=addBeforeOptionAtIndex] void add(HTMLElement element, long before);
     void remove(long index);
     [RaisesException] void remove();
     readonly attribute HTMLCollection selectedOptions;

LayoutTests/fast/forms/select/select-add-expected.txt
@@ -1,0 +1,47 @@
+Test select.add() method
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+Test select
+PASS addOption(0) is "X,0,1,2"
+PASS addOption(1) is "0,X,1,2"
+PASS addOption(2) is "0,1,X,2"
+PASS addOption(3) is "0,1,2,X"
+PASS addOption(100) is "0,1,2,X"
+PASS addOption(-100) is "0,1,2,X"
+PASS getSelectOptions() is "0,1,2,X"
+PASS addOption(null) is "0,1,2,X"
+PASS addOption(NaN) is "X,0,1,2"
+PASS addOption(Infinity) is "X,0,1,2"
+PASS addOption(-Infinity) is "X,0,1,2"
+PASS addOption("foo") is "X,0,1,2"
+PASS select.add() threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': 1 argument required, but only 0 present..
+PASS select.add("foo") threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add("foo", 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(null) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(null, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS getSelectOptions() is "0,X,Y,1,2"
+Test select multiple
+PASS addOption(0) is "X,0,1,2"
+PASS addOption(1) is "0,X,1,2"
+PASS addOption(2) is "0,1,X,2"
+PASS addOption(3) is "0,1,2,X"
+PASS addOption(100) is "0,1,2,X"
+PASS addOption(-100) is "0,1,2,X"
+PASS getSelectOptions() is "0,1,2,X"
+PASS addOption(null) is "0,1,2,X"
+PASS addOption(NaN) is "X,0,1,2"
+PASS addOption(Infinity) is "X,0,1,2"
+PASS addOption(-Infinity) is "X,0,1,2"
+PASS addOption("foo") is "X,0,1,2"
+PASS select.add() threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': 1 argument required, but only 0 present..
+PASS select.add("foo") threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add("foo", 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(null) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(null, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS getSelectOptions() is "0,X,Y,1,2"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/select/select-add.html
@@ -1,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+</head>
+<body>
+<script>
+description('Test select.add() method');
+
+debug('Test select');
+test(false);
+
+debug('Test select multiple');
+test(true);
+
+var select;
+
+function resetSelect(multiple) {
+    select = document.createElement('select');
+    select.multiple = multiple;
+    select.appendChild(new Option("0", "0", false, false));
+    select.appendChild(new Option("1", "1", false, false));
+    select.appendChild(new Option("2", "2", false, false));
+}
+
+function addOption(index) {
+    select.add(new Option('X', 'X', false, false), index);
+    return getSelectOptions();
+}
+
+function getSelectOptions() {
+    var values = [];
+    for (var i = 0; i < select.options.length; ++i) {
+        values.push(select.options[i].value);
+    }
+    return values.join(",");
+}
+
+function test(multiple) {
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(0)', 'X,0,1,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(1)', '0,X,1,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(2)', '0,1,X,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(3)', '0,1,2,X');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(100)', '0,1,2,X');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(-100)', '0,1,2,X');
+
+    resetSelect(multiple);
+    select.add(new Option('X', 'X', false, false));
+    shouldBeEqualToString('getSelectOptions()', '0,1,2,X');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(null)', '0,1,2,X');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(NaN)', 'X,0,1,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(Infinity)', 'X,0,1,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption(-Infinity)', 'X,0,1,2');
+
+    resetSelect(multiple);
+    shouldBeEqualToString('addOption("foo")', 'X,0,1,2');
+
+    resetSelect(multiple);
+    shouldThrow('select.add()');
+    shouldThrow('select.add("foo")');
+    shouldThrow('select.add("foo", 0)');
+    shouldThrow('select.add(null)');
+    shouldThrow('select.add(null, 0)');
+
+    resetSelect(multiple);
+    var group = document.createElement('optgroup');
+    group.appendChild(new Option('X', 'X', false, false));
+    group.appendChild(new Option('Y', 'Y', false, false));
+    select.add(group, 1);
+    shouldBeEqualToString('getSelectOptions()', '0,X,Y,1,2');
+}
+</script>
+</body>
+</html>

LayoutTests/fast/dom/incompatible-operations-expected.txt
@@ -12,11 +12,11 @@ PASS aNode.isSameNode(void 0) is false
 PASS aNode.lookupPrefix(aDOMImplementation) is null
 PASS aNode.lookupPrefix(void 0) is null
 PASS aNode.cloneNode(aDOMImplementation) instanceof HTMLDivElement is true
-PASS aSelect.add(aDOMImplementation, aDOMImplementation) is undefined.
-PASS aSelect.add(aDOMImplementation, anOption) is undefined.
+PASS aSelect.add(aDOMImplementation, aDOMImplementation) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS aSelect.add(aDOMImplementation, anOption) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS aSelect.add(void 0, void 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS aSelect.add(void 0, anOption) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS aSelect.add(anOption, aDOMImplementation) is undefined.
-PASS aSelect.add(void 0, void 0) is undefined.
-PASS aSelect.add(void 0, anOption) is undefined.
 PASS aSelect.add(anOption, void 0) is undefined.
 PASS successfullyParsed is true
 

LayoutTests/fast/dom/incompatible-operations.html
@@ -40,11 +40,11 @@ shouldBeFalse("aNode.isSameNode(void 0)");
 shouldBe("aNode.lookupPrefix(aDOMImplementation)", "null");
 shouldBe("aNode.lookupPrefix(void 0)", "null");
 shouldBeTrue("aNode.cloneNode(aDOMImplementation) instanceof HTMLDivElement");
-shouldBeUndefined("aSelect.add(aDOMImplementation, aDOMImplementation)");
-shouldBeUndefined("aSelect.add(aDOMImplementation, anOption)");
+shouldThrow("aSelect.add(aDOMImplementation, aDOMImplementation)");
+shouldThrow("aSelect.add(aDOMImplementation, anOption)");
+shouldThrow("aSelect.add(void 0, void 0)");
+shouldThrow("aSelect.add(void 0, anOption)");
 shouldBeUndefined("aSelect.add(anOption, aDOMImplementation)");
-shouldBeUndefined("aSelect.add(void 0, void 0)");
-shouldBeUndefined("aSelect.add(void 0, anOption)");
 shouldBeUndefined("aSelect.add(anOption, void 0)");
 </script>
 </body>

Source/core/html/HTMLOptionsCollection.cpp
@@ -93,7 +93,7 @@ void HTMLOptionsCollection::add(PassRefPtr<HTMLOptionElement> element, int index
     if (index == -1 || unsigned(index) >= length())
         select.add(newOption, 0, exceptionState);
     else
-        select.add(newOption, toHTMLOptionElement(item(index)), exceptionState);
+        select.addBeforeOptionAtIndex(newOption, index, exceptionState);
 
     ASSERT(!exceptionState.hadException());
 }

Source/core/html/HTMLSelectElement.cpp
@@ -204,11 +204,17 @@ void HTMLSelectElement::add(HTMLElement* element, HTMLElement* before, Exception
     // Make sure the element is ref'd and deref'd so we don't leak it.
     RefPtr<HTMLElement> protectNewChild(element);
 
-    if (!element || !(isHTMLOptionElement(element) || isHTMLHRElement(element)))
+    if (!element || !(isHTMLOptionElement(element) || isHTMLOptGroupElement(element) || isHTMLHRElement(element)))
         return;
 
     insertBefore(element, before, exceptionState);
     setNeedsValidityCheck();
+}
+
+void HTMLSelectElement::addBeforeOptionAtIndex(HTMLElement* element, int beforeIndex, ExceptionState& exceptionState)
+{
+    HTMLElement* beforeElement = toHTMLElement(options()->item(beforeIndex));
+    add(element, beforeElement, exceptionState);
 }
 
 void HTMLSelectElement::remove(int optionIndex)

Source/core/html/HTMLSelectElement.h
@@ -63,6 +63,7 @@ public:
     bool usesMenuList() const;
 
     void add(HTMLElement*, HTMLElement* beforeElement, ExceptionState&);
+    void addBeforeOptionAtIndex(HTMLElement*, int beforeIndex, ExceptionState&);
 
     using Node::remove;
     void remove(int index);

commit 340cb0fde43dc4d622c5ba0156a4e01d3b9bf472

[Regression] Passing undefined to select.add should fallback to the default value
Regression caused by r170259.

Passing an undefined as the second argument of select.add should be handled by falling back to the default value of the optional argument.

TEST=Automated. Added to select-add.html
BUG=363014

Review URL: https://codereview.chromium.org/238613002

git-svn-id: svn://svn.chromium.org/blink/trunk@171747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/bindings/scripts/v8_interface.py
@@ -430,7 +430,10 @@ def overload_check_argument(index, argument):
     if idl_type.is_wrapper_type:
         type_check = 'V8{idl_type}::hasInstance({cpp_value}, info.GetIsolate())'.format(idl_type=idl_type.base_type, cpp_value=cpp_value)
         if idl_type.is_nullable:
-            type_check = ' || '.join(['%s->IsNull()' % cpp_value, type_check])
+            if argument['has_default']:
+                type_check = ' || '.join(['isUndefinedOrNull(%s)' % cpp_value, type_check])
+            else:
+                type_check = ' || '.join(['%s->IsNull()' % cpp_value, type_check])
         return type_check
     if idl_type.is_interface_type:
         # Non-wrapper types are just objects: we don't distinguish type

Source/core/html/HTMLSelectElement.idl
@@ -37,7 +37,7 @@ interface HTMLSelectElement : HTMLElement {
     [RaisesException, StrictTypeChecking] setter HTMLOptionElement (unsigned long index, HTMLOptionElement? value);
     Element namedItem([Default=Undefined] optional DOMString name);
     [RaisesException] void add(HTMLElement element,
-                               [Default=Undefined] optional HTMLElement? before);
+                               [Default=Null] optional HTMLElement? before);
     [RaisesException, ImplementedAs=addBeforeOptionAtIndex] void add(HTMLElement element, long before);
     void remove(long index);
     [RaisesException] void remove();

LayoutTests/fast/forms/select/select-add-expected.txt
@@ -11,6 +11,7 @@ PASS addOption(3) is "0,1,2,X"
 PASS addOption(100) is "0,1,2,X"
 PASS addOption(-100) is "0,1,2,X"
 PASS getSelectOptions() is "0,1,2,X"
+PASS addOption(undefined) is "0,1,2,X"
 PASS addOption(null) is "0,1,2,X"
 PASS addOption(NaN) is "X,0,1,2"
 PASS addOption(Infinity) is "X,0,1,2"
@@ -19,6 +20,8 @@ PASS addOption("foo") is "X,0,1,2"
 PASS select.add() threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': 1 argument required, but only 0 present..
 PASS select.add("foo") threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add("foo", 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(undefined) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(undefined, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add(null) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add(null, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS getSelectOptions() is "0,X,Y,1,2"
@@ -30,6 +33,7 @@ PASS addOption(3) is "0,1,2,X"
 PASS addOption(100) is "0,1,2,X"
 PASS addOption(-100) is "0,1,2,X"
 PASS getSelectOptions() is "0,1,2,X"
+PASS addOption(undefined) is "0,1,2,X"
 PASS addOption(null) is "0,1,2,X"
 PASS addOption(NaN) is "X,0,1,2"
 PASS addOption(Infinity) is "X,0,1,2"
@@ -38,6 +42,8 @@ PASS addOption("foo") is "X,0,1,2"
 PASS select.add() threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': 1 argument required, but only 0 present..
 PASS select.add("foo") threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add("foo", 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(undefined) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
+PASS select.add(undefined, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add(null) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS select.add(null, 0) threw exception TypeError: Failed to execute 'add' on 'HTMLSelectElement': No function was found that matched the signature provided..
 PASS getSelectOptions() is "0,X,Y,1,2"

LayoutTests/fast/forms/select/select-add.html
@@ -60,6 +60,9 @@ function test(multiple) {
     shouldBeEqualToString('getSelectOptions()', '0,1,2,X');
 
     resetSelect(multiple);
+    shouldBeEqualToString('addOption(undefined)', '0,1,2,X');
+
+    resetSelect(multiple);
     shouldBeEqualToString('addOption(null)', '0,1,2,X');
 
     resetSelect(multiple);
@@ -78,6 +81,8 @@ function test(multiple) {
     shouldThrow('select.add()');
     shouldThrow('select.add("foo")');
     shouldThrow('select.add("foo", 0)');
+    shouldThrow('select.add(undefined)');
+    shouldThrow('select.add(undefined, 0)');
     shouldThrow('select.add(null)');
     shouldThrow('select.add(null, 0)');
 

Source/bindings/tests/idls/TestObject.idl
@@ -392,6 +392,8 @@ interface TestObject {
     // overload resolution can't handle this. http://crbug.com/293561
     void overloadedMethodI(Dictionary dictionaryArg); // Non-wrapper type
     void overloadedMethodI(double doubleArg);
+    void overloadedMethodJ(long longArg);
+    void overloadedMethodJ([Default=Null] TestInterfaceEmpty? testInterfaceEmptyNullableArg);
 
 
     [PerWorldBindings] void overloadedPerWorldBindingsMethod();

Source/bindings/tests/results/V8TestObject.cpp
@@ -6765,6 +6765,55 @@ static void overloadedMethodIMethodCallback(const v8::FunctionCallbackInfo<v8::V
     TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
 }
 
+static void overloadedMethodJ1Method(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    ExceptionState exceptionState(ExceptionState::ExecutionContext, "overloadedMethodJ", "TestObject", info.Holder(), info.GetIsolate());
+    if (UNLIKELY(info.Length() < 1)) {
+        throwArityTypeError(exceptionState, 1, info.Length());
+        return;
+    }
+    TestObject* impl = V8TestObject::toNative(info.Holder());
+    TONATIVE_VOID_EXCEPTIONSTATE(int, longArg, toInt32(info[0], exceptionState), exceptionState);
+    impl->overloadedMethodJ(longArg);
+}
+
+static void overloadedMethodJ2Method(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    if (UNLIKELY(info.Length() < 1)) {
+        throwArityTypeErrorForMethod("overloadedMethodJ", "TestObject", 1, info.Length(), info.GetIsolate());
+        return;
+    }
+    TestObject* impl = V8TestObject::toNative(info.Holder());
+    TONATIVE_VOID(TestInterfaceEmpty*, testInterfaceEmptyNullableArg, V8TestInterfaceEmpty::toNativeWithTypeCheck(info.GetIsolate(), info[0]));
+    impl->overloadedMethodJ(testInterfaceEmptyNullableArg);
+}
+
+static void overloadedMethodJMethod(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    if (((info.Length() == 1))) {
+        overloadedMethodJ1Method(info);
+        return;
+    }
+    if (((info.Length() == 1) && (isUndefinedOrNull(info[0]) || V8TestInterfaceEmpty::hasInstance(info[0], info.GetIsolate())))) {
+        overloadedMethodJ2Method(info);
+        return;
+    }
+    ExceptionState exceptionState(ExceptionState::ExecutionContext, "overloadedMethodJ", "TestObject", info.Holder(), info.GetIsolate());
+    if (UNLIKELY(info.Length() < 1)) {
+        throwArityTypeError(exceptionState, 1, info.Length());
+        return;
+    }
+    exceptionState.throwTypeError("No function was found that matched the signature provided.");
+    exceptionState.throwIfNeeded();
+}
+
+static void overloadedMethodJMethodCallback(const v8::FunctionCallbackInfo<v8::Value>& info)
+{
+    TRACE_EVENT_SET_SAMPLING_STATE("Blink", "DOMMethod");
+    TestObjectV8Internal::overloadedMethodJMethod(info);
+    TRACE_EVENT_SET_SAMPLING_STATE("V8", "V8Execution");
+}
+
 static void overloadedPerWorldBindingsMethod1Method(const v8::FunctionCallbackInfo<v8::Value>& info)
 {
     TestObject* impl = V8TestObject::toNative(info.Holder());
@@ -8237,6 +8286,7 @@ static const V8DOMConfiguration::MethodConfiguration V8TestObjectMethods[] = {
     {"overloadedMethodG", TestObjectV8Internal::overloadedMethodGMethodCallback, 0, 0},
     {"overloadedMethodH", TestObjectV8Internal::overloadedMethodHMethodCallback, 0, 0},
     {"overloadedMethodI", TestObjectV8Internal::overloadedMethodIMethodCallback, 0, 1},
+    {"overloadedMethodJ", TestObjectV8Internal::overloadedMethodJMethodCallback, 0, 1},
     {"overloadedPerWorldBindingsMethod", TestObjectV8Internal::overloadedPerWorldBindingsMethodMethodCallback, TestObjectV8Internal::overloadedPerWorldBindingsMethodMethodCallbackForMainWorld, 0},
     {"voidMethodClampUnsignedShortArg", TestObjectV8Internal::voidMethodClampUnsignedShortArgMethodCallback, 0, 1},
     {"voidMethodClampUnsignedLongArg", TestObjectV8Internal::voidMethodClampUnsignedLongArgMethodCallback, 0, 1},