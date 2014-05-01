primiano/blink-gitcs

commit 787fb179e3ba8bb23137bb549805d6465011b86f

Oilpan: Use weak processing to manage clean-up of ScreenOrientationControllers.
R=haraken@chromium.org
BUG=

Review URL: https://codereview.chromium.org/265843002

git-svn-id: svn://svn.chromium.org/blink/trunk@173084 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/modules/screen_orientation/ScreenOrientationDispatcher.cpp
@@ -13,25 +13,37 @@ namespace WebCore {
 
 ScreenOrientationDispatcher& ScreenOrientationDispatcher::instance()
 {
+#if ENABLE(OILPAN)
+    DEFINE_STATIC_LOCAL(Persistent<ScreenOrientationDispatcher>, screenOrientationDispatcher, (new ScreenOrientationDispatcher()));
+    return *screenOrientationDispatcher;
+#else
     DEFINE_STATIC_LOCAL(ScreenOrientationDispatcher, screenOrientationDispatcher, ());
     return screenOrientationDispatcher;
+#endif
 }
 
 ScreenOrientationDispatcher::ScreenOrientationDispatcher()
+#if !ENABLE(OILPAN)
     : m_needsPurge(false)
     , m_isDispatching(false)
+#endif
 {
 }
 
 void ScreenOrientationDispatcher::addController(ScreenOrientationController* controller)
 {
     bool wasEmpty = m_controllers.isEmpty();
+#if ENABLE(OILPAN)
+    m_controllers.add(controller);
+#else
     if (!m_controllers.contains(controller))
         m_controllers.append(controller);
+#endif
     if (wasEmpty)
         startListening();
 }
 
+#if !ENABLE(OILPAN)
 void ScreenOrientationDispatcher::removeController(ScreenOrientationController* controller)
 {
     // Do not actually remove the controllers from the vector, instead zero them out.
@@ -70,9 +82,21 @@ void ScreenOrientationDispatcher::purgeControllers()
     if (m_controllers.isEmpty())
         stopListening();
 }
+#endif
 
 void ScreenOrientationDispatcher::didChangeScreenOrientation(blink::WebScreenOrientationType orientation)
 {
+#if ENABLE(OILPAN)
+    if (m_controllers.isEmpty()) {
+        stopListening();
+        return;
+    }
+    // The on-stack iterator will make m_controllers strong while iterating,
+    // therefore no controllers can be removed during iteration.
+    for (HeapHashSet<WeakMember<ScreenOrientationController> >::iterator it = m_controllers.begin(); it != m_controllers.end(); ++it) {
+        (*it)->didChangeScreenOrientation(orientation);
+    }
+#else
     {
         TemporaryChange<bool> changeIsDispatching(m_isDispatching, true);
         // Don't fire controllers removed or added during event dispatch.
@@ -86,6 +110,7 @@ void ScreenOrientationDispatcher::didChangeScreenOrientation(blink::WebScreenOri
     if (m_needsPurge)
         purgeControllers();
 }
+#endif
 
 void ScreenOrientationDispatcher::startListening()
 {
@@ -96,5 +121,13 @@ void ScreenOrientationDispatcher::stopListening()
 {
     blink::Platform::current()->setScreenOrientationListener(0);
 }
+
+#if ENABLE(OILPAN)
+void ScreenOrientationDispatcher::trace(Visitor* visitor)
+{
+    // Weak processing will remove controllers once they are dead.
+    visitor->trace(m_controllers);
+}
+#endif
 
 } // namespace WebCore

Source/modules/screen_orientation/ScreenOrientationController.cpp
@@ -13,10 +13,13 @@
 
 namespace WebCore {
 
+#if !ENABLE(OILPAN)
 ScreenOrientationController::~ScreenOrientationController()
 {
+    // With oilpan, weak processing removes the controller once it is dead.
     ScreenOrientationDispatcher::instance().removeController(this);
 }
+#endif
 
 ScreenOrientationController& ScreenOrientationController::from(Document& document)
 {

Source/modules/screen_orientation/ScreenOrientationController.h
@@ -10,10 +10,12 @@
 
 namespace WebCore {
 
-class ScreenOrientationController FINAL : public NoBaseWillBeGarbageCollectedFinalized<ScreenOrientationController>, public DocumentSupplement {
+class ScreenOrientationController FINAL : public NoBaseWillBeGarbageCollected<ScreenOrientationController>, public DocumentSupplement {
     WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(ScreenOrientationController);
 public:
+#if !ENABLE(OILPAN)
     virtual ~ScreenOrientationController();
+#endif
 
     void didChangeScreenOrientation(blink::WebScreenOrientationType);
 

Source/modules/screen_orientation/ScreenOrientationDispatcher.h
@@ -5,33 +5,52 @@
 #ifndef ScreenOrientationDispatcher_h
 #define ScreenOrientationDispatcher_h
 
+#include "platform/heap/Handle.h"
 #include "public/platform/WebScreenOrientationListener.h"
+
+#if ENABLE(OILPAN)
+#include "wtf/HashSet.h"
+#else
 #include "wtf/Vector.h"
+#endif
 
 namespace WebCore {
 
 class ScreenOrientationController;
 
-class ScreenOrientationDispatcher FINAL : public blink::WebScreenOrientationListener {
+class ScreenOrientationDispatcher FINAL : public NoBaseWillBeGarbageCollectedFinalized<ScreenOrientationDispatcher>, public blink::WebScreenOrientationListener {
 public:
     static ScreenOrientationDispatcher& instance();
 
     void addController(ScreenOrientationController*);
+
+#if ENABLE(OILPAN)
+    void trace(Visitor*);
+#else
     void removeController(ScreenOrientationController*);
+#endif
 
 private:
     ScreenOrientationDispatcher();
 
+#if !ENABLE(OILPAN)
     void purgeControllers();
+#endif
+
     void startListening();
     void stopListening();
 
     // WebScreenOrientationListener API.
     virtual void didChangeScreenOrientation(blink::WebScreenOrientationType) OVERRIDE;
 
+#if ENABLE(OILPAN)
+    HeapHashSet<WeakMember<ScreenOrientationController> > m_controllers;
+#else
     Vector<ScreenOrientationController*> m_controllers;
     bool m_needsPurge;
     bool m_isDispatching;
+#endif
+
 };
 
 } // namespace WebCore

commit 3ef2c3d3364de190d27fc38b6b009a4785bb1463

Oilpan: speculative build fix for ScreenOrientationDispatcher
Oilpan builds broken by r173084 - just giving this a whirl to green
those bots.

TBR=haraken@chromium.org,zerny@chromium.org

Review URL: https://codereview.chromium.org/266633008

git-svn-id: svn://svn.chromium.org/blink/trunk@173090 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/modules/screen_orientation/ScreenOrientationDispatcher.cpp
@@ -109,8 +109,8 @@ void ScreenOrientationDispatcher::didChangeScreenOrientation(blink::WebScreenOri
 
     if (m_needsPurge)
         purgeControllers();
-}
 #endif
+}
 
 void ScreenOrientationDispatcher::startListening()
 {