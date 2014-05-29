primiano/blink-gitcs

commit a5582924822cd8111b7dcf2d5481557c4bbf2cd8

Viewport for console messages.
Review URL: https://codereview.chromium.org/255013002

git-svn-id: svn://svn.chromium.org/blink/trunk@174810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/console/ConsoleView.js
@@ -28,10 +28,11 @@
  */
 
 /**
+ * @constructor
  * @extends {WebInspector.VBox}
  * @implements {WebInspector.Searchable}
- * @constructor
  * @implements {WebInspector.TargetManager.Observer}
+ * @implements {WebInspector.ViewportControl.Provider}
  * @param {boolean} hideContextSelector
  */
 WebInspector.ConsoleView = function(hideContextSelector)
@@ -78,20 +79,24 @@ WebInspector.ConsoleView = function(hideContextSelector)
     this._filterBar.setName("consoleView");
     this._filter.addFilters(this._filterBar);
 
-    this._messagesElement = this._contentsElement.createChild("div", "monospace");
+    this._viewport = new WebInspector.ViewportControl(this);
+    this._viewport.setStickToBottom(true);
+    this._viewport.contentElement().classList.add("console-group");
+    this._viewport.contentElement().classList.add("console-group-messages");
+    this._contentsElement.appendChild(this._viewport.element);
+    this._messagesElement = this._viewport.element;
     this._messagesElement.id = "console-messages";
     this._messagesElement.addEventListener("click", this._messagesClicked.bind(this), true);
     this._scrolledToBottom = true;
 
-    this._filterStatusMessageElement = this._messagesElement.createChild("div", "console-message");
+    this._filterStatusMessageElement = document.createElementWithClass("div", "console-message");
+    this._messagesElement.insertBefore(this._filterStatusMessageElement, this._messagesElement.firstChild);
     this._filterStatusTextElement = this._filterStatusMessageElement.createChild("span", "console-info");
     this._filterStatusMessageElement.createTextChild(" ");
     var resetFiltersLink = this._filterStatusMessageElement.createChild("span", "console-info node-link");
     resetFiltersLink.textContent = WebInspector.UIString("Show all messages.");
     resetFiltersLink.addEventListener("click", this._filter.reset.bind(this._filter), true);
 
-    this._messagesContainer = this._messagesElement.createChild("div", "console-group");
-    this._messagesContainer.classList.add("console-group-messages");
     this._topGroup = WebInspector.ConsoleGroup.createTopGroup();
     this._currentGroup = this._topGroup;
 
@@ -138,6 +143,23 @@ WebInspector.ConsoleView = function(hideContextSelector)
 
 WebInspector.ConsoleView.prototype = {
     /**
+     * @return {number}
+     */
+    itemCount: function()
+    {
+        return this._visibleViewMessages.length;
+    },
+
+    /**
+     * @param {number} index
+     * @return {?WebInspector.ViewportElement}
+     */
+    itemElement: function(index)
+    {
+        return this._visibleViewMessages[index];
+    },
+
+    /**
      * @param {!WebInspector.Target} target
      */
     targetAdded: function(target)
@@ -156,6 +178,7 @@ WebInspector.ConsoleView.prototype = {
         target.consoleModel.addEventListener(WebInspector.ConsoleModel.Events.ConsoleCleared, this._consoleCleared, this);
         target.consoleModel.addEventListener(WebInspector.ConsoleModel.Events.CommandEvaluated, this._commandEvaluated, this);
         target.consoleModel.messages.forEach(appendMessage, this);
+        this._viewport.invalidate();
 
         target.runtimeModel.executionContexts().forEach(this._executionContextCreated, this);
         target.runtimeModel.addEventListener(WebInspector.RuntimeModel.Events.ExecutionContextCreated, this._onExecutionContextCreated, this);
@@ -288,6 +311,7 @@ WebInspector.ConsoleView.prototype = {
 
     wasShown: function()
     {
+        this._viewport.refresh();
         if (!this._prompt.isCaretInsidePrompt())
             this._prompt.moveCaretToEndOfPrompt();
     },
@@ -325,20 +349,19 @@ WebInspector.ConsoleView.prototype = {
         return !!this._scrollIntoViewTimer;
     },
 
-    _scheduleScrollIntoView: function()
+    _scheduleViewportRefresh: function()
     {
         if (this._scrollIntoViewTimer)
             return;
-
         /**
          * @this {WebInspector.ConsoleView}
          */
         function scrollIntoView()
         {
             delete this._scrollIntoViewTimer;
-            this._messagesElement.scrollTop = this._messagesElement.scrollHeight;
+            this._viewport.invalidate();
         }
-        this._scrollIntoViewTimer = setTimeout(scrollIntoView.bind(this), 20);
+        this._scrollIntoViewTimer = setTimeout(scrollIntoView.bind(this), 50);
     },
 
     _immediatelyScrollIntoView: function()
@@ -351,8 +374,8 @@ WebInspector.ConsoleView.prototype = {
     {
         if (!this._isScrollIntoViewScheduled())
             return;
-
         clearTimeout(this._scrollIntoViewTimer);
+        this._viewport.refresh();
         delete this._scrollIntoViewTimer;
     },
 
@@ -404,6 +427,7 @@ WebInspector.ConsoleView.prototype = {
         var message = /** @type {!WebInspector.ConsoleMessage} */ (event.data);
         var viewMessage = this._createViewMessage(message);
         this._consoleMessageAdded(viewMessage);
+        this._scheduleViewportRefresh();
     },
 
     /**
@@ -419,17 +443,10 @@ WebInspector.ConsoleView.prototype = {
             return;
         }
         if (!this._currentGroup.messagesHidden()) {
-            // this._messagesElement.isScrolledToBottom() is forcing style recalculation.
-            // We just skip it if the scroll action has been scheduled.
-            if (!this._isScrollIntoViewScheduled() && ((viewMessage instanceof WebInspector.ConsoleCommandResult) || this._messagesElement.isScrolledToBottom()))
-                this._scheduleScrollIntoView();
-
             var originatingMessage = viewMessage.consoleMessage().originatingMessage();
             if (lastMessage && originatingMessage && lastMessage.consoleMessage() === originatingMessage)
                 lastMessage.toMessageElement().classList.add("console-adjacent-user-command-result");
 
-            this._messagesContainer.appendChild(viewMessage.toMessageElement());
-            viewMessage.wasShown();
             this._visibleViewMessages.push(viewMessage);
 
             if (this._searchRegex && viewMessage.matchesRegex(this._searchRegex)) {
@@ -551,13 +568,9 @@ WebInspector.ConsoleView.prototype = {
         this._searchResults = [];
         this._hiddenByFilterCount = 0;
         for (var i = 0; i < this._visibleViewMessages.length; ++i) {
-            var visibleMessage = this._visibleViewMessages[i];
-            visibleMessage.willHide();
-            visibleMessage.toMessageElement().remove();
-            visibleMessage.resetCloseGroupDecorationCount();
-            visibleMessage.resetIncrementRepeatCount();
+            this._visibleViewMessages[i].resetCloseGroupDecorationCount();
+            this._visibleViewMessages[i].resetIncrementRepeatCount();
         }
-
         this._visibleViewMessages = [];
         for (var i = 0; i < this._consoleMessages.length; ++i) {
             var viewMessage = this._consoleMessages[i];
@@ -569,6 +582,7 @@ WebInspector.ConsoleView.prototype = {
                 this._hiddenByFilterCount++;
         }
         this._updateFilterStatus();
+        this._viewport.invalidate();
     },
 
     /**
@@ -592,10 +606,7 @@ WebInspector.ConsoleView.prototype = {
             return;
         var consoleGroupViewMessage = groupMessage.parentElement.message;
         consoleGroupViewMessage.setCollapsed(!consoleGroupViewMessage.collapsed());
-        var originalScrollPosition = this._messagesElement.scrollTop;
         this._updateMessageList();
-        this._cancelScheduledScrollIntoView();
-        this._messagesElement.scrollTop = originalScrollPosition;
     },
 
     _registerShortcuts: function()
@@ -964,14 +975,6 @@ WebInspector.ConsoleCommand = function(message, nestingLevel)
 }
 
 WebInspector.ConsoleCommand.prototype = {
-    wasShown: function()
-    {
-    },
-
-    willHide: function()
-    {
-    },
-
     clearHighlight: function()
     {
         var highlightedMessage = this._formattedCommand;

Source/devtools/front_end/sources/FilteredItemSelectionDialog.js
@@ -54,6 +54,7 @@ WebInspector.FilteredItemSelectionDialog = function(delegate)
 
     this._filteredItems = [];
     this._viewportControl = new WebInspector.ViewportControl(this);
+    this._viewportControl.element.classList.add("fill");
     this._itemElementsContainer = this._viewportControl.element;
     this._itemElementsContainer.classList.add("container");
     this._itemElementsContainer.classList.add("monospace");
@@ -244,7 +245,7 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
                     break;
                 }
             }
-            this._viewportControl.refresh();
+            this._viewportControl.invalidate();
             if (!query)
                 this._selectedIndexInFiltered = 0;
             this._updateSelection(this._selectedIndexInFiltered, false);
@@ -272,6 +273,14 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
         this.element.style.height = shouldShowMatchingItems ? this._dialogHeight + "px" : "auto";
     },
 
+    /**
+     * @return {number}
+     */
+    _rowsPerViewport: function()
+    {
+        return Math.floor(this._viewportControl.element.clientHeight / this._rowHeight);
+    },
+
     _onKeyDown: function(event)
     {
         var newSelectedIndex = this._selectedIndexInFiltered;
@@ -290,12 +299,12 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
             event.consume(true);
             break;
         case WebInspector.KeyboardShortcut.Keys.PageDown.code:
-            newSelectedIndex = Math.min(newSelectedIndex + this._viewportControl.rowsPerViewport(), this._filteredItems.length - 1);
+            newSelectedIndex = Math.min(newSelectedIndex + this._rowsPerViewport(), this._filteredItems.length - 1);
             this._updateSelection(newSelectedIndex, true);
             event.consume(true);
             break;
         case WebInspector.KeyboardShortcut.Keys.PageUp.code:
-            newSelectedIndex = Math.max(newSelectedIndex - this._viewportControl.rowsPerViewport(), 0);
+            newSelectedIndex = Math.max(newSelectedIndex - this._rowsPerViewport(), 0);
             this._updateSelection(newSelectedIndex, false);
             event.consume(true);
             break;
@@ -345,15 +354,17 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
 
     /**
      * @param {number} index
-     * @return {!Element}
+     * @return {!WebInspector.ViewportElement}
      */
     itemElement: function(index)
     {
         var delegateIndex = this._filteredItems[index];
         var element = this._createItemElement(delegateIndex);
+        if (!this._rowHeight)
+            this._rowHeight = element.measurePreferredSize(this._viewportControl.contentElement()).height;
         if (index === this._selectedIndexInFiltered)
             element.classList.add("selected");
-        return element;
+        return new WebInspector.StaticViewportElement(element, this._rowHeight);
     },
 
     __proto__: WebInspector.DialogDelegate.prototype

commit 171379bfdde08639ceec47f8eee0107a9045c11b

DevTools: Fix regression that slows down "Goto Source" dialog
The regression was caused by r174810. The root cause is the
implementation of WebInspector.StaticViewportElement which doesn't
support lazy DOM element creation. So every time the viewport processed
cumulative heights of inner elements, it actually forced all DOM elements
for viewport to be created.

To keep things simple, the patch moves fastHeight calls out of
ViewportElement interface into ViewportProvider.

BUG=377749
R=loislo@chromium.org

Review URL: https://codereview.chromium.org/305813005

git-svn-id: svn://svn.chromium.org/blink/trunk@175059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/console/ConsoleView.js
@@ -160,6 +160,15 @@ WebInspector.ConsoleView.prototype = {
     },
 
     /**
+     * @param {number} index
+     * @return {number}
+     */
+    fastHeight: function(index)
+    {
+        return this._visibleViewMessages[index].fastHeight();
+    },
+
+    /**
      * @param {!WebInspector.Target} target
      */
     targetAdded: function(target)

Source/devtools/front_end/sources/FilteredItemSelectionDialog.js
@@ -354,17 +354,29 @@ WebInspector.FilteredItemSelectionDialog.prototype = {
 
     /**
      * @param {number} index
+     * @return {number}
+     */
+    fastHeight: function(index)
+    {
+        if (!this._rowHeight) {
+            var delegateIndex = this._filteredItems[index];
+            var element = this._createItemElement(delegateIndex);
+            this._rowHeight = element.measurePreferredSize(this._viewportControl.contentElement()).height;
+        }
+        return this._rowHeight;
+    },
+
+    /**
+     * @param {number} index
      * @return {!WebInspector.ViewportElement}
      */
     itemElement: function(index)
     {
         var delegateIndex = this._filteredItems[index];
         var element = this._createItemElement(delegateIndex);
-        if (!this._rowHeight)
-            this._rowHeight = element.measurePreferredSize(this._viewportControl.contentElement()).height;
         if (index === this._selectedIndexInFiltered)
             element.classList.add("selected");
-        return new WebInspector.StaticViewportElement(element, this._rowHeight);
+        return new WebInspector.StaticViewportElement(element);
     },
 
     __proto__: WebInspector.DialogDelegate.prototype

Source/devtools/front_end/ui/ViewportControl.js
@@ -66,6 +66,12 @@ WebInspector.ViewportControl.Provider = function()
 
 WebInspector.ViewportControl.Provider.prototype = {
     /**
+     * @param {number} index
+     * @return {number}
+     */
+    fastHeight: function(index) { return 0; },
+
+    /**
      * @return {number}
      */
     itemCount: function() { return 0; },
@@ -87,11 +93,6 @@ WebInspector.ViewportElement.prototype = {
     wasShown: function() { },
 
     /**
-     * @return {number}
-     */
-    fastHeight: function() { },
-
-    /**
      * @return {!Element}
      */
     element: function() { },
@@ -101,26 +102,16 @@ WebInspector.ViewportElement.prototype = {
  * @constructor
  * @implements {WebInspector.ViewportElement}
  * @param {!Element} element
- * @param {number} height
  */
-WebInspector.StaticViewportElement = function(element, height)
+WebInspector.StaticViewportElement = function(element)
 {
     this._element = element;
-    this._height = height;
 }
 
 WebInspector.StaticViewportElement.prototype = {
     willHide: function() { },
 
     wasShown: function() { },
-
-    /**
-     * @return {number}
-     */
-    fastHeight: function()
-    {
-        return this._height;
-    },
 
     /**
      * @return {!Element}
@@ -188,9 +179,9 @@ WebInspector.ViewportControl.prototype = {
         if (!itemCount)
             return;
         this._cumulativeHeights = new Int32Array(itemCount);
-        this._cumulativeHeights[0] = this._provider.itemElement(0).fastHeight();
+        this._cumulativeHeights[0] = this._provider.fastHeight(0);
         for (var i = 1; i < itemCount; ++i)
-            this._cumulativeHeights[i] = this._cumulativeHeights[i - 1] + this._provider.itemElement(i).fastHeight();
+            this._cumulativeHeights[i] = this._cumulativeHeights[i - 1] + this._provider.fastHeight(i);
     },
 
     /**
@@ -361,7 +352,7 @@ WebInspector.ViewportControl.prototype = {
         for (var i = 0; i < this._renderedItems.length; ++i) {
             this._renderedItems[i].willHide();
             // Tolerate 1-pixel error due to double-to-integer rounding errors.
-            if (this._cumulativeHeights && Math.abs(this._cachedItemHeight(this._firstVisibleIndex + i) - this._renderedItems[i].fastHeight()) > 1)
+            if (this._cumulativeHeights && Math.abs(this._cachedItemHeight(this._firstVisibleIndex + i) - this._provider.fastHeight(i + this._firstVisibleIndex)) > 1)
                 delete this._cumulativeHeights;
         }
         this._rebuildCumulativeHeightsIfNeeded();