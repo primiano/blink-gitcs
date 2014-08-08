primiano/blink-gitcs

commit 305a978d05c5d7d898e1d03d8382af355112474a

Introduced new class ConsoleMessage, that wraps all old arguments.
R=vsevik@chromium.org,aandrey@chromium.org

Review URL: https://codereview.chromium.org/376213002

git-svn-id: svn://svn.chromium.org/blink/trunk@179850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Document.cpp
@@ -150,6 +150,7 @@
 #include "core/html/parser/HTMLParserIdioms.h"
 #include "core/html/parser/NestingLevelIncrementer.h"
 #include "core/html/parser/TextResourceDecoder.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/InspectorCounters.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/inspector/InspectorTraceEvents.h"
@@ -5024,7 +5025,10 @@ void Document::internalAddMessage(MessageSource source, MessageLevel level, cons
                 lineNumber = parser->lineNumber().oneBasedInt();
         }
     }
-    m_frame->console().addMessage(source, level, message, messageURL, lineNumber, 0, callStack, scriptState, 0);
+    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message, messageURL, lineNumber);
+    consoleMessage->setCallStack(callStack);
+    consoleMessage->setScriptState(scriptState);
+    m_frame->console().addMessage(consoleMessage.release());
 }
 
 void Document::addConsoleMessageWithRequestIdentifier(MessageSource source, MessageLevel level, const String& message, unsigned long requestIdentifier)
@@ -5034,8 +5038,11 @@ void Document::addConsoleMessageWithRequestIdentifier(MessageSource source, Mess
         return;
     }
 
-    if (m_frame)
-        m_frame->console().addMessage(source, level, message, String(), 0, 0, nullptr, 0, requestIdentifier);
+    if (m_frame) {
+        RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message);
+        consoleMessage->setRequestIdentifier(requestIdentifier);
+        m_frame->console().addMessage(consoleMessage.release());
+    }
 }
 
 // FIXME(crbug.com/305497): This should be removed after ExecutionContext-LocalDOMWindow migration.

Source/core/frame/FrameConsole.cpp
@@ -31,7 +31,9 @@
 
 #include "core/frame/FrameHost.h"
 #include "core/inspector/ConsoleAPITypes.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/InspectorConsoleInstrumentation.h"
+#include "core/inspector/ScriptCallStack.h"
 #include "core/page/Chrome.h"
 #include "core/page/ChromeClient.h"
 #include "core/page/Page.h"
@@ -50,17 +52,7 @@ FrameConsole::FrameConsole(LocalFrame& frame)
 {
 }
 
-void FrameConsole::addMessage(MessageSource source, MessageLevel level, const String& message)
-{
-    addMessage(source, level, message, String(), 0, 0, nullptr, 0, 0);
-}
-
-void FrameConsole::addMessage(MessageSource source, MessageLevel level, const String& message, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack)
-{
-    addMessage(source, level, message, String(), 0, 0, callStack, 0);
-}
-
-void FrameConsole::addMessage(MessageSource source, MessageLevel level, const String& message, const String& url, unsigned lineNumber, unsigned columnNumber, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack, ScriptState* scriptState, unsigned long requestIdentifier)
+void FrameConsole::addMessage(PassRefPtr<ConsoleMessage> prpConsoleMessage)
 {
     if (muteCount)
         return;
@@ -71,23 +63,23 @@ void FrameConsole::addMessage(MessageSource source, MessageLevel level, const St
     if (!context)
         return;
 
-    String messageURL;
-    if (callStack) {
-        messageURL = callStack->at(0).sourceURL();
-        InspectorInstrumentation::addMessageToConsole(context, source, LogMessageType, level, message, callStack, requestIdentifier);
-    } else {
-        messageURL = url;
-        InspectorInstrumentation::addMessageToConsole(context, source, LogMessageType, level, message, url, lineNumber, columnNumber, scriptState, requestIdentifier);
-    }
+    RefPtr<ConsoleMessage> consoleMessage = prpConsoleMessage;
+    InspectorInstrumentation::addMessageToConsole(context, consoleMessage.get());
 
-    if (source == CSSMessageSource)
+    String messageURL;
+    if (consoleMessage->callStack())
+        messageURL = consoleMessage->callStack()->at(0).sourceURL();
+    else
+        messageURL = consoleMessage->url();
+
+    if (consoleMessage->source() == CSSMessageSource)
         return;
 
     String stackTrace;
-    if (callStack && m_frame.chromeClient().shouldReportDetailedMessageForSource(messageURL))
-        stackTrace = FrameConsole::formatStackTraceString(message, callStack);
+    if (consoleMessage->callStack() && m_frame.chromeClient().shouldReportDetailedMessageForSource(consoleMessage->url()))
+        stackTrace = FrameConsole::formatStackTraceString(consoleMessage->message(), consoleMessage->callStack());
 
-    m_frame.chromeClient().addMessageToConsole(&m_frame, source, level, message, lineNumber, messageURL, stackTrace);
+    m_frame.chromeClient().addMessageToConsole(&m_frame, consoleMessage->source(), consoleMessage->level(), consoleMessage->message(), consoleMessage->lineNumber(), messageURL, stackTrace);
 }
 
 String FrameConsole::formatStackTraceString(const String& originalMessage, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack)

Source/core/frame/FrameConsole.h
@@ -31,13 +31,14 @@
 
 #include "bindings/core/v8/ScriptState.h"
 #include "core/frame/ConsoleTypes.h"
-#include "core/inspector/ScriptCallStack.h"
 #include "wtf/Forward.h"
 #include "wtf/PassOwnPtr.h"
 
 namespace blink {
 
+class ConsoleMessage;
 class FrameHost;
+class ScriptCallStack;
 
 // FrameConsole takes per-frame console messages and routes them up through the FrameHost to the ChromeClient and Inspector.
 // It's meant as an abstraction around ChromeClient calls and the way that Blink core/ can add messages to the console.
@@ -45,9 +46,7 @@ class FrameConsole FINAL {
 public:
     static PassOwnPtr<FrameConsole> create(LocalFrame& frame) { return adoptPtr(new FrameConsole(frame)); }
 
-    void addMessage(MessageSource, MessageLevel, const String& message);
-    void addMessage(MessageSource, MessageLevel, const String& message, const String& sourceURL, unsigned lineNumber, unsigned columnNumber = 0, PassRefPtrWillBeRawPtr<ScriptCallStack> = nullptr, ScriptState* = 0, unsigned long requestIdentifier = 0);
-    void addMessage(MessageSource, MessageLevel, const String& message, PassRefPtrWillBeRawPtr<ScriptCallStack>);
+    void addMessage(PassRefPtr<ConsoleMessage>);
     static String formatStackTraceString(const String& originalMessage, PassRefPtrWillBeRawPtr<ScriptCallStack>);
 
     static void mute();

Source/core/frame/LocalDOMWindow.cpp
@@ -71,6 +71,7 @@
 #include "core/frame/Screen.h"
 #include "core/frame/Settings.h"
 #include "core/html/HTMLFrameOwnerElement.h"
+#include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/inspector/InspectorTraceEvents.h"
 #include "core/inspector/ScriptCallStack.h"
@@ -904,7 +905,9 @@ void LocalDOMWindow::dispatchMessageEventWithOriginCheck(SecurityOrigin* intende
         // Check target origin now since the target document may have changed since the timer was scheduled.
         if (!intendedTargetOrigin->isSameSchemeHostPort(document()->securityOrigin())) {
             String message = ExceptionMessages::failedToExecute("postMessage", "DOMWindow", "The target origin provided ('" + intendedTargetOrigin->toString() + "') does not match the recipient window's origin ('" + document()->securityOrigin()->toString() + "').");
-            frameConsole()->addMessage(SecurityMessageSource, ErrorMessageLevel, message, stackTrace);
+            RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, message);
+            consoleMessage->setCallStack(stackTrace);
+            frameConsole()->addMessage(consoleMessage.release());
             return;
         }
     }
@@ -984,7 +987,7 @@ void LocalDOMWindow::close(ExecutionContext* context)
     bool allowScriptsToCloseWindows = settings && settings->allowScriptsToCloseWindows();
 
     if (!(page->openedByDOM() || page->backForward().backForwardListCount() <= 1 || allowScriptsToCloseWindows)) {
-        frameConsole()->addMessage(JSMessageSource, WarningMessageLevel, "Scripts may close only the windows that were opened by it.");
+        frameConsole()->addMessage(ConsoleMessage::create(JSMessageSource, WarningMessageLevel, "Scripts may close only the windows that were opened by it."));
         return;
     }
 
@@ -1700,7 +1703,7 @@ void LocalDOMWindow::printErrorMessage(const String& message)
     if (message.isEmpty())
         return;
 
-    frameConsole()->addMessage(JSMessageSource, ErrorMessageLevel, message);
+    frameConsole()->addMessage(ConsoleMessage::create(JSMessageSource, ErrorMessageLevel, message));
 }
 
 // FIXME: Once we're throwing exceptions for cross-origin access violations, we will always sanitize the target

Source/core/inspector/ConsoleMessage.cpp
@@ -1,0 +1,107 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/inspector/ConsoleMessage.h"
+
+#include "bindings/core/v8/ScriptState.h"
+
+namespace blink {
+
+ConsoleMessage::ConsoleMessage()
+    : m_lineNumber(0)
+    , m_columnNumber(0)
+    , m_scriptState(nullptr)
+    , m_requestIdentifier(0)
+{
+}
+
+ConsoleMessage::ConsoleMessage(MessageSource source,
+    MessageLevel level,
+    const String& message,
+    const String& url,
+    unsigned lineNumber,
+    unsigned columnNumber)
+    : m_source(source)
+    , m_level(level)
+    , m_message(message)
+    , m_url(url)
+    , m_lineNumber(lineNumber)
+    , m_columnNumber(columnNumber)
+    , m_scriptState(nullptr)
+    , m_requestIdentifier(0)
+{
+}
+
+PassRefPtr<ScriptCallStack> ConsoleMessage::callStack() const
+{
+    return m_callStack;
+}
+
+void ConsoleMessage::setCallStack(PassRefPtr<ScriptCallStack> callStack)
+{
+    m_callStack = callStack;
+}
+
+ScriptState* ConsoleMessage::scriptState() const
+{
+    return m_scriptState;
+}
+
+void ConsoleMessage::setScriptState(ScriptState* scriptState)
+{
+    m_scriptState = scriptState;
+}
+
+unsigned long ConsoleMessage::requestIdentifier() const
+{
+    return m_requestIdentifier;
+}
+
+void ConsoleMessage::setRequestIdentifier(unsigned long requestIdentifier)
+{
+    m_requestIdentifier = requestIdentifier;
+}
+
+const String& ConsoleMessage::url() const
+{
+    return m_url;
+}
+
+void ConsoleMessage::setURL(const String& url)
+{
+    m_url = url;
+}
+
+unsigned ConsoleMessage::lineNumber() const
+{
+    return m_lineNumber;
+}
+
+void ConsoleMessage::setLineNumber(unsigned lineNumber)
+{
+    m_lineNumber = lineNumber;
+}
+
+MessageSource ConsoleMessage::source() const
+{
+    return m_source;
+}
+
+MessageLevel ConsoleMessage::level() const
+{
+    return m_level;
+}
+
+const String& ConsoleMessage::message() const
+{
+    return m_message;
+}
+
+unsigned ConsoleMessage::columnNumber() const
+{
+    return m_columnNumber;
+}
+
+} // namespace WebCore

Source/core/inspector/ConsoleMessage.h
@@ -1,0 +1,61 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ConsoleMessage_h
+#define ConsoleMessage_h
+
+#include "bindings/core/v8/ScriptState.h"
+#include "core/frame/ConsoleTypes.h"
+#include "core/inspector/ScriptCallStack.h"
+#include "wtf/Forward.h"
+#include "wtf/PassRefPtr.h"
+#include "wtf/RefCounted.h"
+#include "wtf/text/WTFString.h"
+
+namespace blink {
+
+class ScriptCallStack;
+class ScriptState;
+
+class ConsoleMessage FINAL: public RefCounted<ConsoleMessage> {
+public:
+    static PassRefPtr<ConsoleMessage> create(MessageSource source, MessageLevel level, const String& message, const String& url = String(), unsigned lineNumber = 0, unsigned columnNumber = 0)
+    {
+        return adoptRef(new ConsoleMessage(source, level, message, url, lineNumber, columnNumber));
+    }
+
+    PassRefPtr<ScriptCallStack> callStack() const;
+    void setCallStack(PassRefPtr<ScriptCallStack>);
+    ScriptState* scriptState() const;
+    void setScriptState(ScriptState*);
+    unsigned long requestIdentifier() const;
+    void setRequestIdentifier(unsigned long);
+    const String& url() const;
+    void setURL(const String&);
+    unsigned lineNumber() const;
+    void setLineNumber(unsigned);
+
+    MessageSource source() const;
+    MessageLevel level() const;
+    const String& message() const;
+    unsigned columnNumber() const;
+
+private:
+    ConsoleMessage();
+    ConsoleMessage(MessageSource, MessageLevel, const String& message, const String& url = String(), unsigned lineNumber = 0, unsigned columnNumber = 0);
+
+    MessageSource m_source;
+    MessageLevel m_level;
+    String m_message;
+    String m_url;
+    unsigned m_lineNumber;
+    unsigned m_columnNumber;
+    RefPtr<ScriptCallStack> m_callStack;
+    ScriptState* m_scriptState;
+    unsigned long m_requestIdentifier;
+};
+
+} // namespace WebCore
+
+#endif // ConsoleMessage_h

commit 28a58b46987461431614d7320ddb5e03354f90f1

Oilpan: Move ConsoleMessage to the heap.
And fix compilation breakage from r179850.

TBR=haraken,oilpan-reviews
BUG=340522
NOTRY=true

Review URL: https://codereview.chromium.org/451153002

git-svn-id: svn://svn.chromium.org/blink/trunk@179873 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/core/dom/Document.cpp
@@ -5025,7 +5025,7 @@ void Document::internalAddMessage(MessageSource source, MessageLevel level, cons
                 lineNumber = parser->lineNumber().oneBasedInt();
         }
     }
-    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message, messageURL, lineNumber);
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message, messageURL, lineNumber);
     consoleMessage->setCallStack(callStack);
     consoleMessage->setScriptState(scriptState);
     m_frame->console().addMessage(consoleMessage.release());
@@ -5039,7 +5039,7 @@ void Document::addConsoleMessageWithRequestIdentifier(MessageSource source, Mess
     }
 
     if (m_frame) {
-        RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message);
+        RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message);
         consoleMessage->setRequestIdentifier(requestIdentifier);
         m_frame->console().addMessage(consoleMessage.release());
     }

Source/core/frame/FrameConsole.cpp
@@ -52,7 +52,7 @@ FrameConsole::FrameConsole(LocalFrame& frame)
 {
 }
 
-void FrameConsole::addMessage(PassRefPtr<ConsoleMessage> prpConsoleMessage)
+void FrameConsole::addMessage(PassRefPtrWillBeRawPtr<ConsoleMessage> prpConsoleMessage)
 {
     if (muteCount)
         return;
@@ -63,7 +63,7 @@ void FrameConsole::addMessage(PassRefPtr<ConsoleMessage> prpConsoleMessage)
     if (!context)
         return;
 
-    RefPtr<ConsoleMessage> consoleMessage = prpConsoleMessage;
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = prpConsoleMessage;
     InspectorInstrumentation::addMessageToConsole(context, consoleMessage.get());
 
     String messageURL;

Source/core/frame/FrameConsole.h
@@ -31,6 +31,7 @@
 
 #include "bindings/core/v8/ScriptState.h"
 #include "core/frame/ConsoleTypes.h"
+#include "platform/heap/Handle.h"
 #include "wtf/Forward.h"
 #include "wtf/PassOwnPtr.h"
 
@@ -46,7 +47,7 @@ class FrameConsole FINAL {
 public:
     static PassOwnPtr<FrameConsole> create(LocalFrame& frame) { return adoptPtr(new FrameConsole(frame)); }
 
-    void addMessage(PassRefPtr<ConsoleMessage>);
+    void addMessage(PassRefPtrWillBeRawPtr<ConsoleMessage>);
     static String formatStackTraceString(const String& originalMessage, PassRefPtrWillBeRawPtr<ScriptCallStack>);
 
     static void mute();

Source/core/frame/LocalDOMWindow.cpp
@@ -905,7 +905,7 @@ void LocalDOMWindow::dispatchMessageEventWithOriginCheck(SecurityOrigin* intende
         // Check target origin now since the target document may have changed since the timer was scheduled.
         if (!intendedTargetOrigin->isSameSchemeHostPort(document()->securityOrigin())) {
             String message = ExceptionMessages::failedToExecute("postMessage", "DOMWindow", "The target origin provided ('" + intendedTargetOrigin->toString() + "') does not match the recipient window's origin ('" + document()->securityOrigin()->toString() + "').");
-            RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, message);
+            RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, message);
             consoleMessage->setCallStack(stackTrace);
             frameConsole()->addMessage(consoleMessage.release());
             return;

Source/core/inspector/ConsoleMessage.cpp
@@ -34,12 +34,16 @@ ConsoleMessage::ConsoleMessage(MessageSource source,
 {
 }
 
-PassRefPtr<ScriptCallStack> ConsoleMessage::callStack() const
+ConsoleMessage::~ConsoleMessage()
+{
+}
+
+PassRefPtrWillBeRawPtr<ScriptCallStack> ConsoleMessage::callStack() const
 {
     return m_callStack;
 }
 
-void ConsoleMessage::setCallStack(PassRefPtr<ScriptCallStack> callStack)
+void ConsoleMessage::setCallStack(PassRefPtrWillBeRawPtr<ScriptCallStack> callStack)
 {
     m_callStack = callStack;
 }
@@ -102,6 +106,11 @@ const String& ConsoleMessage::message() const
 unsigned ConsoleMessage::columnNumber() const
 {
     return m_columnNumber;
+}
+
+void ConsoleMessage::trace(Visitor* visitor)
+{
+    visitor->trace(m_callStack);
 }
 
 } // namespace WebCore

Source/core/inspector/ConsoleMessage.h
@@ -8,6 +8,7 @@
 #include "bindings/core/v8/ScriptState.h"
 #include "core/frame/ConsoleTypes.h"
 #include "core/inspector/ScriptCallStack.h"
+#include "platform/heap/Handle.h"
 #include "wtf/Forward.h"
 #include "wtf/PassRefPtr.h"
 #include "wtf/RefCounted.h"
@@ -18,15 +19,17 @@ namespace blink {
 class ScriptCallStack;
 class ScriptState;
 
-class ConsoleMessage FINAL: public RefCounted<ConsoleMessage> {
+class ConsoleMessage FINAL: public RefCountedWillBeGarbageCollectedFinalized<ConsoleMessage> {
 public:
-    static PassRefPtr<ConsoleMessage> create(MessageSource source, MessageLevel level, const String& message, const String& url = String(), unsigned lineNumber = 0, unsigned columnNumber = 0)
+    static PassRefPtrWillBeRawPtr<ConsoleMessage> create(MessageSource source, MessageLevel level, const String& message, const String& url = String(), unsigned lineNumber = 0, unsigned columnNumber = 0)
     {
-        return adoptRef(new ConsoleMessage(source, level, message, url, lineNumber, columnNumber));
+        return adoptRefWillBeNoop(new ConsoleMessage(source, level, message, url, lineNumber, columnNumber));
     }
 
-    PassRefPtr<ScriptCallStack> callStack() const;
-    void setCallStack(PassRefPtr<ScriptCallStack>);
+    ~ConsoleMessage();
+
+    PassRefPtrWillBeRawPtr<ScriptCallStack> callStack() const;
+    void setCallStack(PassRefPtrWillBeRawPtr<ScriptCallStack>);
     ScriptState* scriptState() const;
     void setScriptState(ScriptState*);
     unsigned long requestIdentifier() const;
@@ -41,6 +44,8 @@ public:
     const String& message() const;
     unsigned columnNumber() const;
 
+    void trace(Visitor*);
+
 private:
     ConsoleMessage();
     ConsoleMessage(MessageSource, MessageLevel, const String& message, const String& url = String(), unsigned lineNumber = 0, unsigned columnNumber = 0);
@@ -51,7 +56,7 @@ private:
     String m_url;
     unsigned m_lineNumber;
     unsigned m_columnNumber;
-    RefPtr<ScriptCallStack> m_callStack;
+    RefPtrWillBeMember<ScriptCallStack> m_callStack;
     ScriptState* m_scriptState;
     unsigned long m_requestIdentifier;
 };

Source/core/inspector/InspectorConsoleAgent.cpp
@@ -280,7 +280,7 @@ void InspectorConsoleAgent::didFinishXHRLoading(XMLHttpRequest*, ThreadableLoade
 {
     if (m_frontend && m_state->getBoolean(ConsoleAgentState::monitoringXHR)) {
         String message = "XHR finished loading: " + method + " \"" + url + "\".";
-        RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, DebugMessageLevel, message, sendURL, sendLineNumber);
+        RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, DebugMessageLevel, message, sendURL, sendLineNumber);
         consoleMessage->setRequestIdentifier(requestIdentifier);
         addMessageToConsole(consoleMessage.get());
     }
@@ -292,7 +292,7 @@ void InspectorConsoleAgent::didReceiveResourceResponse(LocalFrame*, unsigned lon
         return;
     if (response.httpStatusCode() >= 400) {
         String message = "Failed to load resource: the server responded with a status of " + String::number(response.httpStatusCode()) + " (" + response.httpStatusText() + ')';
-        RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, ErrorMessageLevel, message, response.url().string());
+        RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, ErrorMessageLevel, message, response.url().string());
         consoleMessage->setRequestIdentifier(requestIdentifier);
         addMessageToConsole(consoleMessage.get());
     }
@@ -308,7 +308,7 @@ void InspectorConsoleAgent::didFailLoading(unsigned long requestIdentifier, cons
         message.appendLiteral(": ");
         message.append(error.localizedDescription());
     }
-    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, ErrorMessageLevel, message.toString(), error.failingURL());
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(NetworkMessageSource, ErrorMessageLevel, message.toString(), error.failingURL());
     consoleMessage->setRequestIdentifier(requestIdentifier);
     addMessageToConsole(consoleMessage.get());
 }
@@ -350,4 +350,3 @@ void InspectorConsoleAgent::addInspectedHeapObject(ErrorString*, int inspectedHe
 }
 
 } // namespace blink
-

Source/core/inspector/InspectorTimelineAgent.cpp
@@ -825,7 +825,7 @@ void InspectorTimelineAgent::consoleTimeline(ExecutionContext* context, const St
 
     String message = String::format("Timeline '%s' started.", title.utf8().data());
 
-    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
     consoleMessage->setScriptState(scriptState);
     mainFrame()->console().addMessage(consoleMessage.release());
     m_consoleTimelines.append(title);
@@ -848,7 +848,7 @@ void InspectorTimelineAgent::consoleTimelineEnd(ExecutionContext* context, const
     size_t index = m_consoleTimelines.find(title);
     if (index == kNotFound) {
         String message = String::format("Timeline '%s' was not started.", title.utf8().data());
-        RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
+        RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
         consoleMessage->setScriptState(scriptState);
         mainFrame()->console().addMessage(consoleMessage.release());
         return;
@@ -861,7 +861,7 @@ void InspectorTimelineAgent::consoleTimelineEnd(ExecutionContext* context, const
         unwindRecordStack();
         innerStop(true);
     }
-    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(JSMessageSource, DebugMessageLevel, message);
     consoleMessage->setScriptState(scriptState);
     mainFrame()->console().addMessage(consoleMessage.release());
 }
@@ -1377,4 +1377,3 @@ void TimelineThreadState::trace(Visitor* visitor)
 }
 
 } // namespace blink
-

Source/core/workers/WorkerGlobalScope.cpp
@@ -305,7 +305,7 @@ void WorkerGlobalScope::addMessage(MessageSource source, MessageLevel level, con
 void WorkerGlobalScope::addMessageToWorkerConsole(MessageSource source, MessageLevel level, const String& message, const String& sourceURL, unsigned lineNumber, PassRefPtrWillBeRawPtr<ScriptCallStack> callStack, ScriptState* scriptState)
 {
     ASSERT(isContextThread());
-    RefPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message, sourceURL, lineNumber);
+    RefPtrWillBeRawPtr<ConsoleMessage> consoleMessage = ConsoleMessage::create(source, level, message, sourceURL, lineNumber);
     consoleMessage->setCallStack(callStack);
     consoleMessage->setScriptState(scriptState);
     InspectorInstrumentation::addMessageToConsole(this, consoleMessage.get());