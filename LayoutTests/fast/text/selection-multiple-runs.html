primiano/blink-gitcs

commit 2b3551321d7fdcd0ef105429bbc69e8b99bb01e1

Separate advance from offset in GlypBuffer
Change GlyphBuffer to store advances and offsets independently. This
greatly simplifies shaping and drawing of complex text. As advances are
now a single float value per glyph it is made clearer that the simple
text path only handles horizontal advances (as opposed to vertical and
horizontal offsets in addition to horizontal advances).

Also gets rid of HarfBuzzShaper::adjustStartPoint as it is no longer
needed now that offsets and advances are separate.

Must land after 175253002.

TEST=Source/platform/fonts/GlyphBufferTest.cpp
BUG=418071
R=behdad@chromium.org,dominik.rottsches@intel.com

Review URL: https://codereview.chromium.org/607483002

git-svn-id: svn://svn.chromium.org/blink/trunk@182991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
@@ -278,7 +278,7 @@ inline void HarfBuzzShaper::HarfBuzzRun::setGlyphAndPositions(unsigned index, ui
 {
     m_glyphs[index] = glyphId;
     m_advances[index] = advance;
-    m_offsets[index] = FloatPoint(offsetX, offsetY);
+    m_offsets[index] = FloatSize(offsetX, offsetY);
 }
 
 int HarfBuzzShaper::HarfBuzzRun::characterIndexForXPosition(float targetX)
@@ -572,11 +572,6 @@ bool HarfBuzzShaper::shape(GlyphBuffer* glyphBuffer)
         return false;
 
     return true;
-}
-
-FloatPoint HarfBuzzShaper::adjustStartPoint(const FloatPoint& point)
-{
-    return point + m_startOffset;
 }
 
 static inline int handleMultipleUChar(
@@ -953,29 +948,29 @@ void HarfBuzzShaper::setGlyphPositionsForHarfBuzzRun(HarfBuzzRun* currentRun, hb
     m_totalWidth += currentRun->width();
 }
 
-void HarfBuzzShaper::fillGlyphBufferFromHarfBuzzRun(GlyphBuffer* glyphBuffer, HarfBuzzRun* currentRun, FloatPoint& firstOffsetOfNextRun)
+void HarfBuzzShaper::fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets* glyphBuffer, HarfBuzzRun* currentRun)
 {
-    FloatPoint* offsets = currentRun->offsets();
+    FloatSize* offsets = currentRun->offsets();
     uint16_t* glyphs = currentRun->glyphs();
     float* advances = currentRun->advances();
     unsigned numGlyphs = currentRun->numGlyphs();
     uint16_t* glyphToCharacterIndexes = currentRun->glyphToCharacterIndexes();
-    for (unsigned i = 0; i < numGlyphs; ++i) {
-        uint16_t currentCharacterIndex = currentRun->startIndex() + glyphToCharacterIndexes[i];
-        FloatPoint& currentOffset = offsets[i];
-        FloatPoint& nextOffset = (i == numGlyphs - 1) ? firstOffsetOfNextRun : offsets[i + 1];
-        float glyphAdvanceX = advances[i] + nextOffset.x() - currentOffset.x();
-        float glyphAdvanceY = nextOffset.y() - currentOffset.y();
-        if (m_run.rtl()) {
+    float advanceSoFar = 0;
+    if (m_run.rtl()) {
+        for (unsigned i = 0; i < numGlyphs; ++i) {
+            uint16_t currentCharacterIndex = currentRun->startIndex() + glyphToCharacterIndexes[i];
             if (currentCharacterIndex >= m_toIndex)
-                m_startOffset.move(glyphAdvanceX, glyphAdvanceY);
+                advanceSoFar += advances[i];
             else if (currentCharacterIndex >= m_fromIndex)
-                glyphBuffer->add(glyphs[i], currentRun->fontData(), FloatSize(glyphAdvanceX, glyphAdvanceY));
-        } else {
+                glyphBuffer->add(glyphs[i], currentRun->fontData(), offsets[i] + FloatSize(advanceSoFar, 0), advances[i]);
+        }
+    } else {
+        for (unsigned i = 0; i < numGlyphs; ++i) {
+            uint16_t currentCharacterIndex = currentRun->startIndex() + glyphToCharacterIndexes[i];
             if (currentCharacterIndex < m_fromIndex)
-                m_startOffset.move(glyphAdvanceX, glyphAdvanceY);
+                advanceSoFar += advances[i];
             else if (currentCharacterIndex < m_toIndex)
-                glyphBuffer->add(glyphs[i], currentRun->fontData(), FloatSize(glyphAdvanceX, glyphAdvanceY));
+                glyphBuffer->add(glyphs[i], currentRun->fontData(), offsets[i] + FloatSize(advanceSoFar, 0), advances[i]);
         }
     }
 }
@@ -1038,32 +1033,36 @@ bool HarfBuzzShaper::fillGlyphBuffer(GlyphBuffer* glyphBuffer)
 {
     unsigned numRuns = m_harfBuzzRuns.size();
     if (m_run.rtl()) {
-        m_startOffset = m_harfBuzzRuns.last()->offsets()[0];
         for (int runIndex = numRuns - 1; runIndex >= 0; --runIndex) {
             HarfBuzzRun* currentRun = m_harfBuzzRuns[runIndex].get();
             if (!currentRun->hasGlyphToCharacterIndexes()) {
                 // FIXME: bug 337886, 359664
                 continue;
             }
-            FloatPoint firstOffsetOfNextRun = !runIndex ? FloatPoint() : m_harfBuzzRuns[runIndex - 1]->offsets()[0];
-            if (m_forTextEmphasis == ForTextEmphasis)
+            if (m_forTextEmphasis == ForTextEmphasis) {
+                ASSERT(!glyphBuffer->hasOffsets());
                 fillGlyphBufferForTextEmphasis(glyphBuffer, currentRun);
-            else
-                fillGlyphBufferFromHarfBuzzRun(glyphBuffer, currentRun, firstOffsetOfNextRun);
+            } else {
+                ASSERT(glyphBuffer->hasOffsets());
+                fillGlyphBufferFromHarfBuzzRun(
+                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun);
+            }
         }
     } else {
-        m_startOffset = m_harfBuzzRuns.first()->offsets()[0];
         for (unsigned runIndex = 0; runIndex < numRuns; ++runIndex) {
             HarfBuzzRun* currentRun = m_harfBuzzRuns[runIndex].get();
             if (!currentRun->hasGlyphToCharacterIndexes()) {
                 // FIXME: bug 337886, 359664
                 continue;
             }
-            FloatPoint firstOffsetOfNextRun = runIndex == numRuns - 1 ? FloatPoint() : m_harfBuzzRuns[runIndex + 1]->offsets()[0];
-            if (m_forTextEmphasis == ForTextEmphasis)
+            if (m_forTextEmphasis == ForTextEmphasis) {
+                ASSERT(!glyphBuffer->hasOffsets());
                 fillGlyphBufferForTextEmphasis(glyphBuffer, currentRun);
-            else
-                fillGlyphBufferFromHarfBuzzRun(glyphBuffer, currentRun, firstOffsetOfNextRun);
+            } else {
+                ASSERT(glyphBuffer->hasOffsets());
+                fillGlyphBufferFromHarfBuzzRun(
+                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun);
+            }
         }
     }
     return glyphBuffer->size();

Source/platform/fonts/harfbuzz/HarfBuzzShaper.h
@@ -46,7 +46,7 @@
 namespace blink {
 
 class Font;
-class GlyphBuffer;
+class GlyphBufferWithOffsets;
 class SimpleFontData;
 
 class HarfBuzzShaper FINAL {
@@ -60,7 +60,6 @@ public:
 
     void setDrawRange(int from, int to);
     bool shape(GlyphBuffer* = 0);
-    FloatPoint adjustStartPoint(const FloatPoint&);
     float totalWidth() { return m_totalWidth; }
     int offsetForPosition(float targetX);
     FloatRect selectionRect(const FloatPoint&, int height, int from, int to);
@@ -90,7 +89,7 @@ private:
         unsigned numGlyphs() const { return m_numGlyphs; }
         uint16_t* glyphs() { return &m_glyphs[0]; }
         float* advances() { return &m_advances[0]; }
-        FloatPoint* offsets() { return &m_offsets[0]; }
+        FloatSize* offsets() { return &m_offsets[0]; }
         bool hasGlyphToCharacterIndexes() const
         {
             return m_glyphToCharacterIndexes.size() > 0;
@@ -116,7 +115,7 @@ private:
         Vector<uint16_t, 256> m_glyphs;
         Vector<float, 256> m_advances;
         Vector<uint16_t, 256> m_glyphToCharacterIndexes;
-        Vector<FloatPoint, 256> m_offsets;
+        Vector<FloatSize, 256> m_offsets;
         float m_width;
     };
 
@@ -130,7 +129,7 @@ private:
     bool createHarfBuzzRuns();
     bool shapeHarfBuzzRuns();
     bool fillGlyphBuffer(GlyphBuffer*);
-    void fillGlyphBufferFromHarfBuzzRun(GlyphBuffer*, HarfBuzzRun*, FloatPoint& firstOffsetOfNextRun);
+    void fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets*, HarfBuzzRun*);
     void fillGlyphBufferForTextEmphasis(GlyphBuffer*, HarfBuzzRun* currentRun);
     void setGlyphPositionsForHarfBuzzRun(HarfBuzzRun*, hb_buffer_t*);
     void addHarfBuzzRun(unsigned startCharacter, unsigned endCharacter, const SimpleFontData*, UScriptCode);
@@ -148,8 +147,6 @@ private:
 
     Vector<hb_feature_t, 4> m_features;
     Vector<OwnPtr<HarfBuzzRun>, 16> m_harfBuzzRuns;
-
-    FloatPoint m_startOffset;
 
     int m_fromIndex;
     int m_toIndex;

LayoutTests/TestExpectations
@@ -2768,6 +2768,9 @@ crbug.com/375565 [ Mac ] fast/repaint/caret-with-transformation.html [ Pass Fail
 
 crbug.com/375567 web-animations-api/w3c/simple-keyframes.html [ Pass Failure ]
 
+crbug.com/418071 fast/text/justify-ideograph-vertical.html [ NeedsRebaseline ]
+crbug.com/418071 virtual/antialiasedtext/fast/text/international/draw-complex-text-from-to.html [ NeedsRebaseline ]
+
 crbug.com/376597 web-animations-api/player.html [ Pass Failure ]
 
 crbug.com/400841 media/video-canvas-draw.html [ ImageOnlyFailure ]

Source/core/rendering/svg/SVGTextRunRenderingContext.cpp
@@ -113,7 +113,7 @@ void SVGTextRunRenderingContext::drawSVGGlyphs(GraphicsContext* context, const T
         if (!glyph)
             continue;
 
-        float advance = glyphBuffer.advanceAt(from + i).width();
+        float advance = glyphBuffer.advanceAt(from + i);
         SVGGlyph svgGlyph = fontElement->svgGlyphForGlyph(glyph);
         ASSERT(!svgGlyph.isPartOfLigature);
         ASSERT(svgGlyph.tableEntry == glyph);

Source/platform/fonts/Font.cpp
@@ -227,7 +227,6 @@ PassTextBlobPtr Font::buildTextBlobForSimpleText(const TextRunPaintInfo& runInfo
 {
     GlyphBuffer glyphBuffer;
     float initialAdvance = getGlyphsAndAdvancesForSimpleText(runInfo, glyphBuffer);
-    ASSERT(!glyphBuffer.hasVerticalAdvances());
 
     if (glyphBuffer.isEmpty())
         return nullptr;
@@ -694,7 +693,6 @@ float Font::drawSimpleText(GraphicsContext* context, const TextRunPaintInfo& run
     // This glyph buffer holds our glyphs+advances+font data for each glyph.
     GlyphBuffer glyphBuffer;
     float initialAdvance = getGlyphsAndAdvancesForSimpleText(runInfo, glyphBuffer);
-    ASSERT(!glyphBuffer.hasVerticalAdvances());
 
     if (glyphBuffer.isEmpty())
         return 0;
@@ -732,15 +730,13 @@ float Font::drawGlyphBuffer(GraphicsContext* context, const TextRunPaintInfo& ru
     // Draw each contiguous run of glyphs that use the same font data.
     const SimpleFontData* fontData = glyphBuffer.fontDataAt(0);
     FloatPoint startPoint(point);
-    FloatPoint nextPoint = startPoint + glyphBuffer.advanceAt(0);
+    float advanceSoFar = 0;
     unsigned lastFrom = 0;
-    unsigned nextGlyph = 1;
+    unsigned nextGlyph = 0;
 #if ENABLE(SVG_FONTS)
     TextRun::RenderingContext* renderingContext = runInfo.run.renderingContext();
 #endif
 
-    float widthSoFar = 0;
-    widthSoFar += glyphBuffer.advanceAt(0).width();
     while (nextGlyph < glyphBuffer.size()) {
         const SimpleFontData* nextFontData = glyphBuffer.fontDataAt(nextGlyph);
 
@@ -754,10 +750,10 @@ float Font::drawGlyphBuffer(GraphicsContext* context, const TextRunPaintInfo& ru
 
             lastFrom = nextGlyph;
             fontData = nextFontData;
-            startPoint = nextPoint;
+            startPoint += FloatSize(advanceSoFar, 0);
+            advanceSoFar = 0;
         }
-        nextPoint += glyphBuffer.advanceAt(nextGlyph);
-        widthSoFar += glyphBuffer.advanceAt(nextGlyph).width();
+        advanceSoFar += glyphBuffer.advanceAt(nextGlyph);
         nextGlyph++;
     }
 
@@ -767,7 +763,9 @@ float Font::drawGlyphBuffer(GraphicsContext* context, const TextRunPaintInfo& ru
     else
 #endif
         drawGlyphs(context, fontData, glyphBuffer, lastFrom, nextGlyph - lastFrom, startPoint, runInfo.bounds);
-        return widthSoFar;
+
+    startPoint += FloatSize(advanceSoFar, 0);
+    return startPoint.x() - point.x();
 }
 
 inline static float offsetToMiddleOfGlyph(const SimpleFontData* fontData, Glyph glyph)
@@ -782,7 +780,7 @@ inline static float offsetToMiddleOfGlyph(const SimpleFontData* fontData, Glyph 
 
 inline static float offsetToMiddleOfAdvanceAtIndex(const GlyphBuffer& glyphBuffer, size_t i)
 {
-    return glyphBuffer.advanceAt(i).width() / 2;
+    return glyphBuffer.advanceAt(i) / 2;
 }
 
 void Font::drawEmphasisMarks(GraphicsContext* context, const TextRunPaintInfo& runInfo, const GlyphBuffer& glyphBuffer, const AtomicString& mark, const FloatPoint& point) const
@@ -807,7 +805,7 @@ void Font::drawEmphasisMarks(GraphicsContext* context, const TextRunPaintInfo& r
     GlyphBuffer markBuffer;
     for (unsigned i = 0; i + 1 < glyphBuffer.size(); ++i) {
         float middleOfNextGlyph = offsetToMiddleOfAdvanceAtIndex(glyphBuffer, i + 1);
-        float advance = glyphBuffer.advanceAt(i).width() - middleOfLastGlyph + middleOfNextGlyph;
+        float advance = glyphBuffer.advanceAt(i) - middleOfLastGlyph + middleOfNextGlyph;
         markBuffer.add(glyphBuffer.glyphAt(i) ? markGlyph : spaceGlyph, markFontData, advance);
         middleOfLastGlyph = middleOfNextGlyph;
     }

Source/platform/fonts/GlyphBuffer.h
@@ -42,23 +42,19 @@ class SimpleFontData;
 class GlyphBuffer {
     STACK_ALLOCATED();
 public:
-    GlyphBuffer() : m_hasVerticalAdvances(false) { }
-
     bool isEmpty() const { return m_fontData.isEmpty(); }
+    virtual bool hasOffsets() const { return false; }
     unsigned size() const { return m_fontData.size(); }
-    bool hasVerticalAdvances() const { return m_hasVerticalAdvances; }
 
-    void clear()
+    virtual void clear()
     {
         m_fontData.clear();
         m_glyphs.clear();
         m_advances.clear();
-        m_hasVerticalAdvances = false;
     }
 
     const Glyph* glyphs(unsigned from) const { return m_glyphs.data() + from; }
-    const FloatSize* advances(unsigned from) const { return m_advances.data() + from; }
-
+    const float* advances(unsigned from) const { return m_advances.data() + from; }
     const SimpleFontData* fontDataAt(unsigned index) const { return m_fontData[index]; }
 
     Glyph glyphAt(unsigned index) const
@@ -66,7 +62,7 @@ public:
         return m_glyphs[index];
     }
 
-    FloatSize advanceAt(unsigned index) const
+    float advanceAt(unsigned index) const
     {
         return m_advances[index];
     }
@@ -75,42 +71,68 @@ public:
     {
         m_fontData.append(font);
         m_glyphs.append(glyph);
-        m_advances.append(FloatSize(width, 0));
+        m_advances.append(width);
     }
 
-    void add(Glyph glyph, const SimpleFontData* font, const FloatSize& advance)
-    {
-        m_fontData.append(font);
-        m_glyphs.append(glyph);
-        m_advances.append(advance);
-        if (advance.height())
-            m_hasVerticalAdvances = true;
-    }
-
-    void reverse()
+    virtual void reverse()
     {
         m_fontData.reverse();
         m_glyphs.reverse();
         m_advances.reverse();
     }
 
-    void setAdvanceWidth(unsigned index, float newWidth)
-    {
-        m_advances[index].setWidth(newWidth);
-    }
-
     void expandLastAdvance(float width)
     {
         ASSERT(!isEmpty());
-        FloatSize& lastAdvance = m_advances.last();
-        lastAdvance.setWidth(lastAdvance.width() + width);
+        float& lastAdvance = m_advances.last();
+        lastAdvance += width;
+    }
+
+protected:
+    Vector<const SimpleFontData*, 2048> m_fontData;
+    Vector<Glyph, 2048> m_glyphs;
+    Vector<float, 2048> m_advances;
+};
+
+
+class GlyphBufferWithOffsets : public GlyphBuffer {
+public:
+    virtual bool hasOffsets() const OVERRIDE { return true; }
+
+    virtual void clear() OVERRIDE
+    {
+        GlyphBuffer::clear();
+        m_offsets.clear();
+    }
+
+    const FloatSize* offsets(unsigned from) const { return m_offsets.data() + from; }
+
+    FloatSize offsetAt(unsigned index) const
+    {
+        return m_offsets[index];
+    }
+
+    void add(Glyph glyph, const SimpleFontData* font, const FloatSize& offset, float advance)
+    {
+        m_fontData.append(font);
+        m_glyphs.append(glyph);
+        m_offsets.append(offset);
+        m_advances.append(advance);
+    }
+
+    virtual void reverse() OVERRIDE
+    {
+        GlyphBuffer::reverse();
+        m_offsets.reverse();
     }
 
 private:
-    Vector<const SimpleFontData*, 2048> m_fontData;
-    Vector<Glyph, 2048> m_glyphs;
-    Vector<FloatSize, 2048> m_advances;
-    bool m_hasVerticalAdvances;
+    void add(Glyph glyph, const SimpleFontData* font, float width)
+    {
+        ASSERT_NOT_REACHED();
+    }
+
+    Vector<FloatSize, 1024> m_offsets;
 };
 
 } // namespace blink

commit 180f642772b884b9d956c971f7e9ec2485e73882

Carry advances between runs in HarfBuzzShaper.cpp
When the setDrawRange method is used to restrict painting
to a range, the initial advance has to be carried over to
the first glyph that's being added to the glyph buffer,
otherwise things like selection painting will fail.

Fixes a regression after refactoring GlyphBuffer in r182991.

BUG=420614
R=eae,behdad,leviw

Review URL: https://codereview.chromium.org/627273003

git-svn-id: svn://svn.chromium.org/blink/trunk@183347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/platform/fonts/harfbuzz/HarfBuzzShaper.cpp
@@ -949,29 +949,35 @@ void HarfBuzzShaper::setGlyphPositionsForHarfBuzzRun(HarfBuzzRun* currentRun, hb
     m_totalWidth += currentRun->width();
 }
 
-void HarfBuzzShaper::fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets* glyphBuffer, HarfBuzzRun* currentRun)
+void HarfBuzzShaper::fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets* glyphBuffer, HarfBuzzRun* currentRun, float& carryAdvance)
 {
     FloatSize* offsets = currentRun->offsets();
     uint16_t* glyphs = currentRun->glyphs();
     float* advances = currentRun->advances();
     unsigned numGlyphs = currentRun->numGlyphs();
     uint16_t* glyphToCharacterIndexes = currentRun->glyphToCharacterIndexes();
-    float advanceSoFar = 0;
+    FloatSize runStartOffset = FloatSize();
     if (m_run.rtl()) {
         for (unsigned i = 0; i < numGlyphs; ++i) {
             uint16_t currentCharacterIndex = currentRun->startIndex() + glyphToCharacterIndexes[i];
-            if (currentCharacterIndex >= m_toIndex)
-                advanceSoFar += advances[i];
-            else if (currentCharacterIndex >= m_fromIndex)
-                glyphBuffer->add(glyphs[i], currentRun->fontData(), offsets[i] + FloatSize(advanceSoFar, 0), advances[i]);
+            if (currentCharacterIndex >= m_toIndex) {
+                carryAdvance += advances[i];
+            } else if (currentCharacterIndex >= m_fromIndex) {
+                runStartOffset = HB_DIRECTION_IS_HORIZONTAL(currentRun->direction()) ? FloatSize(carryAdvance, 0) : FloatSize(0, carryAdvance);
+                glyphBuffer->add(glyphs[i], currentRun->fontData(), runStartOffset + offsets[i], carryAdvance + advances[i]);
+                carryAdvance = 0;
+            }
         }
     } else {
         for (unsigned i = 0; i < numGlyphs; ++i) {
             uint16_t currentCharacterIndex = currentRun->startIndex() + glyphToCharacterIndexes[i];
-            if (currentCharacterIndex < m_fromIndex)
-                advanceSoFar += advances[i];
-            else if (currentCharacterIndex < m_toIndex)
-                glyphBuffer->add(glyphs[i], currentRun->fontData(), offsets[i] + FloatSize(advanceSoFar, 0), advances[i]);
+            if (currentCharacterIndex < m_fromIndex) {
+                carryAdvance += advances[i];
+            } else if (currentCharacterIndex < m_toIndex) {
+                runStartOffset = HB_DIRECTION_IS_HORIZONTAL(currentRun->direction()) ? FloatSize(carryAdvance, 0) : FloatSize(0, carryAdvance);
+                glyphBuffer->add(glyphs[i], currentRun->fontData(), runStartOffset + offsets[i], carryAdvance + advances[i]);
+                carryAdvance = 0;
+            }
         }
     }
 }
@@ -1033,6 +1039,7 @@ void HarfBuzzShaper::fillGlyphBufferForTextEmphasis(GlyphBuffer* glyphBuffer, Ha
 bool HarfBuzzShaper::fillGlyphBuffer(GlyphBuffer* glyphBuffer)
 {
     unsigned numRuns = m_harfBuzzRuns.size();
+    float carryAdvance = 0;
     if (m_run.rtl()) {
         for (int runIndex = numRuns - 1; runIndex >= 0; --runIndex) {
             HarfBuzzRun* currentRun = m_harfBuzzRuns[runIndex].get();
@@ -1046,7 +1053,7 @@ bool HarfBuzzShaper::fillGlyphBuffer(GlyphBuffer* glyphBuffer)
             } else {
                 ASSERT(glyphBuffer->hasOffsets());
                 fillGlyphBufferFromHarfBuzzRun(
-                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun);
+                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun, carryAdvance);
             }
         }
     } else {
@@ -1062,7 +1069,7 @@ bool HarfBuzzShaper::fillGlyphBuffer(GlyphBuffer* glyphBuffer)
             } else {
                 ASSERT(glyphBuffer->hasOffsets());
                 fillGlyphBufferFromHarfBuzzRun(
-                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun);
+                    static_cast<GlyphBufferWithOffsets*>(glyphBuffer), currentRun, carryAdvance);
             }
         }
     }

Source/platform/fonts/harfbuzz/HarfBuzzShaper.h
@@ -129,7 +129,7 @@ private:
     bool createHarfBuzzRuns();
     bool shapeHarfBuzzRuns();
     bool fillGlyphBuffer(GlyphBuffer*);
-    void fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets*, HarfBuzzRun*);
+    void fillGlyphBufferFromHarfBuzzRun(GlyphBufferWithOffsets*, HarfBuzzRun*, float& carryAdvance);
     void fillGlyphBufferForTextEmphasis(GlyphBuffer*, HarfBuzzRun* currentRun);
     void setGlyphPositionsForHarfBuzzRun(HarfBuzzRun*, hb_buffer_t*);
     void addHarfBuzzRun(unsigned startCharacter, unsigned endCharacter, const SimpleFontData*, UScriptCode);

LayoutTests/TestExpectations
@@ -1414,6 +1414,10 @@ crbug.com/418091 [ SnowLeopard Debug ] virtual/antialiasedtext/fast/text/soft-hy
 crbug.com/418091 [ SnowLeopard Debug ] virtual/antialiasedtext/fast/text/soft-hyphen-4.html [ Pass Failure ]
 crbug.com/418091 [ SnowLeopard Debug ] virtual/antialiasedtext/fast/text/splitText-dirty-lines.html [ Pass Failure ]
 
+crbug.com/420614 fast/text/selection-multiple-runs.html [ NeedsRebaseline ]
+crbug.com/420614 [ Mac Win ] virtual/antialiasedtext/fast/text/selection-multiple-runs.html [ NeedsRebaseline ]
+crbug.com/420614 [ Mac ] virtual/antialiasedtext/fast/text/international/draw-complex-text-from-to.html [ NeedsRebaseline ]
+
 crbug.com/334269 [ SnowLeopard ] fast/text/whitespace/008.html [ NeedsManualRebaseline ]
 crbug.com/334269 [ SnowLeopard ] fast/text/whitespace/009.html [ NeedsManualRebaseline ]
 crbug.com/334269 [ SnowLeopard ] fast/text/whitespace/012.html [ NeedsManualRebaseline ]

LayoutTests/fast/text/selection-multiple-runs.html
@@ -1,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Test selection across multiple shaper runs</title>
+        <style>
+            body { font-size: 30px; }
+            ::selection { color: red; background: rgba(120,120,120,120); }
+        </style>
+    </head>
+    <body>
+        <p>The selection should be painted correctly from and including 猫 until and including C, then from right to left for العر. </p>
+        <div>吾輩は猫で मानक हिन्दीABCالعربية</div>
+        <div id="multiruns">吾輩は猫で मानक हिन्दीABCالعربية</div>
+        <script>
+            var node = document.getElementById('multiruns').firstChild;
+            var range = document.createRange();
+            range.setStart(node, 3);
+            range.setEnd(node, node.length - 3);
+            window.getSelection().addRange(range);
+        </script>
+    </body>
+</html>