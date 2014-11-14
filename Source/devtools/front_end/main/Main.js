primiano/blink-gitcs

commit 29ad8dad13ae12e48928f4f5a52e376fee3f64b6

[DevTools] Extract part of DevTools into inspector app.
This inspector app is intended to be run inside an iframe.
InspectorAppHost exposes an API between inspector app and hosting app.

Created DevTools app which is only embedding inspector app in iframe.
Next patches will define API and move hosting functionality from
inspector app to devtools app.

BUG=431697

Review URL: https://codereview.chromium.org/710033003

git-svn-id: svn://svn.chromium.org/blink/trunk@185127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/main/Main.js
@@ -36,23 +36,13 @@
  */
 WebInspector.Main = function()
 {
-    var boundListener = windowLoaded.bind(this);
-    WebInspector.console.setUIDelegate(this);
-    if (document.readyState === "complete") {
-        this._loaded();
+    if (!InspectorAppHost) {
+        console.error("Inspector should be embedded.");
         return;
     }
-
-    /**
-     * @suppressGlobalPropertiesCheck
-     * @this {WebInspector.Main}
-     */
-    function windowLoaded()
-    {
-        this._loaded();
-        window.removeEventListener("DOMContentLoaded", boundListener, false);
-    }
-    window.addEventListener("DOMContentLoaded", boundListener, false);
+    InspectorAppHost.beforeInspectorAppLoad();
+    WebInspector.console.setUIDelegate(this);
+    runOnWindowLoad(this._loaded.bind(this));
 }
 
 WebInspector.Main.prototype = {
@@ -266,6 +256,7 @@ WebInspector.Main.prototype = {
             WebInspector.inspectElementModeController = new WebInspector.InspectElementModeController();
         this._createGlobalStatusBarItems();
 
+        InspectorAppHost.afterInspectorAppLoad();
         InspectorFrontendHost.loadCompleted();
 
         // Give UI cycles to repaint, then proceed with creating connection.

Source/devtools/BUILD.gn
@@ -17,6 +17,7 @@ devtools_core_files =
   gypi_values.devtools_bindings_js_files +
   gypi_values.devtools_common_js_files +
   gypi_values.devtools_components_js_files +
+  gypi_values.devtools_devtools_app_js_files +
   gypi_values.devtools_host_js_files +
   gypi_values.devtools_main_js_files +
   gypi_values.devtools_platform_js_files +
@@ -87,6 +88,7 @@ group("devtools_frontend_resources") {
     # and thus should be guarded out in Debug builds.
     deps += [
       ":concatenated_devtools_css",
+      ":concatenated_inspector_css",
       ":concatenated_toolbox_css",
     ]
   }
@@ -112,6 +114,7 @@ action("generate_devtools_grd") {
     # Debug: all files are picked as-is.
     generated_files = generated_scripts + [
       resources_out_dir + "devtools.html",
+      resources_out_dir + "inspector.html",
       resources_out_dir + "toolbox.html",
     ]
     static_files += all_devtools_files + [ "front_end/Runtime.js" ]
@@ -121,6 +124,9 @@ action("generate_devtools_grd") {
       resources_out_dir + "devtools.css",
       resources_out_dir + "devtools.html",
       resources_out_dir + "devtools.js",
+      resources_out_dir + "inspector.css",
+      resources_out_dir + "inspector.html",
+      resources_out_dir + "inspector.js",
       resources_out_dir + "toolbox.css",
       resources_out_dir + "toolbox.html",
       resources_out_dir + "toolbox.js",
@@ -210,11 +216,13 @@ action("build_applications") {
 
   inputs = helper_scripts + all_devtools_files + generated_scripts + [
     "front_end/devtools.html",
+    "front_end/inspector.html",
     "front_end/toolbox.html",
   ]
 
   outputs = [
     resources_out_dir + "devtools.html",
+    resources_out_dir + "inspector.html",
     resources_out_dir + "toolbox.html",
   ]
 
@@ -225,6 +233,7 @@ action("build_applications") {
   } else {
     outputs += [
       resources_out_dir + "devtools.js",
+      resources_out_dir + "inspector.js",
       resources_out_dir + "toolbox.js",
       resources_out_dir + "audits_module.js",
       resources_out_dir + "console_module.js",
@@ -254,7 +263,7 @@ action("build_applications") {
   }
 
   args = [
-    "devtools", "toolbox",
+    "devtools", "inspector", "toolbox",
     "--input_path", rebase_path("front_end", root_build_dir),
     "--output_path", rebase_path(resources_out_dir, root_build_dir),
     "--debug", debug_mode
@@ -310,6 +319,10 @@ if (!debug_devtools) {
 
   concatenate_css("concatenated_devtools_css") {
     app_name = "devtools"
+  }
+
+  concatenate_css("concatenated_inspector_css") {
+    app_name = "inspector"
   }
 
   concatenate_css("concatenated_toolbox_css") {

Source/devtools/devtools.gyp
@@ -45,6 +45,7 @@
                 ['debug_devtools==0', {
                     'dependencies': [
                         'concatenated_devtools_css',
+                        'concatenated_inspector_css',
                         'concatenated_toolbox_css',
                     ],
                 }],
@@ -96,6 +97,9 @@
                             '<(PRODUCT_DIR)/resources/inspector/devtools.css',
                             '<(PRODUCT_DIR)/resources/inspector/devtools.html',
                             '<(PRODUCT_DIR)/resources/inspector/devtools.js',
+                            '<(PRODUCT_DIR)/resources/inspector/inspector.css',
+                            '<(PRODUCT_DIR)/resources/inspector/inspector.html',
+                            '<(PRODUCT_DIR)/resources/inspector/inspector.js',
                             '<(PRODUCT_DIR)/resources/inspector/toolbox.css',
                             '<(PRODUCT_DIR)/resources/inspector/toolbox.html',
                             '<(PRODUCT_DIR)/resources/inspector/toolbox.js',
@@ -153,6 +157,7 @@
                             '<(PRODUCT_DIR)/resources/inspector/InspectorBackendCommands.js',
                             '<(PRODUCT_DIR)/resources/inspector/SupportedCSSProperties.js',
                             '<(PRODUCT_DIR)/resources/inspector/devtools.html',
+                            '<(PRODUCT_DIR)/resources/inspector/inspector.html',
                             '<(PRODUCT_DIR)/resources/inspector/toolbox.html',
                         ],
                         'inputs': [
@@ -243,16 +248,19 @@
                     '<@(_helper_scripts)',
                     '<@(all_devtools_files)',
                     'front_end/devtools.html',
+                    'front_end/inspector.html',
                     'front_end/toolbox.html',
                     '<(_output_path)/InspectorBackendCommands.js',
                     '<(_output_path)/SupportedCSSProperties.js',
                 ],
-                'action': ['python', '<@(_script_name)', 'devtools', 'toolbox', '--input_path', 'front_end', '--output_path', '<@(_output_path)', '--debug', '<@(debug_devtools)'],
+                'action': ['python', '<@(_script_name)', 'devtools', 'inspector', 'toolbox', '--input_path', 'front_end', '--output_path', '<@(_output_path)', '--debug', '<@(debug_devtools)'],
                 'conditions': [
                     ['debug_devtools==0', { # Release
                         'outputs': [
                             '<(_output_path)/devtools.html',
                             '<(_output_path)/devtools.js',
+                            '<(_output_path)/inspector.html',
+                            '<(_output_path)/inspector.js',
                             '<(_output_path)/toolbox.html',
                             '<(_output_path)/toolbox.js',
                             '<(_output_path)/audits_module.js',
@@ -277,6 +285,7 @@
                     { # Debug
                         'outputs': [
                             '<(_output_path)/devtools.html',
+                            '<(_output_path)/inspector.html',
                             '<(_output_path)/toolbox.html',
                         ]
                     }]
@@ -330,6 +339,23 @@
                         ],
                         'search_path': [ 'front_end' ],
                         'outputs': ['<(PRODUCT_DIR)/resources/inspector/devtools.css'],
+                        'action': ['python', '<@(_script_name)', '<@(_input_stylesheet)', '<@(_outputs)'],
+                    }],
+                },
+                {
+                    'target_name': 'concatenated_inspector_css',
+                    'type': 'none',
+                    'actions': [{
+                        'action_name': 'concatenate_inspector_css',
+                        'script_name': 'scripts/concatenate_css_files.py',
+                        'input_stylesheet': 'front_end/inspector.css',
+                        'inputs': [
+                            '<@(_script_name)',
+                            '<@(_input_stylesheet)',
+                            '<@(devtools_core_base_files)',
+                        ],
+                        'search_path': [ 'front_end' ],
+                        'outputs': ['<(PRODUCT_DIR)/resources/inspector/inspector.css'],
                         'action': ['python', '<@(_script_name)', '<@(_input_stylesheet)', '<@(_outputs)'],
                     }],
                 },

Source/devtools/devtools.gypi
@@ -39,6 +39,7 @@
             '<@(devtools_common_js_files)',
             '<@(devtools_components_js_files)',
             '<@(devtools_core_base_files)',
+            '<@(devtools_devtools_app_js_files)',
             '<@(devtools_host_js_files)',
             '<@(devtools_main_js_files)',
             '<@(devtools_platform_js_files)',
@@ -54,6 +55,9 @@
             'front_end/devtools.css',
             'front_end/devtools.js',
             'front_end/devtools.json',
+            'front_end/inspector.css',
+            'front_end/inspector.js',
+            'front_end/inspector.json',
             'front_end/toolbox.css',
             'front_end/toolbox.js',
             'front_end/toolbox.json',
@@ -123,6 +127,7 @@
             'front_end/components/TimelineGrid.js',
         ],
         'devtools_host_js_files': [
+            'front_end/host/InspectorAppHost.js',
             'front_end/host/InspectorFrontendHost.js',
             'front_end/host/Platform.js',
             'front_end/host/UserMetrics.js',
@@ -204,6 +209,9 @@
             'front_end/bindings/TempFile.js',
             'front_end/bindings/WorkspaceController.js',
         ],
+        'devtools_devtools_app_js_files': [
+            'front_end/devtools_app/DevToolsApp.js',
+        ],
         'devtools_platform_js_files': [
             'front_end/platform/DOMExtension.js',
             'front_end/platform/Promise.js',
@@ -283,6 +291,7 @@
             'front_end/components/module.json',
             'front_end/console/module.json',
             'front_end/devices/module.json',
+            'front_end/devtools_app/module.json',
             'front_end/documentation/module.json',
             'front_end/elements/module.json',
             'front_end/extensions/module.json',

Source/devtools/front_end/devtools.html
@@ -7,10 +7,14 @@
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <meta http-equiv="Content-Security-Policy" content="object-src 'none'; script-src 'self' 'unsafe-eval'">
+    <meta http-equiv="Content-Security-Policy" content="object-src 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline'">
     <link rel="stylesheet" type="text/css" href="devtools.css">
     <script type="text/javascript" src="Runtime.js"></script>
     <script type="text/javascript" src="devtools.js"></script>
 </head>
-<body class="undocked" id="-blink-dev-tools"></body>
+<body class="undocked" id="-blink-dev-tools">
+<script>
+document.write("<iframe class='inspector-app-iframe' src='inspector.html" + window.location.search + "'></iframe>");
+</script>
+</body>
 </html>

Source/devtools/front_end/devtools_app/DevToolsApp.js
@@ -1,0 +1,83 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @constructor
+ * @implements {InspectorAppHostAPI}
+ */
+WebInspector.DevToolsApp = function()
+{
+    window.InspectorAppHost = this;
+
+    // FIXME: These methods are invoked from the backend and should be removed
+    // once we migrate to the "pull" model for extensions retrieval.
+    WebInspector.addExtensions = this._wrapInvocation.bind(this, "addExtensions");
+    WebInspector.setInspectedTabId = this._wrapInvocation.bind(this, "setInspectedTabId");
+    this._invokeOnWebInspectorOnceLoaded = [];
+
+    /**
+     * @type {?Window}
+     */
+    this._inspectorWindow = null;
+}
+
+WebInspector.DevToolsApp.prototype = {
+    /**
+     * @param {!Window} inspectorWindow
+     * @override
+     */
+    inspectorAppWindowLoaded: function(inspectorWindow)
+    {
+        this._inspectorWindow = inspectorWindow;
+        if (window.domAutomationController)
+            this._inspectorWindow.domAutomationController = window.domAutomationController;
+    },
+
+    /**
+     * @override
+     */
+    beforeInspectorAppLoad: function()
+    {
+        if (this._inspectorWindow.uiTests) {
+            // FIXME: move Tests to the host or teach browser counterpart about iframe.
+            window.uiTests = this._inspectorWindow.uiTests;
+        }
+    },
+
+    /**
+     * @override
+     */
+    afterInspectorAppLoad: function()
+    {
+        while (this._invokeOnWebInspectorOnceLoaded.length) {
+            var methodAndParams = this._invokeOnWebInspectorOnceLoaded.shift();
+            this._invokeOnWebInspector(methodAndParams[0], methodAndParams[1]);
+        }
+    },
+
+    /**
+     * @param {string} method
+     */
+    _wrapInvocation: function(method)
+    {
+        var params = Array.prototype.slice.call(arguments, 1);
+        if (this._inspectorWindow) {
+            this._invokeOnWebInspector(method, params);
+        } else {
+            this._invokeOnWebInspectorOnceLoaded.push([method, params]);
+        }
+    },
+
+    /**
+     * @param {string} method
+     * @param {!Array.<*>} params
+     */
+    _invokeOnWebInspector: function(method, params)
+    {
+        var webInspector = this._inspectorWindow["WebInspector"];
+        webInspector[method].apply(webInspector, params);
+    }
+}
+
+new WebInspector.DevToolsApp();

Source/devtools/front_end/host/InspectorAppHost.js
@@ -1,0 +1,37 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @interface
+ */
+function InspectorAppHostAPI()
+{
+}
+
+InspectorAppHostAPI.prototype = {
+    /**
+     * @param {!Window} window
+     */
+    inspectorAppWindowLoaded: function(window) { },
+
+    beforeInspectorAppLoad: function() { },
+
+    afterInspectorAppLoad: function() { }
+}
+
+/**
+ * @type {!InspectorAppHostAPI}
+ */
+var InspectorAppHost;
+
+/**
+ * @suppressGlobalPropertiesCheck
+ */
+(function()
+{
+    if (window.parent !== window) {
+        InspectorAppHost = window.parent.InspectorAppHost;
+        InspectorAppHost.inspectorAppWindowLoaded(window);
+    }
+})();

Source/devtools/front_end/host/InspectorFrontendHost.js
@@ -628,6 +628,10 @@ WebInspector.InspectorFrontendHostStub.prototype = {
 var InspectorFrontendHost = window.InspectorFrontendHost || null;
 
 (function() {
+    if (window.top) {
+        // FIXME(dgozman): use InspectorAppHost everywhere in inspector app instead of InspectorFrontendHost.
+        InspectorFrontendHost = window.top.InspectorFrontendHost || null;
+    }
     if (!InspectorFrontendHost) {
         // Instantiate stub for web-hosted mode if necessary.
         InspectorFrontendHost = new WebInspector.InspectorFrontendHostStub();

commit 1d703326649d42c277c2fc9df902dfb0fbcf3f2c

[DevTools] Fix WebInspector.reload regressed in r185127.
BUG=431697

Review URL: https://codereview.chromium.org/733473002

git-svn-id: svn://svn.chromium.org/blink/trunk@185376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/main/Main.js
@@ -611,7 +611,7 @@ WebInspector.Main.prototype = {
 WebInspector.reload = function()
 {
     InspectorAgent.reset();
-    window.location.reload();
+    window.top.location.reload();
 }
 
 /**